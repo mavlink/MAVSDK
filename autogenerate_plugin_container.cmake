7d18503d3176c8767ab255d8a166084f80de7240
Julian Oes <julian@oes.ch>
2016-09-08 15:02:38 +0200

    cmake: clean up autogeneration, split up, document

---
+++ autogenerate_plugin_container.cmake
@@ -0,0 +1,105 @@
+# Description
+# -----------
+#
+# This cmake script searches through the plugins directory and generates the
+# files device_plugin_container.h and device_plugin_container.cpp using the
+# templates device_plugin_container.h.in and device_plugin_container.cpp.in.
+#
+# Each plugin should have the following structure:
+#
+# example
+# ├── CMakeLists.txt
+# ├── example.cpp
+# ├── example.h
+# ├── example_impl.cpp
+# └── example_impl.h
+#
+# example.h and example.cpp define the publicly visible interface and
+# example_impl.h and example_impl.cpp contain the implementation of the
+# actual plugin.
+
+
+# This defines the plugins directory.
+file(GLOB plugins plugins/*)
+
+# Look for plugins in plugin directory
+foreach(plugin ${plugins})
+	if(IS_DIRECTORY ${plugin})
+        message("Found plugin ${plugin}")
+		add_subdirectory(${plugin})
+        foreach(source_file ${source_files})
+            list(APPEND plugin_source_files "${plugin}/${source_file}")
+        endforeach()
+        list(APPEND plugin_class_names "${class_name}")
+        list(APPEND plugin_header_files "${header_files}")
+        list(APPEND plugin_impl_header_files "${impl_header_files}")
+	endif()
+endforeach()
+
+# Contains the #include "example.h" for the public facing files.
+set(INCLUDES_STRING "")
+foreach(header_file ${plugin_header_files})
+    set(INCLUDES_STRING "${INCLUDES_STRING}#include \"${header_file}\"\n")
+endforeach()
+
+# Contains #include "include "example_impl.h" for the implementation header files.
+set(IMPL_INCLUDES_STRING "")
+foreach(impl_header_file ${plugin_impl_header_files})
+    set(IMPL_INCLUDES_STRING "${IMPL_INCLUDES_STRING}#include \"${impl_header_file}\"\n")
+endforeach()
+
+# Contains the forward declarations such as `class ExampleImpl;`.
+set(FORWARD_DECLARATION_STRING "")
+
+# Contains the constructor entries.
+set(PLUGIN_CTOR_STRING "")
+
+# Contains the destructor entries.
+set(PLUGIN_DTOR_STRING "")
+
+# Contains the member variables for the plugin instances.
+set(PLUGIN_MEMBER_STRING "")
+
+# Contains the getter methods which allow to access a plugin with device().example().
+set(PLUGIN_GETTER_STRING "")
+
+# Creates a list of the plugin implementations to call init()/deinit() on them.
+set(PLUGIN_LIST_APPEND_STRING "")
+
+# Go through all the plugins and generate the strings.
+foreach(class_name ${plugin_class_names})
+
+    string(TOLOWER ${class_name} class_name_lowercase)
+
+    set(getter_name ${class_name_lowercase})
+    set(member_name "_${class_name_lowercase}")
+    set(impl_class_name "${class_name}Impl")
+    set(impl_member_name "_${class_name_lowercase}_impl")
+
+    set(FORWARD_DECLARATION_STRING
+        "${FORWARD_DECLARATION_STRING}class ${impl_class_name};\n")
+
+    set(PLUGIN_GETTER_STRING
+        "${PLUGIN_GETTER_STRING}    const ${class_name} &${getter_name}() { return ${member_name}; }\n")
+
+    set(PLUGIN_MEMBER_STRING
+        "${PLUGIN_MEMBER_STRING}    ${impl_class_name} *${impl_member_name};\n")
+    set(PLUGIN_MEMBER_STRING
+        "${PLUGIN_MEMBER_STRING}    ${class_name} ${member_name};\n")
+
+    set(PLUGIN_CTOR_STRING
+        "${PLUGIN_CTOR_STRING}    ${impl_member_name}(new ${impl_class_name}()),\n")
+    set(PLUGIN_CTOR_STRING
+        "${PLUGIN_CTOR_STRING}    ${member_name}(${impl_member_name}),\n")
+
+    set(PLUGIN_DTOR_STRING
+        "${PLUGIN_DTOR_STRING}    delete ${impl_member_name};\n")
+
+    set(PLUGIN_LIST_APPEND_STRING
+        "${PLUGIN_LIST_APPEND_STRING}   _plugin_impl_list.push_back(${impl_member_name});\n")
+
+endforeach()
+
+# Do the string replacements.
+configure_file(src/device_plugin_container.h.in src/device_plugin_container.h)
+configure_file(src/device_plugin_container.cpp.in src/device_plugin_container.cpp)
