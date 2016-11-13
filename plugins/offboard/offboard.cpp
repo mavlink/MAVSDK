efced237c157cf3db1aab8ce638564458e0e1711
Julian Oes <julian@oes.ch>
2016-11-13 21:19:51 +0100

    offboard: added result enum to string conversion

--- plugins/offboard/offboard.cpp
+++ plugins/offboard/offboard.cpp
@@ -37,4 +37,25 @@ void Offboard::set_velocity(VelocityNEDYaw velocity_ned_yaw)
     return _impl->set_velocity(velocity_ned_yaw);
 }
 
+const char *Offboard::result_str(Result result)
+{
+    switch (result) {
+        case Result::SUCCESS:
+            return "Success";
+        case Result::NO_DEVICE:
+            return "No device";
+        case Result::CONNECTION_ERROR:
+            return "Connection error";
+        case Result::BUSY:
+            return "Busy";
+        case Result::COMMAND_DENIED:
+            return "Command denied";
+        case Result::TIMEOUT:
+            return "Timeout";
+        case Result::UNKNOWN:
+        default:
+            return "Unknown";
+    }
+}
+
 } // namespace dronelink
