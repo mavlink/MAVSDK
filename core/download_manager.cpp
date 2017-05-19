32f163ae77a6f6630e5a6d0904d38068cdb40144
Dario Röthlisberger <dario.roethlisberger@gmail.com>
2017-05-19 17:45:53 +0200

    download_manager: add download manager
    A simple download manager has been added which makes use of curl and is
    able perform async download or uploads.

---
+++ core/download_manager.cpp
@@ -0,0 +1,119 @@
+#include <download_manager.h>
+#include <http_helper.h>
+
+
+namespace dronelink {
+
+DownloadManager::DownloadManager()
+{
+    start();
+}
+
+DownloadManager::~DownloadManager()
+{
+    stop();
+}
+
+void DownloadManager::start()
+{
+    _should_exit = false;
+    _work_thread = new std::thread(work_thread, this);
+}
+
+void DownloadManager::stop()
+{
+    _should_exit = true;
+    _work_queue.stop();
+    if (_work_thread != nullptr) {
+        _work_thread->join();
+        delete _work_thread;
+        _work_thread = nullptr;
+    }
+}
+
+bool DownloadManager::download_sync(const std::string &url, const std::string &local_path)
+{
+    auto work_item = std::make_shared<DownloadItem>(url, local_path, nullptr);
+    bool success = do_download(work_item);
+    return success;
+}
+
+void DownloadManager::download_async(const std::string &url, const std::string &local_path,
+                                     const progress_callback_t &progress_callback)
+{
+    auto work_item = std::make_shared<DownloadItem>(url, local_path, progress_callback);
+    _work_queue.enqueue(work_item);
+}
+
+bool DownloadManager::upload_sync(const std::string &target_url, const std::string &local_path)
+{
+    auto work_item = std::make_shared<UploadItem>(target_url, local_path, nullptr);
+    bool success = do_upload(work_item);
+    return success;
+}
+
+void DownloadManager::upload_async(const std::string &target_url, const std::string &local_path,
+                                   const progress_callback_t &progress_callback)
+{
+    auto work_item = std::make_shared<UploadItem>(target_url, local_path, progress_callback);
+    _work_queue.enqueue(work_item);
+}
+
+void DownloadManager::work_thread(DownloadManager *self)
+{
+    while (!self->_should_exit) {
+        auto item = self->_work_queue.dequeue();
+        if (item == nullptr) {
+            continue;
+        }
+        do_item(item);
+    }
+}
+
+void DownloadManager::do_item(const std::shared_ptr<WorkItem> &item)
+{
+    auto download_item = std::dynamic_pointer_cast<DownloadItem>(item);
+    if (nullptr != download_item) {
+        do_download(download_item);
+        return;
+    }
+
+    auto upload_item = std::dynamic_pointer_cast<UploadItem>(item);
+    if (nullptr != upload_item) {
+        do_upload(upload_item);
+        return;
+    }
+}
+
+bool DownloadManager::do_download(const std::shared_ptr<DownloadItem> &item)
+{
+    HttpHelper helper;
+    bool success = helper.downloadAndSaveWithProgress(item->get_url(), item->get_local_path(),
+                                                      item->get_progress_callback());
+    return success;
+}
+
+bool DownloadManager::do_upload(const std::shared_ptr<UploadItem> &item)
+{
+    HttpHelper helper;
+    bool success = helper.uploadFile(item->get_target_url(), item->get_local_path(),
+                                     item->get_progress_callback());
+    if (success == false) {
+        auto callback = item->get_progress_callback();
+        callback(100);
+    }
+
+    return success;
+}
+
+bool DownloadManager::download_content_sync(const std::string &url, std::string &content)
+{
+    HttpHelper helper;
+    bool success = helper.download(url, content);
+    return success;
+}
+
+
+} // namespace dronelink
+
+
---
+++ core/download_manager.h
@@ -0,0 +1,146 @@
+#pragma once
+
+#include "queue.h"
+#include <thread>
+#include <atomic>
+#include <memory>
+#include <string>
+#include "update.h"
+
+namespace dronelink {
+
+typedef std::function<int(int progress)> progress_callback_t;
+
+class DownloadManager
+{
+public:
+    explicit DownloadManager();
+    ~DownloadManager();
+
+    void start();
+    void stop();
+
+    bool download_sync(const std::string &url, const std::string &local_path);
+    bool download_content_sync(const std::string &url, std::string &content);
+    void download_async(const std::string &url, const std::string &local_path,
+                        const progress_callback_t &progress_callback = nullptr);
+
+    bool upload_sync(const std::string &target_url, const std::string &local_path);
+    void upload_async(const std::string &target_url, const std::string &local_path,
+                      const progress_callback_t &progress_callback = nullptr);
+
+    // Non-copyable
+    DownloadManager(const DownloadManager &) = delete;
+    const DownloadManager &operator=(const DownloadManager &) = delete;
+
+private:
+
+    class WorkItem
+    {
+    public:
+        WorkItem() {}
+        virtual ~WorkItem() {}
+
+        WorkItem(WorkItem &) = delete;
+        WorkItem operator=(WorkItem &) = delete;
+    };
+
+    class DownloadContentItem : public WorkItem
+    {
+    public:
+        DownloadContentItem(const std::string &url) :
+            _url(url) { }
+
+        std::string get_url() const
+        {
+            return _url;
+        }
+
+        DownloadContentItem(DownloadContentItem &) = delete;
+        DownloadContentItem operator=(DownloadContentItem &) = delete;
+
+    private:
+        std::string _url;
+    };
+
+    class DownloadItem : public WorkItem
+    {
+    public:
+        DownloadItem(const std::string &url, const std::string &local_path,
+                     const progress_callback_t &progress_callback) :
+            _url(url),
+            _local_path(local_path),
+            _progress_callback(progress_callback) { }
+
+        std::string get_local_path() const
+        {
+            return _local_path;
+        }
+
+        std::string get_url() const
+        {
+            return _url;
+        }
+
+        progress_callback_t get_progress_callback() const
+        {
+            return _progress_callback;
+        }
+
+        DownloadItem(DownloadItem &) = delete;
+        DownloadItem operator=(DownloadItem &) = delete;
+
+    private:
+        std::string _url;
+        std::string _local_path;
+        progress_callback_t _progress_callback;
+    };
+
+
+    class UploadItem : public WorkItem
+    {
+    public:
+        UploadItem(const std::string &target_url, const std::string &local_path,
+                   const progress_callback_t &progress_callback) :
+            _target_url(target_url),
+            _local_path(local_path),
+            _progress_callback(progress_callback) { }
+
+        std::string get_local_path() const
+        {
+            return _local_path;
+        }
+
+        std::string get_target_url() const
+        {
+            return _target_url;
+        }
+
+        progress_callback_t get_progress_callback() const
+        {
+            return _progress_callback;
+        }
+
+        UploadItem(UploadItem &) = delete;
+        UploadItem operator=(UploadItem &) = delete;
+
+    private:
+        std::string _target_url;
+        std::string _local_path;
+        progress_callback_t _progress_callback;
+    };
+
+    static void work_thread(DownloadManager *self);
+    static void do_item(const std::shared_ptr<WorkItem> &item);
+    static bool do_download(const std::shared_ptr<DownloadItem> &item);
+    static bool do_upload(const std::shared_ptr<UploadItem> &item);
+
+    SafeQueue <std::shared_ptr<WorkItem>> _work_queue {};
+    std::thread *_work_thread = nullptr;
+
+    std::atomic<bool> _should_exit {false};
+};
+
+} // namespace dronelink
+
+
