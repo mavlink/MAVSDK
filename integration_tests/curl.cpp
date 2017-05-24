3020a4ef305cd5955551ff4e310763e9198c828b
Dario Röthlisberger <dario.roethlisberger@gmail.com>
2017-05-24 11:50:36 +0200

    curl_wrapper: add integration tests for curl
    Added integration tests for download_text and download_file_to_path
    which download a file stored on s3 bucket. Tests for uploading files
    are still missing.

--- integration_tests/curl.cpp
+++ integration_tests/curl.cpp
@@ -1,5 +1,7 @@
 #include "dronelink.h"
 #include "curl_wrapper.h"
+#include <fstream>
+#include <iostream> 
 
 using namespace dronelink;
 
@@ -18,14 +20,28 @@ protected:
         _file_url_not_existing = "https://notexisting.file";
         _local_path = "testfile.txt";
     }
+
     virtual void TearDown()
     {
+        clean();
+    }
 
+    void clean()
+    {
+        remove(_local_path.c_str());
+    }
+
+    bool check_file_exists(const std::string& file_path)
+    {
+        std::ifstream infile(file_path.c_str());
+        return infile.good();
     }
 };
 
 TEST_F(CurlTest, Curl_DownloadText_HTTP_Success)
 {
+    clean();
+
     std::string content;
 
     CurlWrapper curl_wrapper;
@@ -37,6 +53,8 @@ TEST_F(CurlTest, Curl_DownloadText_HTTP_Success)
 
 TEST_F(CurlTest, Curl_DownloadText_HTTPS_Success)
 {
+    clean();
+
     std::string content;
 
     CurlWrapper curl_wrapper;
@@ -48,6 +66,8 @@ TEST_F(CurlTest, Curl_DownloadText_HTTPS_Success)
 
 TEST_F(CurlTest, Curl_DownloadText_FileNotFound)
 {
+    clean();
+
     std::string content = "content gets cleared if download fails";
 
     CurlWrapper curl_wrapper;
@@ -59,40 +79,60 @@ TEST_F(CurlTest, Curl_DownloadText_FileNotFound)
 
 TEST_F(CurlTest, Curl_DownloadFile_WithoutProgressFeedback_Success)
 {
-    CurlWrapper curl_wrapper;
-    bool success = curl_wrapper.download_file_to_path(_file_url_existing_https, _local_path, nullptr);
+    clean();
 
+    CurlWrapper curl_wrapper;
+
+    bool success = curl_wrapper.download_file_to_path(_file_url_existing_https, _local_path, nullptr);
     EXPECT_EQ(success, true);
+
+    bool file_exists = check_file_exists(_local_path);
+    EXPECT_EQ(file_exists, true);
 }
 
 TEST_F(CurlTest, Curl_DownloadFile_WithoutProgressFeedback_FileNotFound)
 {
-    CurlWrapper curl_wrapper;
-    bool success = curl_wrapper.download_file_to_path(_file_url_not_existing, _local_path, nullptr);
+    clean();
 
+    CurlWrapper curl_wrapper;
+
+    bool success = curl_wrapper.download_file_to_path(_file_url_not_existing, _local_path, nullptr);
     EXPECT_EQ(success, false);
+
+    bool file_exists = check_file_exists(_local_path);
+    EXPECT_EQ(file_exists, false);
 }
 
 TEST_F(CurlTest, Curl_DownloadFile_ProgressFeedback_Success)
 {
+    clean();
+
     int last_progress;
     auto progress = [&last_progress](int progress) -> int { last_progress = progress; return 0; };
 
     CurlWrapper curl_wrapper;
-    bool success = curl_wrapper.download_file_to_path(_file_url_existing_https, _local_path, progress);
 
+    bool success = curl_wrapper.download_file_to_path(_file_url_existing_https, _local_path, progress);
     EXPECT_EQ(success, true);
     EXPECT_EQ(last_progress, 100);
+
+    bool file_exists = check_file_exists(_local_path);
+    EXPECT_EQ(file_exists, true);
 }
 
 TEST_F(CurlTest, Curl_DownloadFile_ProgressFeedback_FileNotFound)
 {
+    clean();
+
     int last_progress = -1;
     auto progress = [&last_progress](int progress) -> int { last_progress = progress; return 0; };
 
     CurlWrapper curl_wrapper;
-    bool success = curl_wrapper.download_file_to_path(_file_url_not_existing, _local_path, progress);
 
+    bool success = curl_wrapper.download_file_to_path(_file_url_not_existing, _local_path, progress);
     EXPECT_EQ(success, false);
     EXPECT_EQ(last_progress, -1);
+
+    bool file_exists = check_file_exists(_local_path);
+    EXPECT_EQ(file_exists, false);
 }
