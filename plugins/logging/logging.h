8811635a1c5f070a8b4b4f1820dfec0b8d61d848
Julian Oes <julian@oes.ch>
2017-08-29 16:06:45 +0100

    logging: added doxygen comments (not implemented)

--- plugins/logging/logging.h
+++ plugins/logging/logging.h
@@ -6,12 +6,27 @@ namespace dronecore {
 
 class LoggingImpl;
 
+/**
+ * The Logging class allows log data using logger and log streaming from the vehicle.
+ *
+ * Note that this feature is not implemented yet.
+ */
 class Logging
 {
 public:
+    /**
+     * Constructor for Logging called internally.
+     */
     explicit Logging(LoggingImpl *impl);
+
+    /**
+     * Destructor for Logging called internally.
+     */
     ~Logging();
 
+    /**
+     * Results for logging requests.
+     */
     enum class Result {
         SUCCESS = 0,
         NO_DEVICE,
@@ -22,14 +37,52 @@ public:
         UNKNOWN
     };
 
+    /**
+     * Returns human-readable English string for Logging::Result.
+     *
+     * @param result result enum
+     */
     static const char *result_str(Result);
 
+    /**
+     * Callback type for logging requests.
+     */
     typedef std::function<void(Result)> result_callback_t;
 
+    /**
+     * Start logging (synchronous).
+     *
+     * Note this is not implemented yet.
+     *
+     * @return result of request
+     */
     Result start_logging() const;
+
+    /**
+     * Stop logging (synchronous).
+     *
+     * Note this is not implemented yet.
+     *
+     * @return result of request
+     */
     Result stop_logging() const;
 
+    /**
+     * Start logging (asynchronous).
+     *
+     * Note this is not implemented yet.
+     *
+     * @param callback to get result of request
+     */
     void start_logging_async(result_callback_t callback);
+
+    /**
+     * Stop logging (asynchronous).
+     *
+     * Note this is not implemented yet.
+     *
+     * @param callback to get result of request
+     */
     void stop_logging_async(result_callback_t callback);
 
     // Non-copyable
