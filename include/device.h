fbb35815a47712cba824930bb8a48a800eaa7ba5
Julian Oes <julian@oes.ch>
2017-08-30 11:51:55 +0100

    device: comment fixes

--- include/device.h
+++ include/device.h
@@ -7,27 +7,27 @@ namespace dronecore {
 class DeviceImpl;
 
 /**
- * A Device represents a unit such as a drone or robot.
+ * A Device represents a vehicle such as a drone or robot.
  *
- * A device can consists of multiple components such as an autopilot with a gimbal and camera.
- * The device class allows to access plugins such as Action or Telemetry because it is based
- * on DevicePluginContainer.
+ * A device can consist of multiple components such as an autopilot with a gimbal and camera.
+ * The device class allows to access plugins such as dronecore::Action or dronecore::Telemetry
+ * because it is based on DevicePluginContainer.
  */
 class Device : public DevicePluginContainer
 {
 public:
     /**
-     * Constructor of a device called internally.
+     * Constructor (called internall).
      *
-     * This constructor does not need to be called by any consumer of the API. Instead
-     * the device can be accessed using e.g. `DroneCore::device()`.
+     * This constructor does not need to be called by any consumer of the API. Instead the device
+     * can be accessed using e.g. `DroneCore::device()`.
      *
      * @param impl the underlying device implementation
      */
     explicit Device(DeviceImpl *impl);
 
     /**
-     * Destructor of a device called internally.
+     * Destructor (called internally).
      *
      * The destructor of Device does not need to be called by any consumer of the API. Instead
      * the device will be cleaned up when DroneCore is destroyed.
