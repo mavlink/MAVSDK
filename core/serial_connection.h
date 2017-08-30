7e538dd3486fd45f45cba3756a9c4ed306021046
ritul jasuja <ritul.jasuja@intel.com>
2017-08-30 14:19:30 +0530

    Serial connection support
    Add API's for Serial connection send and receive
    Compilation supported for Linux platform
    Test performed on Intel Aero

--- core/serial_connection.h
+++ core/serial_connection.h
@@ -13,10 +13,27 @@ public:
     bool is_ok() const;
     DroneCore::ConnectionResult start();
     DroneCore::ConnectionResult stop();
+    ~SerialConnection();
 
     bool send_message(const mavlink_message_t &message);
+    // Non-copyable
+    SerialConnection(const SerialConnection &) = delete;
+    const SerialConnection &operator=(const SerialConnection &) = delete;
 
 private:
+    DroneCore::ConnectionResult setup_port();
+    void start_recv_thread();
+    static void receive(SerialConnection *parent);
+
+    static constexpr int DEFAULT_SERIAL_BAUDRATE = 9600;
+    static constexpr auto DEFAULT_SERIAL_DEV_PATH = "/dev/ttyS0";
+    std::string _serial_node = {};
+    int _baudrate = DEFAULT_SERIAL_BAUDRATE;
+
+    std::mutex _mutex = {};
+    int _fd = -1;
+    std::thread *_recv_thread = nullptr;
+    std::atomic_bool _should_exit;
 };
 
 } // namespace dronecore
