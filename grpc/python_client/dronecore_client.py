78750726d90954037afcb0af7f7fb6cefe2fbc91
Avinash Reddy Palleti <avinash.reddy.palleti@intel.com>
2017-08-30 16:16:19 +0530

    Sample application to explore gRPC
    Sample python application using gRPC to arm, takeoff and land.

---
+++ grpc/python_client/dronecore_client.py
@@ -0,0 +1,16 @@
+import grpc
+import time
+import dronecore_pb2
+import dronecore_pb2_grpc
+
+def run():
+  channel = grpc.insecure_channel('0.0.0.0:50051')
+  stub = dronecore_pb2_grpc.DroneCoreRPCStub(channel)
+  stub.Arm(dronecore_pb2.Empty())
+  time.sleep(2)
+  stub.TakeOff(dronecore_pb2.Empty())
+  time.sleep(5)
+  stub.Land(dronecore_pb2.Empty())
+
+if __name__=='__main__':
+  run()
