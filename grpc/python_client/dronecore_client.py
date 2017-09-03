626772f9dcf005a77c9b851e74bab0b924f6ade2
Julian Oes <julian@oes.ch>
2017-09-03 20:30:06 +0200

    dronecore_client.py: PEP8 indenting

--- grpc/python_client/dronecore_client.py
+++ grpc/python_client/dronecore_client.py
@@ -6,13 +6,13 @@ import dronecore_pb2
 import dronecore_pb2_grpc
 
 def run():
-  channel = grpc.insecure_channel('0.0.0.0:50051')
-  stub = dronecore_pb2_grpc.DroneCoreRPCStub(channel)
-  stub.Arm(dronecore_pb2.Empty())
-  time.sleep(2)
-  stub.TakeOff(dronecore_pb2.Empty())
-  time.sleep(5)
-  stub.Land(dronecore_pb2.Empty())
+    channel = grpc.insecure_channel('0.0.0.0:50051')
+    stub = dronecore_pb2_grpc.DroneCoreRPCStub(channel)
+    stub.Arm(dronecore_pb2.Empty())
+    time.sleep(2)
+    stub.TakeOff(dronecore_pb2.Empty())
+    time.sleep(5)
+    stub.Land(dronecore_pb2.Empty())
 
 if __name__=='__main__':
-  run()
+    run()
