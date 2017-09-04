b1f8c1bcc74345aa991802f16ab4db2c590cc89b
Hamish Willee <hamishwillee@gmail.com>
2017-09-04 17:50:41 +1000

    General fixes to in-source docs

--- include/dronecore.h
+++ include/dronecore.h
@@ -11,122 +11,130 @@ namespace dronecore {
 class DroneCoreImpl;
 
 /**
- * DroneCore is an API and library for the PX4 flight stack using Mavlink.
+ * @brief This is the main class of **%DroneCore MAVLink API Library** (for the Dronecode Platform).
+
+ * It is used to discover vehicles and manage active connections.
  *
- * DroneCore is the main class of the DroneCore library.
- * It manages connections and allows vehicles to be discovered and addressed.
+ * An instance of this class must be created (first) in order to use the library.
+ * The instance must be destroyed after use in order to break connections and release all resources.
  */
 class DroneCore
 {
 public:
 
     /**
-     * Constructor
-     *
-     * In order to use DroneCore, an instance of this class needs to be created first.
+     * @brief Constructor.
      */
     DroneCore();
 
     /**
-     * Destructor
+     * @brief Destructor.
      *
-     * In order to disconnect everything and release all resources, a DroneCore instance should be
-     * destroyed.
+     * Disconnects all connected vehicles and releases all resources.
      */
     ~DroneCore();
 
     /**
-     * Result returned when adding a connection.
+     * @brief Result type returned when adding a connection.
      *
-     * DroneCore does not throw exceptions, instead a result will be returned.
+     * **Note**: DroneCore does not throw exceptions. Instead a result of this type will be
+     * returned when you add a connection: add_udp_connection().
      */
     enum class ConnectionResult {
-        SUCCESS = 0,
-        TIMEOUT,
-        SOCKET_ERROR,
-        BIND_ERROR,
-        CONNECTION_ERROR,
-        NOT_IMPLEMENTED,
-        DEVICE_NOT_CONNECTED,
-        DEVICE_BUSY,
-        COMMAND_DENIED,
-        DESTINATION_IP_UNKNOWN,
-        CONNECTIONS_EXHAUSTED
+        SUCCESS = 0, /**< @brief %Connection succeeded. */
+        TIMEOUT, /**< @brief %Connection timed out. */
+        SOCKET_ERROR, /**< @brief Socket error. */
+        BIND_ERROR, /**< @brief Bind error. */
+        CONNECTION_ERROR, /**< @brief %Connection error. */
+        NOT_IMPLEMENTED, /**< @brief %Connection type not implemented. */
+        DEVICE_NOT_CONNECTED, /**< @brief No device is connected. */
+        DEVICE_BUSY, /**< @brief %Device is busy. */
+        COMMAND_DENIED, /**< @brief Command is denied. */
+        DESTINATION_IP_UNKNOWN, /**< @brief %Connection IP is unknown. */
+        CONNECTIONS_EXHAUSTED /**< @brief %Connections exhausted. */
     };
 
     /**
-     * Translates the DroneCore::ConnectionResult enum to a human-readable English string.
+     * @brief Translates the DroneCore::ConnectionResult enum to a human-readable English string.
      */
     static const char *connection_result_str(ConnectionResult);
 
     /**
-     * Adds a UDP connection with the default arguments.
+     * @brief Adds a UDP connection with the default arguments.
      *
      * This will listen on UDP port 14540.
      *
-     * @return the result of adding the connection.
+     * @return The result of adding the connection.
      */
     ConnectionResult add_udp_connection();
 
     /**
-     * Adds a UDP connection with a specific port number.
+     * @brief Adds a UDP connection with a specific port number.
      *
-     * @param local_port_number The local UDP port to listen to
-     * @return the result of adding the connection.
+     * @param local_port_number The local UDP port to listen to.
+     * @return The result of adding the connection.
      */
     ConnectionResult add_udp_connection(int local_port_number);
 
     /**
-     * Return vector of device UUIDs.
+     * @brief Get vector of device UUIDs.
      *
      * This returns a vector of the UUIDs of all devices that have been discovered so far.
      *
-     * @return a reference to the vector containing the UUIDs
+     * @return A reference to the vector containing the UUIDs.
      */
     const std::vector<uint64_t> &device_uuids() const;
 
     /**
-     * Return the first discovered device.
+     * @brief Get the first discovered device.
      *
      * This returns the first discovered device or a null device if no device has yet been found.
      *
-     * @return a reference to a device
+     * @return A reference to a device.
      */
     Device &device() const;
 
     /**
-     * Return the device with the specified UUID.
+     * @brief Get the device with the specified UUID.
      *
      * This returns a device for a given UUID if such a device has been discovered and a null
      * device otherwise.
+     *
+     * @param uuid UUID of device to get.
+     * @return A reference to the specified device.
      */
     Device &device(uint64_t uuid) const;
 
     /**
-     * Callback type for discover and timeout notifications.
+     * @brief Callback type for discover and timeout notifications.
      *
-     * @param uuid UUID of device
+     * @param uuid UUID of device.
      */
     typedef std::function<void(uint64_t uuid)> event_callback_t;
 
     /**
-     * Register callback for device discovery.
+     * @brief Register callback for device discovery.
      *
      * This sets a callback that will be notified if a new device is discovered.
      *
-     * Note that only one callback can be registered at a time. If this function is called several
+     * **Note** Only one callback can be registered at a time. If this function is called several
      * times, previous callbacks will be overwritten.
+     *
+     * @param callback Callback to register.
+     *
      */
     void register_on_discover(event_callback_t callback);
 
     /**
-     * Register callback for device timeout.
+     * @brief Register callback for device timeout.
      *
      * This sets a callback that will be notified if no heartbeat of the device has been received
      * in 3 seconds.
      *
-     * Note that only one callback can be registered at a time. If this function is called several
+     * **Note** Only one callback can be registered at a time. If this function is called several
      * times, previous callbacks will be overwritten.
+     *
+     * @param callback Callback to register.
      */
     void register_on_timeout(event_callback_t callback);
 
--- plugins/offboard/offboard.h
+++ plugins/offboard/offboard.h
@@ -7,120 +7,127 @@ namespace dronecore {
 class OffboardImpl;
 
 /**
- * The Offboard class allows to control a drone by velocity commands.
+ * @brief This class is used to control a drone with velocity commands.
  *
  * The module is called offboard because the velocity commands can be sent from external sources
  * as opposed to onboard control right inside the autopilot "board".
  *
- * Attention: this is work in progress, use with caution!
+ * **Attention:** this is work in progress, use with caution!
  */
 class Offboard
 {
 public:
     /**
-     * Constructor for Offboard called internally.
+     * @brief Constructor (internal use only).
      */
     explicit Offboard(OffboardImpl *impl);
 
     /**
-     * Destructor for Offboard called internally.
+     * @brief Destructor (internal use only).
      */
     ~Offboard();
 
     /**
-     * Results for offboard requests
+     * @brief Results for offboard requests.
      */
     enum class Result {
-        SUCCESS = 0,
-        NO_DEVICE,
-        CONNECTION_ERROR,
-        BUSY,
-        COMMAND_DENIED,
-        TIMEOUT,
-        UNKNOWN
+        SUCCESS = 0, /**< @brief %Request succeeded. */
+        NO_DEVICE, /**< @brief No device connected. */
+        CONNECTION_ERROR, /**< @brief %Connection error. */
+        BUSY, /**< @brief Vehicle busy. */
+        COMMAND_DENIED, /**< @brief Command denied. */
+        TIMEOUT, /**< @brief %Request timeout. */
+        UNKNOWN /**< @brief Unknown error. */
     };
 
     /**
-     * Returns human-readable English string for Offboard::Result.
+     * @brief Get human-readable English string for Offboard::Result.
      *
-     * @param result result enum
+     * @param result The enum value for which a string is required.
+     * @returns Human-readable string for enum value.
      */
     static const char *result_str(Result result);
 
     /**
-     * Callback type for offboard requests.
+     * @brief Callback type for offboard requests.
      */
     typedef std::function<void(Result)> result_callback_t;
 
     /**
-     * Type for Velocity commands in NED (North East Down) coordinates and yaw.
+     * @brief Type for Velocity commands in NED (North East Down) coordinates and yaw.
      */
     struct VelocityNEDYaw {
-        float north_m_s; /**< Velocity North in meters/second */
-        float east_m_s; /**< Velocity East in meters/second */
-        float down_m_s; /**< Velocity Down in meters/second */
+        float north_m_s; /**< Velocity North in metres/second. */
+        float east_m_s; /**< Velocity East in metres/second. */
+        float down_m_s; /**< Velocity Down in metres/second. */
         float yaw_deg; /**< Yaw in degrees (0 North, positive is clock-wise looking from above. */
     };
 
     /**
-     * Type for velocity commands in body coordinates (forward, right, down and yaw angular rate).
+     * @brief Type for velocity commands in body coordinates (forward, right, down and yaw angular rate).
      */
     struct VelocityBodyYawspeed {
-        float forward_m_s; /**< Velocity forward in meters/second */
-        float right_m_s; /**< Velocity right in meters/second */
-        float down_m_s; /**< Velocity down in meters/second */
+        float forward_m_s; /**< Velocity forward in metres/second. */
+        float right_m_s; /**< Velocity right in metres/secon.d */
+        float down_m_s; /**< Velocity down in metres/second. */
         float yawspeed_deg_s; /**< Yaw angular rate in degrees/second (positive for clock-wise
-                                   looking from above */
+                                   looking from above. */
     };
 
     /**
-     * Starts offboard control (synchronous).
+     * @brief Start offboard control (synchronous).
      *
-     * Attention: this is work in progress, use with caution!
+     * **Attention:** this is work in progress, use with caution!
      *
-     * @return result of request
+     * @return Result of request.
      */
     Offboard::Result start() const;
 
     /**
-     * Stops offboard control (synchronous).
+     * @brief Stop offboard control (synchronous).
      *
-     * @return result of request
+     * @return Result of request.
      */
     Offboard::Result stop() const;
 
     /**
-     * Starts offboard control (asynchronous).
+     * @brief Start offboard control (asynchronous).
      *
-     * Attention: this is work in progress, use with caution!
+     * **Attention:** This is work in progress, use with caution!
      *
-     * @param callback callback to receive request result
+     * @param callback Callback to receive request result.
      */
     void start_async(result_callback_t callback);
 
     /**
-     * Stops offboard control (asynchronous).
+     * @brief Stop offboard control (asynchronous).
      *
-     * @param callback callback to receive request result
+     * @param callback Callback to receive request result.
      */
     void stop_async(result_callback_t callback);
 
     /**
-     * Sets the velocity in NED coordinates and yaw.
+     * @brief Set the velocity in NED coordinates and yaw.
      *
-     * @param velocity_ned_yaw velocity and yaw struct
+     * @param velocity_ned_yaw Velocity and yaw `struct`.
      */
     void set_velocity_ned(VelocityNEDYaw velocity_ned_yaw);
 
     /**
-     * Sets the velocity body coordinates coordinates and yaw angular rate.
+     * @brief Set the velocity body coordinates coordinates and yaw angular rate.
      *
-     * @param velocity_body_yawspeed velocity and yaw angular rate struct
+     * @param velocity_body_yawspeed Velocity and yaw angular rate `struct`.
      */
     void set_velocity_body(VelocityBodyYawspeed velocity_body_yawspeed);
 
     // Non-copyable
+    /**
+     * @brief Copy constructor (object is not copyable).
+     */
     Offboard(const Offboard &) = delete;
+    /**
+     * @brief Equality operator (object is not copyable).
+     */
     const Offboard &operator=(const Offboard &) = delete;
 
 private:
