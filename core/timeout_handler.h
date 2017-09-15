d25fee6bbbc6600f490821e5272621d3ec2e97cd
Julian Oes <julian@oes.ch>
2017-09-15 17:10:39 +0200

    core: Added new class for timeouts
    This will replace the functionality inside device_impl.

---
+++ core/timeout_handler.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <mutex>
+#include <memory>
+#include <functional>
+#include <map>
+#include "global_include.h"
+
+namespace dronecore {
+
+class TimeoutHandler
+{
+public:
+    TimeoutHandler();
+    ~TimeoutHandler();
+
+    // delete copy and move constructors and assign operators
+    TimeoutHandler(TimeoutHandler const &) = delete;            // Copy construct
+    TimeoutHandler(TimeoutHandler &&) = delete;                 // Move construct
+    TimeoutHandler &operator=(TimeoutHandler const &) = delete; // Copy assign
+    TimeoutHandler &operator=(TimeoutHandler &&) = delete;      // Move assign
+
+    void add(std::function<void()> callback, double duration_s, void **cookie);
+    void refresh(const void *cookie);
+    void remove(const void *cookie);
+
+    void run_once();
+
+private:
+    struct Timeout {
+        std::function<void()> callback;
+        dl_time_t time;
+        double duration_s;
+    };
+
+    std::map<void *, std::shared_ptr<Timeout>> _timeouts {};
+    std::mutex _timeouts_mutex {};
+};
+
+} // namespace dronecore
