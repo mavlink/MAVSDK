d4d7ee08e110b33604ae6f57a4157cac6a47460e
Julian Oes <julian@oes.ch>
2017-10-08 21:58:26 +0200

    core/telemetry: unify rad/deg conersions
    These functions were defined twice in different places, once for float
    and once for double.

--- plugins/telemetry/math_conversions.h
+++ plugins/telemetry/math_conversions.h
@@ -4,9 +4,6 @@
 
 namespace dronecore {
 
-float to_deg_from_rad(float rad);
-float to_rad_from_deg(float deg);
-
 Telemetry::EulerAngle to_euler_angle_from_quaternion(Telemetry::Quaternion quaternion);
 Telemetry::Quaternion to_quaternion_from_euler_angle(Telemetry::EulerAngle euler_angle);
 
