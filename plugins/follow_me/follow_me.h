922f695065cbe3044bf1461cec094df9c1bc86ec
Shakthi Prashanth M <shakthi.prashanth.m@intel.com>
2017-10-16 22:29:07 +0530

    Implementation of Follow Me mode.

---
+++ plugins/follow_me/follow_me.h
@@ -0,0 +1,24 @@
+#pragma once
+
+namespace dronecore {
+
+class FollowMeImpl;
+
+class FollowMe
+{
+public:
+    explicit FollowMe(FollowMeImpl *impl);
+    ~FollowMe();
+
+    void say_hello() const;
+
+    // Non-copyable
+    FollowMe(const FollowMe &) = delete;
+    const FollowMe &operator=(const FollowMe &) = delete;
+
+private:
+    // Underlying implementation, set at instantiation
+    FollowMeImpl *_impl;
+};
+
+} // namespace dronecore
