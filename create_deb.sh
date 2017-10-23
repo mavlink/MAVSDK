124f42d42479cc013031dfb34ccfec9ab3613058
Julian Oes <julian@oes.ch>
2017-10-23 16:41:22 -0400

    Added script to create .deb package
    This adds a script which can create a .deb package using checkinstall.

---
+++ create_deb.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+# This creates a version such as "v1.2.3-5-g123abc".
+version=`git describe --always --tags`
+# We want to extract 1.2.3 from it.
+version=`echo $version | sed 's/v\([0-9]*\.[0-9]*\.[0-9]*\).*$/\1/'`
+
+make clean
+make INSTALL_PREFIX=/usr BUILD_TYPE=Release
+
+checkinstall \
+    --maintainer="Julian Oes \<julian@oes.ch\>" \
+    --pkgname="dronecore-dev" \
+    --pkgversion="$version" \
+    --pkgarch="amd64" \
+    --pkgrelease=1 \
+    --pkglicense="BSD-3-clause" \
+    --requires="libcurl4-openssl-dev" \
+    --provides="dronecore-dev" \
+    --backup=no \
+    --deldoc=yes \
+    --deldesc=yes \
+    --default=yes
+
