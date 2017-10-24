db05110e6d83673f3995e928eef03b2ee6b859f6
Julian Oes <julian@oes.ch>
2017-10-24 13:48:53 -0400

    travis: correct fix_style make target

--- travis-docker-build.sh
+++ travis-docker-build.sh
@@ -5,7 +5,7 @@ set -e
 make BUILD_TYPE=Debug run_unit_tests -j4
 make install
 make docs
-make fix-style
+make fix_style
 (cd example/takeoff_land && mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Debug .. && make)
 (cd example/fly_mission && mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Debug .. && make)
 (cd example/offboard_velocity && mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Debug .. && make)
@@ -14,7 +14,7 @@ make clean
 make BUILD_TYPE=Release run_unit_tests -j4
 make install
 make docs
-make fix-style
+make fix_style
 (cd example/takeoff_land && mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && make)
 (cd example/fly_mission && mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && make)
 (cd example/offboard_velocity && mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && make)
