559d389f40d7b7bad881df8588296b6c94237e11
Shakthi Prashanth M <shakthi.prashanth.m@intel.com>
2017-10-27 12:11:16 +0530

    Refactored FollowMe plugin.
    1. FollowMe::MotionReport is the class that holds motion info of the GCS.
    2. Exported FollowMe::set_motion_report() method for use by example.
    3. Added FollowMe::stop()
    4. Added overloaded version: FollowMe::start(const MotionReport &mr)
    3. FollowMe::result_str() now returns std::string.
    4. FollowMe example does start FollowMe mode and stops it after 20 seconds.
    5. Fixed minor build errors.

--- plugins/follow_me/follow_me_impl.h
+++ plugins/follow_me/follow_me_impl.h
@@ -18,31 +18,23 @@ public:
     void deinit() override;
 
     FollowMe::Result start();
-    FollowMe::Result stop();
+    FollowMe::Result stop() const;
+    FollowMe::Result start(const FollowMe::MotionReport &mr);
+    void set_motion_report(const FollowMe::MotionReport &mr);
 
 private:
     void process_heartbeat(const mavlink_message_t &message);
     FollowMe::Result get_result_from_mavcmd_result(MavlinkCommands::Result result) const;
     void timeout_occurred();
+    void send_gcs_motion_report();
 
-    struct motion_report {
-        uint32_t timestamp;     // time since boot
-        int32_t lat_int;        // X Position in WGS84 frame in 1e7 * meters
-        int32_t lon_int;        // Y Position in WGS84 frame in 1e7 * meters
-        float alt;              //	Altitude in meters in AMSL altitude, not WGS84 if absolute or relative, above terrain if GLOBAL_TERRAIN_ALT_INT
-        float vx;               //	X velocity in NED frame in meter / s
-        float vy;               //	Y velocity in NED frame in meter / s
-        float vz;               //	Z velocity in NED frame in meter / s
-        float afx;              //	X acceleration in NED frame in meter / s^2 or N
-        float afy;              //	Y acceleration in NED frame in meter / s^2 or N
-        float afz;              //	Z acceleration in NED frame in meter / s^2 or N
-        float pos_std_dev[3];   // -1 for unknown
-    } _motion_report;
+    // required for emitting MotionReport updates to Vehicle
+    FollowMe::MotionReport _motion_report;
+
     TimeoutHandler _motion_report_timer;
     void *_timeout_cookie = nullptr;
     std::chrono::system_clock::time_point _start;
     uint8_t _estimatation_capabilities;
-    void send_gcs_motion_report();
 
     bool _followme_mode_active;
 };
