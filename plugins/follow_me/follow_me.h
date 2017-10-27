f061886be68bdc8cd4c483294bea45b365bfccf4
Shakthi Prashanth M <shakthi.prashanth.m@intel.com>
2017-10-27 12:11:16 +0530

    Refactored FollowMe plugin.
    1. FollowMe::MotionReport is the class that holds motion info of the GCS.
    2. Exported FollowMe::set_motion_report() method for use by example.
    3. Added FollowMe::stop()
    4. Added overloaded version: FollowMe::start(const MotionReport &mr)
    3. FollowMe::result_str() now returns std::string.
    4. FollowMe example does start FollowMe mode and stops it after 20 seconds.
    5. Fixed minor build errors.

--- plugins/follow_me/follow_me.h
+++ plugins/follow_me/follow_me.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream>
+#include <cmath>
 
 namespace dronecore {
 
@@ -36,6 +37,26 @@ public:
     };
 
     /**
+     * @brief Motion report info which will be emitted peridically to the Vehicle
+     */
+    struct MotionReport {
+        // set to home position used by PX4.
+        int32_t lat_int = 47.3977419 * 1e7;        // X Position in WGS84 frame in 1e7 * meters
+        int32_t lon_int = 8.5455938 * 1e7;        // Y Position in WGS84 frame in 1e7 * meters
+        float alt = 488.03f;              //	Altitude in meters in AMSL altitude
+        // velocity
+        float vx = cos(0.13) * 5.0;               //	X velocity in NED frame in meter / s
+        float vy = sin(0.13) * 5.0;              //	Y velocity in NED frame in meter / s
+        float vz = 0;               //	Z velocity in NED frame in meter / s
+        // acceleration
+        float afx;              //	X acceleration in NED frame in meter / s^2 or N
+        float afy;              //	Y acceleration in NED frame in meter / s^2 or N
+        float afz;              //	Z acceleration in NED frame in meter / s^2 or N
+
+        float pos_std_dev[3] = { 0.8f, 0.8f, 0.f };   // -1 for unknown
+    };
+
+    /**
      * @brief Return English string for FollowMe error codes
      *
      * @param result FollowMe::Result code
@@ -50,10 +71,22 @@ public:
     FollowMe::Result start() const;
 
     /**
+     * @brief Starts FollowMe mode with position
+     * @return Result::SUCCESS if succeeded, error otherwise. See FollowMe::Result for error codes.
+     */
+    FollowMe::Result start(const MotionReport &mr);
+
+    /**
      * @brief Stops FollowMe mode
      * @return Result::SUCCESS if succeeded, error otherwise. See FollowMe::Result for error codes.
      */
     FollowMe::Result stop() const;
+
+    /**
+     * @brief Sets position of the FollowMe mode
+     * @return none
+     */
+    void set_motion_report(const MotionReport &mr);
 
     // Non-copyable
     FollowMe(const FollowMe &) = delete;
