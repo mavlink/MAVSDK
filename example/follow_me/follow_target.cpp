1c3b696ab0a724b1bde26ef4ad2a9b82e5baaf66
Shakthi Prashanth M <shakthi.prashanth.m@intel.com>
2017-10-27 12:11:16 +0530

    Refactored FollowMe plugin.
    1. FollowMe::MotionReport is the class that holds motion info of the GCS.
    2. Exported FollowMe::set_motion_report() method for use by example.
    3. Added FollowMe::stop()
    4. Added overloaded version: FollowMe::start(const MotionReport &mr)
    3. FollowMe::result_str() now returns std::string.
    4. FollowMe example does start FollowMe mode and stops it after 20 seconds.
    5. Fixed minor build errors.

--- example/follow_me/follow_target.cpp
+++ example/follow_me/follow_target.cpp
@@ -1,38 +1,100 @@
+/**
+* @file follow_target.cpp
+* @brief Example that switches Vehicle mode to FollowMe.
+* @author Author: Shakthi Prashanth M <shakthi.prashanth.m@intel.com>
+* @date 2017-10-27
+*/
+
 #include <iostream>
 #include <cmath>
-#include <dronecore/dronecore.h>
 #include <thread>
 #include <chrono>
+#include <dronecore/dronecore.h>
 
 using namespace dronecore;
+
+using std::this_thread::sleep_for;
+using std::chrono::seconds;
+
+#define ERROR_CONSOLE_TEXT "\033[31m" //Turn text on console red
+#define TELEMETRY_CONSOLE_TEXT "\033[34m" //Turn text on console blue
+#define NORMAL_CONSOLE_TEXT "\033[0m"  //Restore normal console colour
+
+// Handles Action's result
+inline void action_error_exit(Action::Result result, const std::string &message)
+{
+    if (result != Action::Result::SUCCESS) {
+        std::cerr << ERROR_CONSOLE_TEXT << message << ": " << Action::result_str(
+                      result) << NORMAL_CONSOLE_TEXT << std::endl;
+        exit(EXIT_FAILURE);
+    }
+}
+
+// Handles FollowMe's result
+inline void followme_error_exit(FollowMe::Result result, const std::string &message)
+{
+    if (result != FollowMe::Result::SUCCESS) {
+        std::cerr << ERROR_CONSOLE_TEXT << message << ": " << FollowMe::result_str(
+                      result) << NORMAL_CONSOLE_TEXT << std::endl;
+        exit(EXIT_FAILURE);
+    }
+}
+
+// Handles connection result
+inline void connection_error_exit(DroneCore::ConnectionResult result, const std::string &message)
+{
+    if (result != DroneCore::ConnectionResult::SUCCESS) {
+        std::cerr << ERROR_CONSOLE_TEXT << message << ": "
+                  << DroneCore::connection_result_str(result)
+                  << NORMAL_CONSOLE_TEXT << std::endl;
+        exit(EXIT_FAILURE);
+    }
+}
 
 int main(int, char **)
 {
     DroneCore dc;
 
-    (void) dc.add_udp_connection();
+    // add udp connection
+    DroneCore::ConnectionResult conn_result = dc.add_udp_connection();
+    connection_error_exit(conn_result, "Connection failed");
 
-    // Wait for device to connect via heartbeat.
-    std::this_thread::sleep_for(std::chrono::seconds(2));
+    // Wait for the device to connect via heartbeat
+    while (!dc.is_connected()) {
+        std::cout << "Wait for device to connect via heartbeat" << std::endl;
+        sleep_for(seconds(1));
+    }
+
+    // Device got connected...
     Device &device = dc.device();
 
     while (!device.telemetry().health_all_ok()) {
-        std::cout << "waiting for device to be ready" << std::endl;
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::cout << "Waiting for device to be ready" << std::endl;
+        sleep_for(seconds(1));
     }
+    std::cout << "Device is ready" << std::endl;
 
-    (void) device.action().arm();
+    // Arm
+    Action::Result arm_result = device.action().arm();
+    action_error_exit(arm_result, "Arming failed");
+    std::cout << "Armed" << std::endl;
 
-    (void) device.action().takeoff();
+    // Takeoff
+    Action::Result takeoff_result = device.action().takeoff();
+    action_error_exit(takeoff_result, "Takeoff failed");
+    std::cout << "In Air..." << std::endl;
+    sleep_for(seconds(5));
 
-    std::cout << "[Takeoff] Waiting for 5 secs..." << std::endl;
-    std::this_thread::sleep_for(std::chrono::seconds(5));
-    std::cout << "Done." << std::endl;
-
-    (void) device.followme().start();
-
+    // Start FollowMe mode (by passing optional position)
+    FollowMe::Result followme_result = device.followme().start();
+    followme_error_exit(followme_result, "Failed to start FollowMe mode");
     std::cout << "[FollowMe] Waiting for 20 secs :-) ..." << std::endl;
     std::this_thread::sleep_for(std::chrono::seconds(20));
-    std::cout << "Done." << std::endl;
+
+    // Stop FollowMe mode
+    followme_result = device.followme().stop();
+    followme_error_exit(followme_result, "Failed to stop FollowMe mode");
+    std::cout << "Exiting." << std::endl;
+
     return 0;
 }
--- plugins/follow_me/follow_me.cpp
+++ plugins/follow_me/follow_me.cpp
@@ -12,15 +12,40 @@ FollowMe::~FollowMe()
 {
 }
 
-
 FollowMe::Result FollowMe::start() const
 {
     return _impl->start();
 }
 
+FollowMe::Result FollowMe::start(const MotionReport &mr)
+{
+    return _impl->start(mr);
+}
+
 FollowMe::Result FollowMe::stop() const
 {
     return _impl->stop();
+}
+
+std::string FollowMe::result_str(Result result)
+{
+    switch (result) {
+        case Result::SUCCESS:
+            return "Success";
+        case Result::NO_DEVICE:
+            return "No device";
+        case Result::CONNECTION_ERROR:
+            return "Connection error";
+        case Result::BUSY:
+            return "Busy";
+        case Result::COMMAND_DENIED:
+            return "Command denied";
+        case Result::TIMEOUT:
+            return "Timeout";
+        case Result::UNKNOWN:
+        default:
+            return "Unknown";
+    }
 }
 
 } // namespace dronecore
