b58f1f03ae17dad9134a63b933328df6de2aca96
Julian Oes <julian@oes.ch>
2017-10-30 15:30:08 -0400

    mission: add mission item download
    This adds the feature to download mission items from a vehicle. This
    currently fails if any command or waypoint is downloaded that is not
    implemented. Implemented means what can be uploaded.

--- plugins/mission/mission_impl.cpp
+++ plugins/mission/mission_impl.cpp
@@ -38,11 +38,20 @@ void MissionImpl::init()
     _parent->register_mavlink_message_handler(
         MAVLINK_MSG_ID_MISSION_ITEM_REACHED,
         std::bind(&MissionImpl::process_mission_item_reached, this, _1), this);
+
+    _parent->register_mavlink_message_handler(
+        MAVLINK_MSG_ID_MISSION_COUNT,
+        std::bind(&MissionImpl::process_mission_count, this, _1), this);
+
+    _parent->register_mavlink_message_handler(
+        MAVLINK_MSG_ID_MISSION_ITEM_INT,
+        std::bind(&MissionImpl::process_mission_item_int, this, _1), this);
 }
 
 void MissionImpl::deinit()
 {
     _parent->unregister_all_mavlink_message_handlers(this);
+    _parent->unregister_timeout_handler(_timeout_cookie);
 }
 
 void MissionImpl::process_mission_request(const mavlink_message_t &unused)
@@ -169,6 +178,59 @@ void MissionImpl::process_mission_item_reached(const mavlink_message_t &message)
     }
 }
 
+void MissionImpl::process_mission_count(const mavlink_message_t &message)
+{
+    std::lock_guard<std::mutex> lock(_mutex);
+
+    if (_activity != Activity::GET_MISSION) {
+        return;
+    }
+
+    mavlink_mission_count_t mission_count;
+    mavlink_msg_mission_count_decode(&message, &mission_count);
+
+    _num_mission_items_to_download = mission_count.count;
+    _next_mission_item_to_download = 0;
+    _parent->refresh_timeout_handler(_timeout_cookie);
+    download_next_mission_item();
+}
+
+void MissionImpl::process_mission_item_int(const mavlink_message_t &message)
+{
+    std::lock_guard<std::mutex> lock(_mutex);
+
+    auto mission_item_int_ptr = std::make_shared<mavlink_mission_item_int_t>();
+    mavlink_msg_mission_item_int_decode(&message, mission_item_int_ptr.get());
+
+    _mavlink_mission_items_downloaded.push_back(mission_item_int_ptr);
+
+    if (mission_item_int_ptr->seq == _next_mission_item_to_download) {
+        LogDebug() << "Received mission item " << _next_mission_item_to_download;
+    }
+
+    if (_next_mission_item_to_download + 1 == _num_mission_items_to_download) {
+        _parent->unregister_timeout_handler(_timeout_cookie);
+
+        mavlink_message_t ack_message;
+        mavlink_msg_mission_ack_pack(_parent->get_own_system_id(),
+                                     _parent->get_own_component_id(),
+                                     &ack_message,
+                                     _parent->get_target_system_id(),
+                                     _parent->get_target_component_id(),
+                                     MAV_MISSION_ACCEPTED,
+                                     MAV_MISSION_TYPE_MISSION);
+
+        _parent->send_message(ack_message);
+
+        assemble_mission_items();
+
+    } else {
+        ++_next_mission_item_to_download;
+        _parent->refresh_timeout_handler(_timeout_cookie);
+        download_next_mission_item();
+    }
+}
+
 void MissionImpl::upload_mission_async(const std::vector<std::shared_ptr<MissionItem>>
                                        &mission_items,
                                        const Mission::result_callback_t &callback)
@@ -190,8 +252,6 @@ void MissionImpl::upload_mission_async(const std::vector<std::shared_ptr<Mission
 
     assemble_mavlink_messages();
 
-    _activity = Activity::SET_MISSION;
-
     mavlink_message_t message;
     mavlink_msg_mission_count_pack(_parent->get_own_system_id(),
                                    _parent->get_own_component_id(),
@@ -209,9 +269,40 @@ void MissionImpl::upload_mission_async(const std::vector<std::shared_ptr<Mission
     _result_callback = callback;
 }
 
+void MissionImpl::download_mission_async(const Mission::mission_items_and_result_callback_t
+                                         &callback)
+{
+    std::lock_guard<std::mutex> lock(_mutex);
+
+    if (_activity != Activity::NONE) {
+        report_mission_items_and_result(callback, Mission::Result::BUSY);
+        return;
+    }
+
+    mavlink_message_t message;
+    mavlink_msg_mission_request_list_pack(_parent->get_own_system_id(),
+                                          _parent->get_own_component_id(),
+                                          &message,
+                                          _parent->get_target_system_id(),
+                                          _parent->get_target_component_id(),
+                                          MAV_MISSION_TYPE_MISSION);
+
+    if (!_parent->send_message(message)) {
+        report_mission_items_and_result(callback, Mission::Result::ERROR);
+        return;
+    }
+
+    _parent->register_timeout_handler(std::bind(&MissionImpl::process_timeout, this), 1.0,
+                                      &_timeout_cookie);
+
+    // Clear our internal cache and re-populate it.
+    _mavlink_mission_items_downloaded.clear();
+    _activity = Activity::GET_MISSION;
+    _mission_items_and_result_callback = callback;
+}
+
 void MissionImpl::assemble_mavlink_messages()
 {
-    // TODO: delete all entries first
     _mavlink_mission_item_messages.clear();
 
     unsigned item_i = 0;
@@ -391,6 +482,123 @@ void MissionImpl::assemble_mavlink_messages()
     }
 }
 
+void MissionImpl::assemble_mission_items()
+{
+    _mission_items.clear();
+
+    Mission::Result result = Mission::Result::SUCCESS;
+
+    auto new_mission_item = std::make_shared<MissionItem>();
+    bool have_set_position = false;
+
+    if (_mavlink_mission_items_downloaded.size() > 0) {
+        // The first mission item needs to be a waypoint with position.
+        if (_mavlink_mission_items_downloaded.at(0)->command != MAV_CMD_NAV_WAYPOINT) {
+            LogErr() << "First mission item is not a waypoint";
+            result = Mission::Result::UNSUPPORTED;
+            return;
+        }
+    }
+
+    if (_mavlink_mission_items_downloaded.size() == 0) {
+        LogErr() << "No downloaded mission items";
+        result = Mission::Result::NO_MISSION_AVAILABLE;
+        return;
+    }
+
+    for (auto &it : _mavlink_mission_items_downloaded) {
+        LogDebug() << "Assembling Message: " << int(it->seq);
+
+
+        if (it->command == MAV_CMD_NAV_WAYPOINT) {
+            if (it->frame != MAV_FRAME_GLOBAL_RELATIVE_ALT_INT) {
+                LogErr() << "Waypoint frame not supported unsupported";
+                result = Mission::Result::UNSUPPORTED;
+                break;
+            }
+
+            if (have_set_position) {
+                // When a new position comes in, create next mission item.
+                _mission_items.push_back(new_mission_item);
+                new_mission_item = std::make_shared<MissionItem>();
+                have_set_position = false;
+            }
+
+            new_mission_item->set_position(double(it->x) * 1e-7, double(it->y) * 1e-7);
+            new_mission_item->set_relative_altitude(it->z);
+
+            new_mission_item->set_fly_through(!(it->param1 > 0));
+
+            have_set_position = true;
+
+        } else if (it->command == MAV_CMD_DO_MOUNT_CONTROL) {
+            if (int(it->z) != MAV_MOUNT_MODE_MAVLINK_TARGETING) {
+                LogErr() << "Gimbal mount mode unsupported";
+                result = Mission::Result::UNSUPPORTED;
+                break;
+            }
+
+            new_mission_item->set_gimbal_pitch_and_yaw(it->param1, it->param3);
+
+        } else if (it->command == MAV_CMD_IMAGE_START_CAPTURE) {
+            if (it->param2 > 0 && int(it->param3) == 0) {
+                new_mission_item->set_camera_action(MissionItem::CameraAction::START_PHOTO_INTERVAL);
+                new_mission_item->set_camera_photo_interval(it->param2);
+            } else if (int(it->param2) == 0 && int(it->param3) == 1) {
+                new_mission_item->set_camera_action(MissionItem::CameraAction::TAKE_PHOTO);
+            } else {
+                LogErr() << "Mission item START_CAPTURE params unsupported.";
+                result = Mission::Result::UNSUPPORTED;
+                break;
+            }
+
+        } else if (it->command == MAV_CMD_IMAGE_STOP_CAPTURE) {
+            new_mission_item->set_camera_action(MissionItem::CameraAction::STOP_PHOTO_INTERVAL);
+
+        } else if (it->command == MAV_CMD_VIDEO_START_CAPTURE) {
+            new_mission_item->set_camera_action(MissionItem::CameraAction::START_VIDEO);
+
+        } else if (it->command == MAV_CMD_VIDEO_STOP_CAPTURE) {
+            new_mission_item->set_camera_action(MissionItem::CameraAction::STOP_VIDEO);
+
+        } else if (it->command == MAV_CMD_DO_CHANGE_SPEED) {
+            if (int(it->param1) == 1 && it->param3 < 0 && int(it->param4) == 0) {
+                new_mission_item->set_speed(it->param2);
+            } else {
+                LogErr() << "Mission item DO_CHANGE_SPEED params unsupported";
+                result = Mission::Result::UNSUPPORTED;
+            }
+
+        } else {
+            LogErr() << "UNSUPPORTED mission item command (" << it->command << ")";
+            result = Mission::Result::UNSUPPORTED;
+            break;
+        }
+    }
+
+    // Don't forget to add last mission item.
+    _mission_items.push_back(new_mission_item);
+
+    report_mission_items_and_result(_mission_items_and_result_callback, result);
+    _activity = Activity::NONE;
+}
+
+void MissionImpl::download_next_mission_item()
+{
+    mavlink_message_t message;
+    mavlink_msg_mission_request_int_pack(_parent->get_own_system_id(),
+                                         _parent->get_own_component_id(),
+                                         &message,
+                                         _parent->get_target_system_id(),
+                                         _parent->get_target_component_id(),
+                                         _next_mission_item_to_download,
+                                         MAV_MISSION_TYPE_MISSION);
+
+    LogDebug() << "Requested mission item " << _next_mission_item_to_download;
+
+    _parent->send_message(message);
+}
+
 void MissionImpl::start_mission_async(const Mission::result_callback_t &callback)
 {
     std::lock_guard<std::mutex> lock(_mutex);
@@ -531,6 +739,22 @@ void MissionImpl::report_mission_result(const Mission::result_callback_t &callba
     callback(result);
 }
 
+void MissionImpl::report_mission_items_and_result(const Mission::mission_items_and_result_callback_t
+                                                  &callback,
+                                                  Mission::Result result)
+{
+    if (callback == nullptr) {
+        LogWarn() << "Callback is not set";
+        return;
+    }
+
+    if (result != Mission::Result::SUCCESS) {
+        // Don't return garbage, better clear it.
+        _mission_items.clear();
+    }
+    callback(result, _mission_items);
+}
+
 void MissionImpl::report_progress()
 {
     if (_progress_callback == nullptr) {
@@ -610,6 +834,17 @@ int MissionImpl::total_mission_items() const
 void MissionImpl::subscribe_progress(Mission::progress_callback_t callback)
 {
     _progress_callback = callback;
+}
+
+void MissionImpl::process_timeout()
+{
+    std::lock_guard<std::mutex> lock(_mutex);
+
+    LogErr() << "Mission handling timed out.";
+
+    if (_activity != Activity::NONE) {
+        report_mission_result(_result_callback, Mission::Result::TIMEOUT);
+    }
 }
 
 } // namespace dronecore
