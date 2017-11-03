f001e07e3e76cff529cbad9cfab646e7ac9b52c4
Julian Oes <julian@oes.ch>
2017-11-03 14:20:31 -0400

    core: autostarting SITL not available on Windows

--- core/integration_test_helper.h
+++ core/integration_test_helper.h
@@ -4,11 +4,14 @@
 #include "global_include.h"
 #include "log.h"
 
+// Autostarting SITL is not supported on Windows
+
 class SitlTest : public testing::Test
 {
 protected:
     virtual void SetUp()
     {
+#ifndef WINDOWS
         const int ret = system("./start_px4_sitl.sh");
         if (ret != 0) {
             dronecore::LogErr() << "./start_px4_sitl.sh failed, giving up.";
@@ -16,9 +19,11 @@ protected:
         }
         // We need to wait a bit until it's up and running.
         std::this_thread::sleep_for(std::chrono::seconds(3));
+#endif
     }
     virtual void TearDown()
     {
+#ifndef WINDOWS
         // Don't rush this either.
         std::this_thread::sleep_for(std::chrono::seconds(1));
         const int ret = system("./stop_px4_sitl.sh");
@@ -26,5 +31,6 @@ protected:
             dronecore::LogErr() << "./stop_px4_sitl.sh failed, giving up.";
             abort();
         }
+#endif
     }
 };
