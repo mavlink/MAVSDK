d358271e7e2909659cae53115b44b4a9aa829490
Shakthi Prashanth <shakthi.prashanth.m@intel.com>
2017-11-30 19:41:20 +0530

    updated comments

--- generate_docs.sh
+++ generate_docs.sh
@@ -22,10 +22,9 @@ fi
 install_prefix="$source_dir/install"
 
 if [ "$skip_checks" = false ]; then
-    # We need to do a clean build, otherwise the INSTALL_PREFIX has no effect.
-    # Check for leftover install artefacts.
+    # Clean-up build & install directory
     if [ -e $source_dir/build/ ] || [ -e $install_prefix ] ; then
-	printf "Cleaup your build & install directory using below command.\nmake clean && rm -rf $install_prefix\n"
+	printf "Clean-up your build & install directory using below command.\nmake clean && rm -rf $install_prefix\n"
 	exit 1
     fi
 fi
