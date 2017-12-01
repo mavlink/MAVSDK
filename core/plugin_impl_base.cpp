e7201f3639a0f4dba615b709b186fff67ab7cbff
Julian Oes <julian@oes.ch>
2017-12-01 09:46:43 -0500

    core: fail compiling instead of runtime warnings
    Since the runtime warnings might get ignored anyway, it might make sense
    to just make it a compile error anyway, and update all the plugins.
    
    Also, the demangle function caused problems on Mac CI, so this deals
    with that.

--- core/plugin_impl_base.cpp
+++ core/plugin_impl_base.cpp
@@ -6,50 +6,11 @@
 namespace dronecore {
 
 PluginImplBase::PluginImplBase() :
-    _parent()
-{
-}
+    _parent() {}
 
 void PluginImplBase::set_parent(DeviceImpl *parent)
 {
     _parent = parent;
-}
-
-// TODO: remove this again
-#ifdef __GNUG__ // GNU C++ compiler
-#include <cxxabi.h>
-#include <stdlib.h>
-#include <string>
-std::string demangle(const char *mangled_name)
-{
-    std::string result;
-    std::size_t len = 0;
-    int status = 0;
-    char *ptr = __cxxabiv1::__cxa_demangle(mangled_name, nullptr, &len, &status);
-    if (status == 0) {
-        result = ptr;
-    } else {
-        result = "demangle error";
-    }
-    ::free(ptr);
-    return result;
-}
-#else
-#include <string>
-std::string demangle(const char *name)
-{
-    return name;
-}
-#endif // __GNUG__
-
-void PluginImplBase::enable()
-{
-    LogWarn() << "Plugin " << demangle(typeid(*this).name()) << " does not implement enable()";
-}
-
-void PluginImplBase::disable()
-{
-    LogWarn() << "Plugin " << demangle(typeid(*this).name()) << " does not implement disable()";
 }
 
 } // namespace dronecore
