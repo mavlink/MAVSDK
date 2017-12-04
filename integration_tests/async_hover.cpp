7601bee85976d240e0f1abd4fc0d8f75c445d60b
Julian Oes <julian@oes.ch>
2017-12-03 20:32:11 -0500

    integration_tests: enable two tets again

--- integration_tests/async_hover.cpp
+++ integration_tests/async_hover.cpp
@@ -2,6 +2,8 @@
 #include "integration_test_helper.h"
 #include "global_include.h"
 #include "dronecore.h"
+#include "plugins/action/action.h"
+#include "plugins/telemetry/telemetry.h"
 
 using namespace dronecore;
 using namespace std::placeholders; // for `_1`
@@ -26,30 +28,33 @@ TEST_F(SitlTest, ActionAsyncHover)
     // TODO: this test is pretty dumb, should be improved with more checks.
     Device &device = dc.device();
 
-    device.telemetry().health_all_ok_async(std::bind(&receive_health_all_ok, _1));
-    device.telemetry().in_air_async(std::bind(&receive_in_air, _1));
+    auto telemetry = std::make_shared<Telemetry>(&device);
+    telemetry->health_all_ok_async(std::bind(&receive_health_all_ok, _1));
+    telemetry->in_air_async(std::bind(&receive_in_air, _1));
+
+    auto action = std::make_shared<Action>(&device);
 
     while (!_all_ok) {
         std::cout << "Waiting to be ready..." << std::endl;
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 
-    device.action().arm_async(std::bind(&receive_result, _1));
+    action->arm_async(std::bind(&receive_result, _1));
     std::this_thread::sleep_for(std::chrono::seconds(2));
 
-    device.action().set_takeoff_altitude(5.0f);
+    action->set_takeoff_altitude(5.0f);
 
-    device.action().takeoff_async(std::bind(&receive_result, _1));
+    action->takeoff_async(std::bind(&receive_result, _1));
     std::this_thread::sleep_for(std::chrono::seconds(5));
 
-    device.action().land_async(std::bind(&receive_result, _1));
+    action->land_async(std::bind(&receive_result, _1));
 
     while (_in_air) {
         std::cout << "Waiting to be landed..." << std::endl;
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 
-    device.action().disarm_async(std::bind(&receive_result, _1));
+    action->disarm_async(std::bind(&receive_result, _1));
     std::this_thread::sleep_for(std::chrono::seconds(2));
 }
 
--- integration_tests/telemetry_simple.cpp
+++ integration_tests/telemetry_simple.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "integration_test_helper.h"
 #include "dronecore.h"
+#include "plugins/telemetry/telemetry.h"
 
 using namespace dronecore;
 
@@ -14,30 +15,32 @@ TEST_F(SitlTest, TelemetrySimple)
     std::this_thread::sleep_for(std::chrono::seconds(2));
     Device &device = dc.device();
 
-    while (!device.telemetry().health_all_ok()) {
+    auto telemetry = std::make_shared<Telemetry>(&device);
+
+    while (!telemetry->health_all_ok()) {
         std::cout << "waiting for device to be ready" << std::endl;
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 
-    Telemetry::Result result = device.telemetry().set_rate_position(10.0);
+    Telemetry::Result result = telemetry->set_rate_position(10.0);
     ASSERT_EQ(result, Telemetry::Result::SUCCESS);
-    result = device.telemetry().set_rate_home_position(10.0);
+    result = telemetry->set_rate_home_position(10.0);
     ASSERT_EQ(result, Telemetry::Result::SUCCESS);
-    result = device.telemetry().set_rate_in_air(10.0);
+    result = telemetry->set_rate_in_air(10.0);
     ASSERT_EQ(result, Telemetry::Result::SUCCESS);
-    result = device.telemetry().set_rate_attitude(10.0);
+    result = telemetry->set_rate_attitude(10.0);
     ASSERT_EQ(result, Telemetry::Result::SUCCESS);
-    result = device.telemetry().set_rate_ground_speed_ned(10.0);
+    result = telemetry->set_rate_ground_speed_ned(10.0);
     ASSERT_EQ(result, Telemetry::Result::SUCCESS);
-    result = device.telemetry().set_rate_gps_info(10.0);
+    result = telemetry->set_rate_gps_info(10.0);
     ASSERT_EQ(result, Telemetry::Result::SUCCESS);
-    result = device.telemetry().set_rate_battery(10.0);
+    result = telemetry->set_rate_battery(10.0);
     ASSERT_EQ(result, Telemetry::Result::SUCCESS);
 
     // Print 3s of telemetry.
     for (unsigned i = 0; i < 50; ++i) {
 
-        const Telemetry::Position &position = device.telemetry().position();
+        const Telemetry::Position &position = telemetry->position();
         std::cout << "Position: " << std::endl
                   << "Absolute altitude: " << position.absolute_altitude_m << " m" << std::endl
                   << "Relative altitude: " << position.relative_altitude_m << " m" << std::endl
@@ -46,7 +49,7 @@ TEST_F(SitlTest, TelemetrySimple)
                   << ", longitude: "
                   << position.longitude_deg << " deg" << std::endl;
 
-        const Telemetry::Position &home_position = device.telemetry().home_position();
+        const Telemetry::Position &home_position = telemetry->home_position();
         std::cout << "Home position: " << std::endl
                   << "Absolute altitude: " << home_position.absolute_altitude_m
                   << " m" << std::endl
@@ -57,11 +60,11 @@ TEST_F(SitlTest, TelemetrySimple)
                   << ", longitude: "
                   << home_position.longitude_deg << " deg" << std::endl;
 
-        std::cout << (device.telemetry().in_air() ? "In-air" : "On-ground")
+        std::cout << (telemetry->in_air() ? "In-air" : "On-ground")
                   << std::endl;
 
         const Telemetry::Quaternion &quaternion
-            = device.telemetry().attitude_quaternion();
+            = telemetry->attitude_quaternion();
         std::cout << "Quaternion: ("
                   << quaternion.w << ", "
                   << quaternion.x << ","
@@ -69,22 +72,22 @@ TEST_F(SitlTest, TelemetrySimple)
                   << quaternion.z << ")" << std::endl;
 
         const Telemetry::EulerAngle &euler_angle
-            = device.telemetry().attitude_euler_angle();
+            = telemetry->attitude_euler_angle();
         std::cout << "Euler: (" << euler_angle.roll_deg << " deg, "
                   << euler_angle.pitch_deg << " deg,"
                   << euler_angle.yaw_deg << " deg)" << std::endl;
 
         const Telemetry::GroundSpeedNED &ground_speed_ned
-            = device.telemetry().ground_speed_ned();
+            = telemetry->ground_speed_ned();
         std::cout << "Speed: (" << ground_speed_ned.velocity_north_m_s << " m/s, "
                   << ground_speed_ned.velocity_east_m_s << " m/s,"
                   << ground_speed_ned.velocity_down_m_s << " m/s)" << std::endl;
 
-        const Telemetry::GPSInfo &gps_info = device.telemetry().gps_info();
+        const Telemetry::GPSInfo &gps_info = telemetry->gps_info();
         std::cout << "GPS sats: " << gps_info.num_satellites
                   << ", fix type: " << gps_info.fix_type << std::endl;
 
-        const Telemetry::Battery &battery = device.telemetry().battery();
+        const Telemetry::Battery &battery = telemetry->battery();
         std::cout << "Battery voltage: " << battery.voltage_v << " v, "
                   << "remaining: " << battery.remaining_percent * 100.0f << " %" << std::endl;
 
