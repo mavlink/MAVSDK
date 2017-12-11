d32a260a035b2df597031a194baa5be41bce8070
Julian Oes <julian@oes.ch>
2017-12-11 14:22:44 -0500

    create_deb.sh: use fpm instead of checkinstall
    This will allow us to have separate packages for the library and
    executables.

--- create_deb.sh
+++ create_deb.sh
@@ -8,19 +8,44 @@ version=`git describe --always --tags`
 version=`echo $version | sed 's/v\([0-9]*\.[0-9]*\.[0-9]*\).*$/\1/'`
 
 make clean
-make INSTALL_PREFIX=/usr BUILD_TYPE=Release
+make BUILD_TYPE=Release
 
-sudo checkinstall \
-    --maintainer="Julian Oes \<julian@oes.ch\>" \
-    --pkgname="dronecore-dev" \
-    --pkgversion="$version" \
-    --pkgarch="amd64" \
-    --pkgrelease=1 \
-    --pkglicense="BSD-3-clause" \
-    --requires="libcurl4-openssl-dev,libtinyxml2-dev" \
-    --provides="dronecore-dev" \
-    --backup=no \
-    --deldoc=yes \
-    --deldesc=yes \
-    --default make default install
+common_args="--input-type dir  \
+    --version $version \
+    --maintainer julian@oes.ch \
+    --url http://dronecore.io \
+    --license BSD-3-clause \
+    --force"
+
+files="\
+    build/default/include/device_plugin_container.h=/usr/include/dronecore/device_plugin_container.h \
+    include/device.h=/usr/include/dronecore/device.h \
+    include/dronecore.h=/usr/include/dronecore/dronecore.h \
+    build/default/libdronecore.so=/usr/lib/libdronecore.so \
+    plugins/action/action.h=/usr/include/dronecore/action.h \
+    plugins/follow_me/follow_me.h=/usr/include/dronecore/follow_me.h \
+    plugins/gimbal/gimbal.h=/usr/include/dronecore/gimbal.h \
+    plugins/info/info.h=/usr/include/dronecore/info.h \
+    plugins/logging/logging.h=/usr/include/dronecore/logging.h \
+    plugins/mission/mission.h=/usr/include/dronecore/mission.h \
+    plugins/mission/mission_item.h=/usr/include/dronecore/mission_item.h \
+    plugins/offboard/offboard.h=/usr/include/dronecore/offboard.h \
+    plugins/telemetry/telemetry.h=/usr/include/dronecore/telemetry.h"
+
+fpm $common_args \
+    --output-type deb \
+    --depends libtinyxml2-dev \
+    --depends libcurl4-openssl-dev \
+    --deb-no-default-config-files \
+    --name libdronecore-dev \
+    --provides libdronecore-dev \
+    $files
+
+fpm $common_args \
+    --output-type rpm \
+    --depends libtinyxml2-devel \
+    --depends libcurl-devel \
+    --name libdronecore-devel \
+    --provides libdronecore-devel \
+    $files
 
