a37e0d97ea5058926fcfc679e2cf5426b27a2160
Shakthi Prashanth M <shakthi.prashanth.m@intel.com>
2018-01-05 13:13:06 +0530

    Add `FakeLocationProvider` class for Follow Me example
    This change uses Boost APIs for registering with `FakeLocationProvider`.
    Removed location generator logic in example.
    Also, adds stop Follow Me before landing.

---
+++ example/follow_me/fake_location_provider.cpp
@@ -0,0 +1,60 @@
+
+#include "fake_location_provider.h"
+
+void FakeLocationProvider::request_location_updates(location_callback_t callback)
+{
+    location_callback_ = callback;
+    timer_.async_wait(boost::bind(&FakeLocationProvider::compute_next_location, this));
+}
+
+// Rudimentary location provider whose successive lat, lon combination
+// makes Drone revolve in a semi-circular path.
+void FakeLocationProvider::compute_next_location()
+{
+    if (count_++ < 10) {
+        location_callback_(latitude_deg_, longitude_deg_);
+        latitude_deg_ -= LATITUDE_DEG_PER_METER * 4;
+        timer_.expires_at(timer_.expires_at() + boost::posix_time::seconds(1));
+        timer_.async_wait(boost::bind(&FakeLocationProvider::compute_next_location, this));
+        sleep(1);
+    }
+    if (count_++ < 20) {
+        location_callback_(latitude_deg_, longitude_deg_);
+        longitude_deg_ += LONGITUDE_DEG_PER_METER * 4;
+        timer_.expires_at(timer_.expires_at() + boost::posix_time::seconds(1));
+        timer_.async_wait(boost::bind(&FakeLocationProvider::compute_next_location, this));
+        sleep(1);
+    }
+    if (count_++ < 30) {
+        location_callback_(latitude_deg_, longitude_deg_);
+        latitude_deg_ += LATITUDE_DEG_PER_METER * 4;
+        timer_.expires_at(timer_.expires_at() + boost::posix_time::seconds(1));
+        timer_.async_wait(boost::bind(&FakeLocationProvider::compute_next_location, this));
+        sleep(1);
+    }
+    if (count_++ < 40) {
+        location_callback_(latitude_deg_, longitude_deg_);
+        longitude_deg_ -= LONGITUDE_DEG_PER_METER * 4;
+        timer_.expires_at(timer_.expires_at() + boost::posix_time::seconds(1));
+        timer_.async_wait(boost::bind(&FakeLocationProvider::compute_next_location, this));
+        sleep(1);
+    }
+    if (count_++ < 50) {
+        location_callback_(latitude_deg_, longitude_deg_);
+        latitude_deg_ -= LATITUDE_DEG_PER_METER * 3;
+        timer_.expires_at(timer_.expires_at() + boost::posix_time::seconds(1));
+        timer_.async_wait(boost::bind(&FakeLocationProvider::compute_next_location, this));
+        sleep(1);
+    }
+    if (count_++ < MAX_LOCATIONS) {
+        location_callback_(latitude_deg_, longitude_deg_);
+        longitude_deg_ += LONGITUDE_DEG_PER_METER * 3;
+        timer_.expires_at(timer_.expires_at() + boost::posix_time::seconds(1));
+        timer_.async_wait(boost::bind(&FakeLocationProvider::compute_next_location, this));
+        sleep(1);
+    }
+}
+
+const size_t FakeLocationProvider::MAX_LOCATIONS = 60u;
+const double FakeLocationProvider::LATITUDE_DEG_PER_METER = 0.000009044;
+const double FakeLocationProvider::LONGITUDE_DEG_PER_METER = 0.000008985;
