4e69398af7f57a188c24799554e73905ed1388e1
Julian Oes <julian@oes.ch>
2018-02-05 11:48:21 +0100

    zlib: omit installing library

--- cmake/zlib.cmake
+++ cmake/zlib.cmake
@@ -1,5 +1,7 @@
 set(ZLIB_ROOT_DIR ${CMAKE_SOURCE_DIR}/third_party/zlib)
 
+set(SKIP_INSTALL_ALL "true")
+
 include_directories("${ZLIB_ROOT_DIR}")
 add_subdirectory(${ZLIB_ROOT_DIR} third_party/zlib)
 
