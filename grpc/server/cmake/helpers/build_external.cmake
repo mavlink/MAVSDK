ce14c2fc9c2ad8ad542329c8cc72b651941f29ab
Jonas Vautherin <jonas.vautherin@gmail.com>
2018-02-06 01:15:38 +0100

    build_system: build dronecore_server (including grpc) for iOS and Android
    * needed to fork gRPC for now (in http://github.com/dronecore/grpc)
    * added boringssl as a dependency to not rely on the system openssl libs
    * plugins need to be built as static libraries for iOS
    * removing support for android mips and mips64

--- grpc/server/cmake/helpers/build_external.cmake
+++ grpc/server/cmake/helpers/build_external.cmake
@@ -1,6 +1,7 @@
-set(DRONECORESERVER_EXTERNAL_SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/cmake/external")
-set(DRONECORESERVER_EXTERNAL_BINARY_DIR "${CMAKE_BINARY_DIR}/external")
+set(DRONECORESERVER_EXTERNAL_SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/cmake/third_party")
+set(DRONECORESERVER_EXTERNAL_BINARY_DIR "${CMAKE_BINARY_DIR}/third_party")
 
+include(cmake/helpers/build_boringssl.cmake)
 include(cmake/helpers/build_protobuf.cmake)
 include(cmake/helpers/build_cares.cmake)
 include(cmake/helpers/build_grpc.cmake)
