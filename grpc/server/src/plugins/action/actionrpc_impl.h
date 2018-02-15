a987181f75847d1520829e3da3f01af2e7dc91d2
Jonas Vautherin <jonas.vautherin@gmail.com>
2018-02-15 12:53:14 +0100

    backend: update action proto file
    * remove 'using namespace' from header
    * create implementation file for actionrpc_impl
    * update action proto file and change actionrpc_impl accordingly
    
    Fixes #238.

--- grpc/server/src/plugins/action/actionrpc_impl.h
+++ grpc/server/src/plugins/action/actionrpc_impl.h
@@ -1,44 +1,24 @@
 #include "action.h"
 #include "action/action.grpc.pb.h"
 
-using grpc::Status;
-using grpc::ServerContext;
-
-using namespace dronecore;
-
-class ActionServiceImpl final : public rpc::action::ActionService::Service
+class ActionServiceImpl final : public dronecore::rpc::action::ActionService::Service
 {
 public:
-    ActionServiceImpl(const Action &action)
+    ActionServiceImpl(const dronecore::Action &action)
         : action(action) {}
 
-    Status Arm(ServerContext *context, const rpc::action::ArmRequest *request,
-               rpc::action::ActionResult *response) override
-    {
-        const Action::Result action_result = action.arm();
-        response->set_result(static_cast<rpc::action::ActionResult::Result>(action_result));
-        response->set_result_str(Action::result_str(action_result));
-        return Status::OK;
-    }
+    grpc::Status Arm(grpc::ServerContext *context,
+                     const dronecore::rpc::action::ArmRequest *request,
+                     dronecore::rpc::action::ArmResponse *response) override;
 
-    Status TakeOff(ServerContext *context, const rpc::action::TakeOffRequest *request,
-                   rpc::action::ActionResult *response) override
-    {
-        const Action::Result action_result = action.takeoff();
-        response->set_result(static_cast<rpc::action::ActionResult::Result>(action_result));
-        response->set_result_str(Action::result_str(action_result));
-        return Status::OK;
-    }
+    grpc::Status Takeoff(grpc::ServerContext *context,
+                         const dronecore::rpc::action::TakeoffRequest *request,
+                         dronecore::rpc::action::TakeoffResponse *response) override;
 
-    Status Land(ServerContext *context, const rpc::action::LandRequest *request,
-                rpc::action::ActionResult *response) override
-    {
-        const Action::Result action_result = action.land();
-        response->set_result(static_cast<rpc::action::ActionResult::Result>(action_result));
-        response->set_result_str(Action::result_str(action_result));
-        return Status::OK;
-    }
+    grpc::Status Land(grpc::ServerContext *context,
+                      const dronecore::rpc::action::LandRequest *request,
+                      dronecore::rpc::action::LandResponse *response) override;
 
 private:
-    const Action &action;
+    const dronecore::Action &action;
 };
