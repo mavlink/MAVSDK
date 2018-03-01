4b3ba5147c2d7c2d99da9b9cefc85c556efbb8e7
Jonas Vautherin <jonas.vautherin@gmail.com>
2018-03-01 15:39:23 +0100

    backend: make a pImpl out of DroneCoreBackend

--- grpc/server/src/backend.h
+++ grpc/server/src/backend.h
@@ -1,9 +1,4 @@
-#include <grpc++/server.h>
 #include <memory>
-#include <mutex>
-
-#include "connection_initiator.h"
-#include "dronecore.h"
 
 namespace dronecore {
 namespace backend {
@@ -11,18 +6,17 @@ namespace backend {
 class DroneCoreBackend
 {
 public:
-    DroneCoreBackend() {}
-    ~DroneCoreBackend() {}
+    DroneCoreBackend();
+    ~DroneCoreBackend();
+
+    DroneCoreBackend(DroneCoreBackend &&op);
+    DroneCoreBackend &operator=(DroneCoreBackend &&op);
 
     bool run(const int mavlink_listen_port = 14540);
 
 private:
-    bool run_server();
-    void setup_port(grpc::ServerBuilder &builder);
-
-    dronecore::DroneCore _dc;
-    dronecore::backend::ConnectionInitiator<dronecore::DroneCore> _connection_initiator;
-    std::unique_ptr<grpc::Server> _server;
+    class Impl;
+    std::unique_ptr<Impl> _impl;
 };
 
 } // namespace backend
