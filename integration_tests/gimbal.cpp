5bfc97e6ea6271a7302c782e4262d104e43ba4df
Beat Küng <beat-kueng@gmx.net>
2018-03-15 20:44:00 +0100

    gimbal integration tests: add GimbalROIOffboard
    This sets a region of interest for the gimbal, and then uses offboard
    velocity control to move the drone around it.

--- integration_tests/gimbal.cpp
+++ integration_tests/gimbal.cpp
@@ -8,12 +8,15 @@
 #include "plugins/telemetry/telemetry.h"
 #include "plugins/action/action.h"
 #include "plugins/gimbal/gimbal.h"
+#include "plugins/offboard/offboard.h"
 
 using namespace std::placeholders; // for `_1`
 using namespace dronecore;
 
 
 void send_new_gimbal_command(std::shared_ptr<Gimbal> gimbal, int i);
+void send_gimbal_roi_location(std::shared_ptr<Gimbal> gimbal, double latitude_deg,
+                              double longitude_deg, float altitude_m);
 void receive_gimbal_result(Gimbal::Result result);
 void receive_gimbal_attitude_euler_angles(Telemetry::EulerAngle euler_angle);
 
@@ -82,6 +85,78 @@ TEST_F(SitlTest, GimbalTakeoffAndMove)
     std::this_thread::sleep_for(std::chrono::seconds(3));
 }
 
+TEST_F(SitlTest, GimbalROIOffboard)
+{
+    DroneCore dc;
+
+    ConnectionResult ret = dc.add_udp_connection();
+    ASSERT_EQ(ret, ConnectionResult::SUCCESS);
+
+    // Wait for device to connect via heartbeat.
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+
+    Device &device = dc.device();
+    auto telemetry = std::make_shared<Telemetry>(device);
+    auto gimbal = std::make_shared<Gimbal>(device);
+    auto action = std::make_shared<Action>(device);
+    auto offboard = std::make_shared<Offboard>(device);
+
+    while (!telemetry->health_all_ok()) {
+        LogInfo() << "waiting for device to be ready";
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+
+    const Telemetry::Position &position = telemetry->position();
+
+    // set the ROI location: a bit to the north of the vehicle's location
+    const double latitude_offset_deg = 3. / 111111.; // this is about 3 m
+    send_gimbal_roi_location(gimbal, position.latitude_deg + latitude_offset_deg,
+                             position.longitude_deg, position.absolute_altitude_m + 1.f);
+
+    action->arm();
+    action->takeoff();
+
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+
+    telemetry->set_rate_camera_attitude(10.0);
+
+    telemetry->camera_attitude_euler_angle_async(
+        std::bind(&receive_gimbal_attitude_euler_angles, _1));
+
+    // Send it once before starting offboard, otherwise it will be rejected.
+    offboard->set_velocity_ned({0.0f, 0.0f, 0.0f, 0.0f});
+    offboard->start();
+
+    auto fly_straight = [&offboard](int step_count, float max_speed) {
+        for (int i = 0; i < step_count; ++i) {
+            int k = (i <= step_count / 2) ? i : step_count - i;
+            float vy = static_cast<float>(k) / (step_count / 2) * max_speed;
+            offboard->set_velocity_ned({0.0f, vy, 0.0f, 90.0f});
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        }
+    };
+
+    // fly east for a bit
+    fly_straight(300, 2.f);
+
+    // fly in north-south direction (back & forth a few times)
+    const float step_size = 0.01f;
+    const float one_cycle = 2.0f * M_PI_F;
+    const unsigned steps = 2.5f * one_cycle / step_size;
+
+    for (unsigned i = 0; i < steps; ++i) {
+        float vx = 5.0f * sinf(i * step_size);
+        offboard->set_velocity_ned({vx, 0.0f, 0.0f, 90.0f});
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+
+    // fly west for a bit
+    fly_straight(600, -4.f);
+
+    action->land();
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+}
+
 void send_new_gimbal_command(std::shared_ptr<Gimbal> gimbal, int i)
 {
     float pitch_deg = 30.0f * cosf(i / 360.0f * 2 * M_PI_F);
@@ -89,6 +164,13 @@ void send_new_gimbal_command(std::shared_ptr<Gimbal> gimbal, int i)
 
     gimbal->set_pitch_and_yaw_async(pitch_deg, yaw_deg,
                                     std::bind(&receive_gimbal_result, _1));
+}
+
+void send_gimbal_roi_location(std::shared_ptr<Gimbal> gimbal, double latitude_deg,
+                              double longitude_deg, float altitude_m)
+{
+    gimbal->set_roi_location_async(latitude_deg, longitude_deg, altitude_m,
+                                   std::bind(&receive_gimbal_result, _1));
 }
 
 void receive_gimbal_result(Gimbal::Result result)
