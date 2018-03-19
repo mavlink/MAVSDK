bd9d44a4f6d78fe1cd0ab27620661afb4fbc9bef
Shakthi Prashanth M <shakthi.prashanth.m@intel.com>
2018-03-19 16:32:28 +0530

    backend: Replace class Device -> System

--- backend/test/connection_initiator_test.cpp
+++ backend/test/connection_initiator_test.cpp
@@ -39,7 +39,7 @@ TEST(ConnectionInitiator, startAddsUDPConnection)
     initiator.start(dc, ARBITRARY_PORT);
 }
 
-TEST(ConnectionInitiator, startHangsUntilDeviceDiscovered)
+TEST(ConnectionInitiator, startHangsUntilSystemDiscovered)
 {
     ConnectionInitiator initiator;
     MockDroneCore dc;
