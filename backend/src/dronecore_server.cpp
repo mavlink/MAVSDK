5f00f155843e6f638fa90d1a605faffb74f193f2
Jonas Vautherin <jonas.vautherin@gmail.com>
2018-04-06 22:43:14 +0200

    backend: improve backend api and initialization

--- backend/src/dronecore_server.cpp
+++ backend/src/dronecore_server.cpp
@@ -1,12 +1,6 @@
-#include "backend.h"
+#include "backend_api.h"
 
 int main(int argc, char **argv)
 {
-    dronecore::backend::DroneCoreBackend backend;
-
-    if (!backend.run()) {
-        return 1;
-    } else {
-        return 0;
-    }
+    runBackend(14540, nullptr, nullptr);
 }
