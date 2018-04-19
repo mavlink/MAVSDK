eaf1cf532cc464e18a73ddd39217e00f8b5e26de
Julian Oes <julian@oes.ch>
2018-04-19 14:12:51 -0400

    camera: don't set mode if already correct
    The E90 camera doesn't like a mode to be set to what it's already set
    to.

--- integration_tests/camera_test_helpers.cpp
+++ integration_tests/camera_test_helpers.cpp
@@ -40,8 +40,14 @@ Camera::Mode get_mode(std::shared_ptr<Camera> camera)
 
 void set_mode(std::shared_ptr<Camera> camera, Camera::Mode mode)
 {
+    Camera::Mode mode_already_set = get_mode(camera);
+
     // FIXME: this should not be required.
     std::this_thread::sleep_for(std::chrono::seconds(2));
+
+    if (mode == mode_already_set) {
+        return;
+    }
 
     auto prom = std::make_shared<std::promise<void>>();
     auto ret = prom->get_future();
