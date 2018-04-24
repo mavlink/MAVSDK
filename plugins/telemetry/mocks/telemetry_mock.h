768488926f3d95014afedf9035e9446e3b630a7c
Jonas Vautherin <jonas.vautherin@gmail.com>
2018-04-24 12:42:26 +0200

    backend: add armed to telemetry

--- plugins/telemetry/mocks/telemetry_mock.h
+++ plugins/telemetry/mocks/telemetry_mock.h
@@ -12,6 +12,7 @@ public:
     MOCK_CONST_METHOD1(health_async, void(Telemetry::health_callback_t));
     MOCK_CONST_METHOD1(home_position_async, void(Telemetry::position_callback_t));
     MOCK_CONST_METHOD1(in_air_async, void(Telemetry::in_air_callback_t));
+    MOCK_CONST_METHOD1(armed_async, void(Telemetry::armed_callback_t));
 };
 
 } // namespace testing
