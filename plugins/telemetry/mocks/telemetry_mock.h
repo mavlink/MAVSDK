068181364ca1c79401da08fddb7617809bf499fc
Jonas Vautherin <jonas.vautherin@gmail.com>
2018-04-28 20:20:00 +0200

    backend: add flightmode to telemetry

--- plugins/telemetry/mocks/telemetry_mock.h
+++ plugins/telemetry/mocks/telemetry_mock.h
@@ -15,6 +15,7 @@ public:
     MOCK_CONST_METHOD1(armed_async, void(Telemetry::armed_callback_t));
     MOCK_CONST_METHOD1(gps_info_async, void(Telemetry::gps_info_callback_t));
     MOCK_CONST_METHOD1(battery_async, void(Telemetry::battery_callback_t));
+    MOCK_CONST_METHOD1(flight_mode_async, void(Telemetry::flight_mode_callback_t));
 };
 
 } // namespace testing
