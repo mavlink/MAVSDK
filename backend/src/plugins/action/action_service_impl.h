d8965de2f4d98a4858c3d235bb4274896f2fe8db
Jonas Vautherin <jonas.vautherin@gmail.com>
2018-04-30 21:21:56 +0200

    backend: remove unnecessary explicit namespacing in action

--- backend/src/plugins/action/action_service_impl.h
+++ backend/src/plugins/action/action_service_impl.h
@@ -5,15 +5,15 @@ namespace dronecore {
 namespace backend {
 
 template <typename Action = Action>
-class ActionServiceImpl final : public dronecore::rpc::action::ActionService::Service
+class ActionServiceImpl final : public rpc::action::ActionService::Service
 {
 public:
     ActionServiceImpl(Action &action)
         : _action(action) {}
 
     grpc::Status Arm(grpc::ServerContext * /* context */,
-                     const dronecore::rpc::action::ArmRequest * /* request */,
-                     dronecore::rpc::action::ArmResponse *response) override
+                     const rpc::action::ArmRequest * /* request */,
+                     rpc::action::ArmResponse *response) override
     {
         auto action_result = _action.arm();
 
@@ -27,18 +27,18 @@ public:
     template <typename ResponseType>
     void fillResponseWithResult(ResponseType *response, ActionResult &action_result) const
     {
-        auto rpc_result = static_cast<dronecore::rpc::action::ActionResult::Result>(action_result);
+        auto rpc_result = static_cast<rpc::action::ActionResult::Result>(action_result);
 
-        auto *rpc_action_result = new dronecore::rpc::action::ActionResult();
+        auto *rpc_action_result = new rpc::action::ActionResult();
         rpc_action_result->set_result(rpc_result);
-        rpc_action_result->set_result_str(dronecore::action_result_str(action_result));
+        rpc_action_result->set_result_str(action_result_str(action_result));
 
         response->set_allocated_action_result(rpc_action_result);
     }
 
     grpc::Status Disarm(grpc::ServerContext * /* context */,
-                        const dronecore::rpc::action::DisarmRequest * /* request */,
-                        dronecore::rpc::action::DisarmResponse *response) override
+                        const rpc::action::DisarmRequest * /* request */,
+                        rpc::action::DisarmResponse *response) override
     {
         auto action_result = _action.disarm();
 
@@ -50,8 +50,8 @@ public:
     }
 
     grpc::Status Takeoff(grpc::ServerContext * /* context */,
-                         const dronecore::rpc::action::TakeoffRequest * /* request */,
-                         dronecore::rpc::action::TakeoffResponse *response) override
+                         const rpc::action::TakeoffRequest * /* request */,
+                         rpc::action::TakeoffResponse *response) override
     {
         auto action_result = _action.takeoff();
 
@@ -63,8 +63,8 @@ public:
     }
 
     grpc::Status Land(grpc::ServerContext * /* context */,
-                      const dronecore::rpc::action::LandRequest * /* request */,
-                      dronecore::rpc::action::LandResponse *response) override
+                      const rpc::action::LandRequest * /* request */,
+                      rpc::action::LandResponse *response) override
     {
         auto action_result = _action.land();
 
@@ -76,8 +76,8 @@ public:
     }
 
     grpc::Status Kill(grpc::ServerContext * /* context */,
-                      const dronecore::rpc::action::KillRequest * /* request */,
-                      dronecore::rpc::action::KillResponse *response) override
+                      const rpc::action::KillRequest * /* request */,
+                      rpc::action::KillResponse *response) override
     {
         auto action_result = _action.kill();
 
@@ -89,8 +89,8 @@ public:
     }
 
     grpc::Status ReturnToLaunch(grpc::ServerContext * /* context */,
-                                const dronecore::rpc::action::ReturnToLaunchRequest * /* request */,
-                                dronecore::rpc::action::ReturnToLaunchResponse *response) override
+                                const rpc::action::ReturnToLaunchRequest * /* request */,
+                                rpc::action::ReturnToLaunchResponse *response) override
     {
         auto action_result = _action.return_to_launch();
 
@@ -102,8 +102,8 @@ public:
     }
 
     grpc::Status TransitionToFixedWings(grpc::ServerContext * /* context */,
-                                        const dronecore::rpc::action::TransitionToFixedWingsRequest * /* request */,
-                                        dronecore::rpc::action::TransitionToFixedWingsResponse *response) override
+                                        const rpc::action::TransitionToFixedWingsRequest * /* request */,
+                                        rpc::action::TransitionToFixedWingsResponse *response) override
     {
         auto action_result = _action.transition_to_fixedwing();
 
@@ -115,8 +115,8 @@ public:
     }
 
     grpc::Status TransitionToMulticopter(grpc::ServerContext * /* context */,
-                                         const dronecore::rpc::action::TransitionToMulticopterRequest * /* request */,
-                                         dronecore::rpc::action::TransitionToMulticopterResponse *response) override
+                                         const rpc::action::TransitionToMulticopterRequest * /* request */,
+                                         rpc::action::TransitionToMulticopterResponse *response) override
     {
         auto action_result = _action.transition_to_multicopter();
 
@@ -128,8 +128,8 @@ public:
     }
 
     grpc::Status GetTakeoffAltitude(grpc::ServerContext * /* context */,
-                                    const dronecore::rpc::action::GetTakeoffAltitudeRequest * /* request */,
-                                    dronecore::rpc::action::GetTakeoffAltitudeResponse *response) override
+                                    const rpc::action::GetTakeoffAltitudeRequest * /* request */,
+                                    rpc::action::GetTakeoffAltitudeResponse *response) override
     {
         if (response != nullptr) {
             auto takeoff_altitude = _action.get_takeoff_altitude_m();
@@ -140,8 +140,8 @@ public:
     }
 
     grpc::Status SetTakeoffAltitude(grpc::ServerContext * /* context */,
-                                    const dronecore::rpc::action::SetTakeoffAltitudeRequest *request,
-                                    dronecore::rpc::action::SetTakeoffAltitudeResponse * /* response */) override
+                                    const rpc::action::SetTakeoffAltitudeRequest *request,
+                                    rpc::action::SetTakeoffAltitudeResponse * /* response */) override
     {
         if (request != nullptr) {
             const auto requested_altitude = request->altitude_m();
@@ -152,8 +152,8 @@ public:
     }
 
     grpc::Status GetMaximumSpeed(grpc::ServerContext * /* context */,
-                                 const dronecore::rpc::action::GetMaximumSpeedRequest * /* request */,
-                                 dronecore::rpc::action::GetMaximumSpeedResponse *response) override
+                                 const rpc::action::GetMaximumSpeedRequest * /* request */,
+                                 rpc::action::GetMaximumSpeedResponse *response) override
     {
         if (response != nullptr) {
             auto max_speed = _action.get_max_speed_m_s();
@@ -164,8 +164,8 @@ public:
     }
 
     grpc::Status SetMaximumSpeed(grpc::ServerContext * /* context */,
-                                 const dronecore::rpc::action::SetMaximumSpeedRequest *request,
-                                 dronecore::rpc::action::SetMaximumSpeedResponse * /* response */) override
+                                 const rpc::action::SetMaximumSpeedRequest *request,
+                                 rpc::action::SetMaximumSpeedResponse * /* response */) override
     {
         if (request != nullptr) {
             const auto requested_speed = request->speed_m_s();
