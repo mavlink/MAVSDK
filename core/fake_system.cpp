218074b8d15265f3c75943b7bf5ccb336fe367ee
Julian Oes <julian@oes.ch>
2018-05-21 17:01:13 -0400

    fake_system: use UDPConnection interface

--- core/fake_system.cpp
+++ core/fake_system.cpp
@@ -1,5 +1,6 @@
 #include "fake_system.h"
 #include "log.h"
+#include "udp_connection.h"
 
 namespace dronecore {
 
@@ -8,6 +9,30 @@ FakeSystem::FakeSystem(State state) :
 
 FakeSystem::~FakeSystem()
 {
+    stop();
+}
+
+bool FakeSystem::start()
+{
+    auto new_conn = std::make_shared<UdpConnection>(
+                        std::bind(&FakeSystem::receive_message, this, std::placeholders::_1),
+                        "0.0.0.0", 12345);
+    // TODO: we need to actually respond to 14540 but that's not implemented yet
+
+    ConnectionResult ret = new_conn->start();
+    return (ret == ConnectionResult::SUCCESS);
+}
+
+void FakeSystem::stop()
+{
+    // TODO: destruct connection here
+}
+
+
+void FakeSystem::receive_message(const mavlink_message_t &message)
+{
+    LogDebug() << "Got message with id: " << int(message.msgid);
+    // TODO: do more things
 }
 
 } // namespace dronecore
--- core/fake_system.h
+++ core/fake_system.h
@@ -15,8 +15,13 @@ public:
     FakeSystem(State state);
     ~FakeSystem();
 
+    bool start();
+    void stop();
+
 private:
     State _state;
+
+    void receive_message(const mavlink_message_t &message);
 };
 
 } // namespace dronecore
