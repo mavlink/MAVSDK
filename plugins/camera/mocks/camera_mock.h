47934c23010e1375599c7ec132a6edee808626e1
Jonas Vautherin <jonas.vautherin@gmail.com>
2018-05-24 16:47:35 +0200

    backend: add StopVideo to camera

--- plugins/camera/mocks/camera_mock.h
+++ plugins/camera/mocks/camera_mock.h
@@ -12,6 +12,7 @@ public:
     MOCK_CONST_METHOD1(start_photo_interval, Camera::Result(float));
     MOCK_CONST_METHOD0(stop_photo_interval, Camera::Result());
     MOCK_CONST_METHOD0(start_video, Camera::Result());
+    MOCK_CONST_METHOD0(stop_video, Camera::Result());
 };
 
 } // namespace testing
