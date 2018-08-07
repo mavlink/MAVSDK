7380ecc6cd8e0747f3014a852a8422133ea86d13
Jonas Vautherin <jonas.vautherin@gmail.com>
2018-08-07 15:43:09 +0200

    backend: version backend.framework on s3

--- backend/tools/push_backend_framework_to_s3.bash
+++ backend/tools/push_backend_framework_to_s3.bash
@@ -4,6 +4,10 @@ set -e
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 FAT_BIN_DIR=${SCRIPT_DIR}/../../build/fat_bin
+CURRENT_VERSION=0.0.2
 
 aws s3 cp ${FAT_BIN_DIR}/dronecode-backend.zip s3://dronecode-sdk/dronecode-backend-latest.zip
 aws s3api put-object-acl --bucket dronecode-sdk --key dronecode-backend-latest.zip --acl public-read
+
+aws s3 cp ${FAT_BIN_DIR}/dronecode-backend.zip s3://dronecode-sdk/dronecode-backend-${CURRENT_VERSION}.zip
+aws s3api put-object-acl --bucket dronecode-sdk --key dronecode-backend-${CURRENT_VERSION}.zip --acl public-read
