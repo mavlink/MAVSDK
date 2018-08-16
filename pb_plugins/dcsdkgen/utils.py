b1a4a81b1e33b1de099c3031dd9c96bf22aa3441
Jonas Vautherin <jonas.vautherin@gmail.com>
2018-08-16 10:14:37 +0200

    Handle non-primitive return types

--- pb_plugins/dcsdkgen/utils.py
+++ pb_plugins/dcsdkgen/utils.py
@@ -62,6 +62,10 @@ def extract_string_type(field):
     else:
         return "UNKNOWN_TYPE"
 
+def is_primitive_type(field):
+    """ Check if the field type is primitive (e.g. bool,
+    float) or not (e.g message, enum) """
+    return (not field.type in { 11, 14 })
 
 def filter_out_result(fields):
     """ Filters out the result fields (".*Result$") """
