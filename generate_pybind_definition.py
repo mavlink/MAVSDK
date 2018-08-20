fde1f0a551195327fe4f4f27541c534c6edf2d07
Julian Oes <julian@oes.ch>
2018-08-20 09:16:31 +0200

    Python: run auto-generation at build time

--- generate_pybind_definition.py
+++ generate_pybind_definition.py
@@ -2,6 +2,8 @@
 
 
 import re
+import argparse
+from string import Template
 
 
 class Method(object):
@@ -10,10 +12,11 @@ class Method(object):
         self.method_name = ""
         self.arguments = ""
 
-class Parser(object):
+class HeaderParser(object):
 
     def __init__(self):
         self.methods = []
+        self.output = ""
 
     def parse_plugin_header(self, lines):
         part = ""
@@ -70,26 +73,49 @@ class Parser(object):
         else:
             raise Exception("Could not apply regex to '{}'".format(part))
 
-    def print_pybind_entry(self):
-        print('    py::class_<Action>(m, "Action", plugin_base)')
-        print('        .def(py::init<System &>())')
+    def get_pybind_entry(self):
+        self.output += '    py::class_<Action>(m, "Action", plugin_base)\n'
+        self.output += '        .def(py::init<System &>())\n'
 
         for i, method in enumerate(self.methods):
-            print('        .def("{0}", &Action::{0})'
-                  .format(method.method_name), end="")
+            self.output += '        .def("{0}", &Action::{0})'.format(method.method_name)
             if i + 1 == len(self.methods):
-                print(";")
+                self.output += ";\n"
             else:
-                print()
+                self.output += "\n"
+        return self.output
 
 
 
 def main():
-    with open("plugins/action/include/plugins/action/action.h", "r") as f:
-        lines = f.readlines()
-        parser = Parser()
-        parser.parse_plugin_header(lines)
-        parser.print_pybind_entry()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--headers',
+                        nargs='+',
+                        help="List all header files to parse",
+                        required=True)
+    parser.add_argument('--template',
+                        help="Template to use for pybind source output",
+                        required=True)
+    parser.add_argument('--output',
+                        help="Where to put pybind source output",
+                        required=True)
+
+    args = parser.parse_args()
+
+    for header in args.headers:
+        with open(header, "r") as f:
+            lines = f.readlines()
+            header_parser = HeaderParser()
+            header_parser.parse_plugin_header(lines)
+
+    with open(args.template, 'r') as f:
+        template = f.read()
+
+    s = Template(template)
+    filled_template = s.substitute(plugins=header_parser.get_pybind_entry())
+
+    with open(args.output, 'w') as f:
+        f.write(filled_template)
 
 
 if __name__ == '__main__':
