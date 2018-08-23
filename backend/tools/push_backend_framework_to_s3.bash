1985bb268f649106b34aa620aee68bfe498b9803
Julian Oes <julian@oes.ch>
2018-08-23 17:06:12 +0200

    backend: bump version to 0.1.2

--- backend/tools/push_backend_framework_to_s3.bash
+++ backend/tools/push_backend_framework_to_s3.bash
@@ -4,7 +4,7 @@ set -e
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 FAT_BIN_DIR=${SCRIPT_DIR}/../../build/fat_bin
-CURRENT_VERSION=0.1.1
+CURRENT_VERSION=0.1.2
 
 aws s3 cp ${FAT_BIN_DIR}/dronecode-backend.zip s3://dronecode-sdk/dronecode-backend-latest.zip
 aws s3api put-object-acl --bucket dronecode-sdk --key dronecode-backend-latest.zip --acl public-read
