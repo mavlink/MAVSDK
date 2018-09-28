950562a4b5e88a8a922cbea6a24b8bbc2a947ec3
Julian Oes <julian@oes.ch>
2018-09-28 08:30:43 +0200

    docker: use user as user, set UID using entrypoint

---
+++ docker/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Utility to use local user, taken from:
+# https://denibertovic.com/posts/handling-permissions-with-docker-volumes/
+
+# Add local user
+# Either use the LOCAL_USER_ID if passed in at runtime or
+# make educated guess to default of first user.
+
+USER_ID=${LOCAL_USER_ID:-1000}
+
+echo "Starting with UID : $USER_ID"
+useradd --shell /bin/bash -u $USER_ID -o -c "" -m user
+export HOME=/home/user
+
+exec /usr/bin/su user -c "$@"
