86eb6131f07cb97ecb142388058bb4e3a23ceecd
Julian Oes <julian@oes.ch>
2018-10-03 14:34:14 +0200

    travis: also merge develop into master

--- ci-git-release.sh
+++ ci-git-release.sh
@@ -2,7 +2,16 @@
 
 set -e
 
-# We want to create a git tag when the version file changes.
+git_branch=`git branch`
+
+# Make sure we're on the 'develop' branch.
+if [ ! $git_branch = "develop" ]; then
+    echo "Error: branch is '$git_branch' instead of 'develop'"
+    exit 1
+fi
+
+# We want to create a git tag when the version file changes, and also merge
+# the latest 'develop' branch into the 'master' branch.
 
 # Read the version file.
 version_file=`cat version`
@@ -13,11 +22,13 @@ version_tag=`git describe --always --tags | sed 's/v\([0-9]*\.[0-9]*\.[0-9]*\).*
 echo "Current version file: $version_file"
 echo "Current version tag:  $version_tag"
 
-if [  "$version_file" = "$version_tag" ]; then
+if [ "$version_file" = "$version_tag" ]; then
     echo "Version tag is up to date"
 else
     new_tag="v$version_file"
     echo "Tags don't match, adding new tag $new_tag"
+    git checkout master
+    git merge -ff-only develop
     git tag $new_tag
-    git push origin $new_tag --dry-run
+    git push origin master $new_tag --dry-run
 fi
