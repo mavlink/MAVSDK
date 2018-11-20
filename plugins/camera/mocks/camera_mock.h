5c99584bfd866f1eb794ef1119c4a510b3fa36c7
Julian Oes <julian@oes.ch>
2018-11-20 16:58:35 +0100

    plugins: -Weffc++ fixes

--- plugins/camera/mocks/camera_mock.h
+++ plugins/camera/mocks/camera_mock.h
@@ -7,26 +7,28 @@ namespace testing {
 
 class MockCamera {
 public:
-    MOCK_CONST_METHOD0(take_photo, Camera::Result());
-    MOCK_CONST_METHOD1(start_photo_interval, Camera::Result(const float));
-    MOCK_CONST_METHOD0(stop_photo_interval, Camera::Result());
-    MOCK_CONST_METHOD0(start_video, Camera::Result());
-    MOCK_CONST_METHOD0(stop_video, Camera::Result());
-    MOCK_CONST_METHOD0(start_video_streaming, Camera::Result());
-    MOCK_CONST_METHOD0(stop_video_streaming, Camera::Result());
-    MOCK_CONST_METHOD1(set_mode, Camera::Result(Camera::Mode));
-    MOCK_CONST_METHOD1(subscribe_mode, void(Camera::subscribe_mode_callback_t));
-    MOCK_CONST_METHOD1(set_video_stream_settings, void(Camera::VideoStreamSettings));
+    MOCK_CONST_METHOD0(take_photo, Camera::Result()){};
+    MOCK_CONST_METHOD1(start_photo_interval, Camera::Result(const float)){};
+    MOCK_CONST_METHOD0(stop_photo_interval, Camera::Result()){};
+    MOCK_CONST_METHOD0(start_video, Camera::Result()){};
+    MOCK_CONST_METHOD0(stop_video, Camera::Result()){};
+    MOCK_CONST_METHOD0(start_video_streaming, Camera::Result()){};
+    MOCK_CONST_METHOD0(stop_video_streaming, Camera::Result()){};
+    MOCK_CONST_METHOD1(set_mode, Camera::Result(Camera::Mode)){};
+    MOCK_CONST_METHOD1(subscribe_mode, void(Camera::subscribe_mode_callback_t)){};
+    MOCK_CONST_METHOD1(set_video_stream_settings, void(Camera::VideoStreamSettings)){};
     MOCK_CONST_METHOD1(subscribe_video_stream_info,
-                       void(Camera::subscribe_video_stream_info_callback_t));
-    MOCK_CONST_METHOD1(subscribe_capture_info, void(Camera::capture_info_callback_t));
-    MOCK_CONST_METHOD1(subscribe_status, void(Camera::subscribe_status_callback_t));
+                       void(Camera::subscribe_video_stream_info_callback_t)){};
+    MOCK_CONST_METHOD1(subscribe_capture_info, void(Camera::capture_info_callback_t)){};
+    MOCK_CONST_METHOD1(subscribe_status, void(Camera::subscribe_status_callback_t)){};
     MOCK_CONST_METHOD1(subscribe_current_settings,
-                       void(Camera::subscribe_current_settings_callback_t));
+                       void(Camera::subscribe_current_settings_callback_t)){};
     MOCK_CONST_METHOD1(subscribe_possible_setting_options,
-                       void(Camera::subscribe_possible_setting_options_callback_t));
+                       void(Camera::subscribe_possible_setting_options_callback_t)){};
     MOCK_CONST_METHOD3(set_option_async,
-                       void(Camera::result_callback_t, const std::string &, const Camera::Option));
+                       void(Camera::result_callback_t,
+                            const std::string &,
+                            const Camera::Option)){};
 };
 
 } // namespace testing
--- plugins/telemetry/mocks/telemetry_mock.h
+++ plugins/telemetry/mocks/telemetry_mock.h
@@ -7,23 +7,24 @@ namespace testing {
 
 class MockTelemetry {
 public:
-    MOCK_CONST_METHOD1(position_async, void(Telemetry::position_callback_t));
-    MOCK_CONST_METHOD1(health_async, void(Telemetry::health_callback_t));
-    MOCK_CONST_METHOD1(home_position_async, void(Telemetry::position_callback_t));
-    MOCK_CONST_METHOD1(in_air_async, void(Telemetry::in_air_callback_t));
-    MOCK_CONST_METHOD1(armed_async, void(Telemetry::armed_callback_t));
-    MOCK_CONST_METHOD1(gps_info_async, void(Telemetry::gps_info_callback_t));
-    MOCK_CONST_METHOD1(battery_async, void(Telemetry::battery_callback_t));
-    MOCK_CONST_METHOD1(flight_mode_async, void(Telemetry::flight_mode_callback_t));
-    MOCK_CONST_METHOD1(attitude_quaternion_async, void(Telemetry::attitude_quaternion_callback_t));
+    MOCK_CONST_METHOD1(position_async, void(Telemetry::position_callback_t)){};
+    MOCK_CONST_METHOD1(health_async, void(Telemetry::health_callback_t)){};
+    MOCK_CONST_METHOD1(home_position_async, void(Telemetry::position_callback_t)){};
+    MOCK_CONST_METHOD1(in_air_async, void(Telemetry::in_air_callback_t)){};
+    MOCK_CONST_METHOD1(armed_async, void(Telemetry::armed_callback_t)){};
+    MOCK_CONST_METHOD1(gps_info_async, void(Telemetry::gps_info_callback_t)){};
+    MOCK_CONST_METHOD1(battery_async, void(Telemetry::battery_callback_t)){};
+    MOCK_CONST_METHOD1(flight_mode_async, void(Telemetry::flight_mode_callback_t)){};
+    MOCK_CONST_METHOD1(attitude_quaternion_async,
+                       void(Telemetry::attitude_quaternion_callback_t)){};
     MOCK_CONST_METHOD1(attitude_euler_angle_async,
-                       void(Telemetry::attitude_euler_angle_callback_t));
+                       void(Telemetry::attitude_euler_angle_callback_t)){};
     MOCK_CONST_METHOD1(camera_attitude_quaternion_async,
-                       void(Telemetry::attitude_quaternion_callback_t));
+                       void(Telemetry::attitude_quaternion_callback_t)){};
     MOCK_CONST_METHOD1(camera_attitude_euler_angle_async,
-                       void(Telemetry::attitude_euler_angle_callback_t));
-    MOCK_CONST_METHOD1(ground_speed_ned_async, void(Telemetry::ground_speed_ned_callback_t));
-    MOCK_CONST_METHOD1(rc_status_async, void(Telemetry::rc_status_callback_t));
+                       void(Telemetry::attitude_euler_angle_callback_t)){};
+    MOCK_CONST_METHOD1(ground_speed_ned_async, void(Telemetry::ground_speed_ned_callback_t)){};
+    MOCK_CONST_METHOD1(rc_status_async, void(Telemetry::rc_status_callback_t)){};
 };
 
 } // namespace testing
