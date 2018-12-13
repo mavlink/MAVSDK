d80c90bb2694bf776a10ff12b60e965a459c4375
Julian Oes <julian@oes.ch>
2018-12-13 09:49:20 +0100

    calibration: remove unused methods
    These had been copied from telemetry but not actually used.

--- plugins/calibration/calibration_impl.h
+++ plugins/calibration/calibration_impl.h
@@ -24,10 +24,6 @@ public:
     void calibrate_magnetometer_async(const Calibration::calibration_callback_t &callback);
     void calibrate_gimbal_accelerometer_async(const Calibration::calibration_callback_t &callback);
 
-    bool is_gyro_calibration_ok() const;
-    bool is_accelerometer_calibration_ok() const;
-    bool is_magnetometer_calibration_ok() const;
-
 private:
     void call_user_callback(const Calibration::calibration_callback_t &callback,
                             const Calibration::Result &result,
@@ -38,14 +34,6 @@ private:
 
     static Calibration::Result
     calibration_result_from_command_result(MAVLinkCommands::Result result);
-
-    void receive_param_cal_gyro(MAVLinkParameters::Result result, int value);
-    void receive_param_cal_accel(MAVLinkParameters::Result result, int value);
-    void receive_param_cal_mag(MAVLinkParameters::Result result, int value);
-
-    void set_gyro_calibration(bool ok);
-    void set_accelerometer_calibration(bool ok);
-    void set_magnetometer_calibration(bool ok);
 
     void report_started();
     void report_done();
