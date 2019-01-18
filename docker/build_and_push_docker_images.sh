01ae1cd9c0328b572d7472ab28fab29701984cca
Julian Oes <julian@oes.ch>
2019-01-18 10:38:59 +0100

    docker: remove Fedora 27, add Fedora 29
    Fedora 27 was end-of-lifed, so we should remove it and add Fedora 29
    instead.

--- docker/build_and_push_docker_images.sh
+++ docker/build_and_push_docker_images.sh
@@ -2,27 +2,27 @@
 
 set -e
 
-docker pull dronecode/dronecode-sdk-fedora-27
 docker pull dronecode/dronecode-sdk-fedora-28
+docker pull dronecode/dronecode-sdk-fedora-29
 docker pull dronecode/dronecode-sdk-ubuntu-16.04
 docker pull dronecode/dronecode-sdk-ubuntu-18.04
 
-docker build -f Dockerfile-Fedora-27 -t dronecode/dronecode-sdk-fedora-27 .
 docker build -f Dockerfile-Fedora-28 -t dronecode/dronecode-sdk-fedora-28 .
+docker build -f Dockerfile-Fedora-29 -t dronecode/dronecode-sdk-fedora-29 .
 docker build -f Dockerfile-Ubuntu-16.04 -t dronecode/dronecode-sdk-ubuntu-16.04 .
 docker build -f Dockerfile-Ubuntu-18.04 -t dronecode/dronecode-sdk-ubuntu-18.04 .
 
 DATE=`date +%Y-%m-%d`
 
-docker tag dronecode/dronecode-sdk-fedora-27:latest dronecode/dronecode-sdk-fedora-27:$DATE
 docker tag dronecode/dronecode-sdk-fedora-28:latest dronecode/dronecode-sdk-fedora-28:$DATE
+docker tag dronecode/dronecode-sdk-fedora-29:latest dronecode/dronecode-sdk-fedora-29:$DATE
 docker tag dronecode/dronecode-sdk-ubuntu-16.04:latest dronecode/dronecode-sdk-ubuntu-16.04:$DATE
 docker tag dronecode/dronecode-sdk-ubuntu-18.04:latest dronecode/dronecode-sdk-ubuntu-18.04:$DATE
 
-docker push dronecode/dronecode-sdk-fedora-27:latest
-docker push dronecode/dronecode-sdk-fedora-27:$DATE
 docker push dronecode/dronecode-sdk-fedora-28:latest
 docker push dronecode/dronecode-sdk-fedora-28:$DATE
+docker push dronecode/dronecode-sdk-fedora-29:latest
+docker push dronecode/dronecode-sdk-fedora-29:$DATE
 docker push dronecode/dronecode-sdk-ubuntu-16.04:latest
 docker push dronecode/dronecode-sdk-ubuntu-16.04:$DATE
 docker push dronecode/dronecode-sdk-ubuntu-18.04:latest
