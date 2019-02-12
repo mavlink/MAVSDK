d4117fc10a0e8f5008c5feb7832b058f2718d1ed
Omar Shrit <shrit@lri.fr>
2019-02-12 16:32:01 +0100

    Remove the Connection class parts

--- core/dronecode_sdk.cpp
+++ core/dronecode_sdk.cpp
@@ -9,34 +9,6 @@ DronecodeSDK::DronecodeSDK() : _impl{new DronecodeSDKImpl()} {}
 
 DronecodeSDK::~DronecodeSDK() {}
 
-ConnectionResult DronecodeSDK::add_any_connection(const std::string &connection_url)
-{
-    return _impl->add_any_connection(connection_url);
-}
-
-ConnectionResult DronecodeSDK::add_udp_connection(int local_port)
-{
-    return DronecodeSDK::add_udp_connection(DEFAULT_UDP_BIND_IP, local_port);
-}
-
-ConnectionResult DronecodeSDK::add_udp_connection(const std::string &local_bind_ip,
-                                                  const int local_port)
-{
-    return _impl->add_udp_connection(local_bind_ip, local_port);
-}
-
-ConnectionResult DronecodeSDK::add_tcp_connection(const std::string &remote_ip,
-                                                  const int remote_port)
-{
-    return _impl->add_tcp_connection(remote_ip, remote_port);
-}
-
-ConnectionResult DronecodeSDK::add_serial_connection(const std::string &dev_path,
-                                                     const int baudrate)
-{
-    return _impl->add_serial_connection(dev_path, baudrate);
-}
-
 std::vector<uint64_t> DronecodeSDK::system_uuids() const
 {
     return _impl->get_system_uuids();
