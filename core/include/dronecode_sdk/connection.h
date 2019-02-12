cfa2d003051421efbbd4fe20d4940565d50a9270
Omar Shrit <shrit@lri.fr>
2019-02-12 16:04:21 +0100

    Add the Conneciton::Result

--- core/include/dronecode_sdk/connection.h
+++ core/include/dronecode_sdk/connection.h
@@ -1,55 +1,57 @@
+#pragma once
+
 namespace dronecode_sdk {
 
 class Connection {
 public:
-    /** @brief Default UDP bind IP (accepts any incoming connections). */
-    static constexpr auto DEFAULT_UDP_BIND_IP = "0.0.0.0";
-    /** @brief Default UDP bind port (same port as used by MAVROS). */
-    static constexpr int DEFAULT_UDP_PORT = 14540;
-    /** @brief Default TCP remote IP (localhost). */
-    static constexpr auto DEFAULT_TCP_REMOTE_IP = "127.0.0.1";
-    /** @brief Default TCP remote port. */
-    static constexpr int DEFAULT_TCP_REMOTE_PORT = 5760;
-    /** @brief Default serial baudrate. */
-    static constexpr int DEFAULT_SERIAL_BAUDRATE = 57600;
-
-    /**
-     * @brief Constructor.
-     */
-    DronecodeSDK();
-
-    /**
-     * @brief Destructor.
-     *
-     * Disconnects all connected vehicles and releases all resources.
-     */
-    ~DronecodeSDK();
-
-    /**
-     * @brief Adds Connection via URL
-     *
-     * Supports connection: Serial, TCP or UDP.
-     * Connection URL format should be:
-     * - UDP - udp://[Bind_host][:Bind_port]
-     * - TCP - tcp://[Remote_host][:Remote_port]
-     * - Serial - serial://Dev_Node[:Baudrate]
-     *
-     * @param connection_url connection URL string.
-     * @return The result of adding the connection.
-     */
-    ConnectionResult add_any_connection(const std::string &connection_url);
-
-    /**
-     * @brief Adds a UDP connection to the specified port number.
-     *
-     * Any incoming connections are accepted (0.0.0.0).
-     *
-     * @param local_port The local UDP port to listen to (defaults to 14540, the same as MAVROS).
-     * @return The result of adding the connection.
-     */
-    ConnectionResult add_udp_connection(int local_port = DEFAULT_UDP_PORT);
-
-    /**
+  /** @brief Default UDP bind IP (accepts any incoming connections). */
+  static constexpr auto DEFAULT_UDP_BIND_IP = "0.0.0.0";
+  /** @brief Default UDP bind port (same port as used by MAVROS). */
+  static constexpr int DEFAULT_UDP_PORT = 14540;
+  /** @brief Default TCP remote IP (localhost). */
+  static constexpr auto DEFAULT_TCP_REMOTE_IP = "127.0.0.1";
+  /** @brief Default TCP remote port. */
+  static constexpr int DEFAULT_TCP_REMOTE_PORT = 5760;
+  /** @brief Default serial baudrate. */
+  static constexpr int DEFAULT_SERIAL_BAUDRATE = 57600;
+  
+  /**
+   * @brief Constructor.
+   */
+  Connection();
+  
+  /**
+   * @brief Destructor.
+   *
+   * Disconnects all connected vehicles and releases all resources.
+   */
+  ~Connection();
+  
+  /**
+   * @brief Adds Connection via URL
+   *
+   * Supports connection: Serial, TCP or UDP.
+   * Connection URL format should be:
+   * - UDP - udp://[Bind_host][:Bind_port]
+   * - TCP - tcp://[Remote_host][:Remote_port]
+   * - Serial - serial://Dev_Node[:Baudrate]
+   *
+   * @param connection_url connection URL string.
+   * @return The result of adding the connection.
+   */
+  Result add_any_connection(const std::string &connection_url);
+  
+  /**
+   * @brief Adds a UDP connection to the specified port number.
+   *
+   * Any incoming connections are accepted (0.0.0.0).
+   *
+   * @param local_port The local UDP port to listen to (defaults to 14540, the same as MAVROS).
+   * @return The result of adding the connection.
+   */
+  Result add_udp_connection(int local_port = DEFAULT_UDP_PORT);
+  
+  /**
      * @brief Adds a UDP connection to the specified port number and local interface.
      *
      * To accept only local connections of the machine, use 127.0.0.1.
@@ -59,28 +61,28 @@ public:
      * @param local_port The local UDP port to listen to (defaults to 14540, the same as MAVROS).
      * @return The result of adding the connection.
      */
-    ConnectionResult add_udp_connection(const std::string &local_ip,
-                                        int local_port = DEFAULT_UDP_PORT);
-
+  Result add_udp_connection(const std::string &local_ip,
+				      int local_port = DEFAULT_UDP_PORT);
+  
     /**
      * @brief Adds a TCP connection with a specific port number on localhost.
      *
      * @param remote_port The TCP port to connect to (defaults to 5760).
      * @return The result of adding the connection.
      */
-    ConnectionResult add_tcp_connection(int remote_port = DEFAULT_TCP_REMOTE_PORT);
-
-    /**
-     * @brief Adds a TCP connection with a specific IP address and port number.
-     *
-     * @param remote_ip Remote IP address to connect to.
-     * @param remote_port The TCP port to connect to (defaults to 5760).
-     * @return The result of adding the connection.
-     */
-    ConnectionResult add_tcp_connection(const std::string &remote_ip,
-                                        int remote_port = DEFAULT_TCP_REMOTE_PORT);
-
-    /**
+  Result add_tcp_connection(int remote_port = DEFAULT_TCP_REMOTE_PORT);
+  
+  /**
+   * @brief Adds a TCP connection with a specific IP address and port number.
+   *
+   * @param remote_ip Remote IP address to connect to.
+   * @param remote_port The TCP port to connect to (defaults to 5760).
+   * @return The result of adding the connection.
+   */
+  Result add_tcp_connection(const std::string &remote_ip,
+				      int remote_port = DEFAULT_TCP_REMOTE_PORT);
+  
+  /**
      * @brief Adds a serial connection with a specific port (COM or UART dev node) and baudrate as
      * specified.
      *
@@ -89,11 +91,11 @@ public:
      * @param baudrate Baudrate of the serial port (defaults to 57600).
      * @return The result of adding the connection.
      */
-    ConnectionResult add_serial_connection(const std::string &dev_path,
-                                           int baudrate = DEFAULT_SERIAL_BAUDRATE);
-
+  Result add_serial_connection(const std::string &dev_path,
+					 int baudrate = DEFAULT_SERIAL_BAUDRATE);
+  
   /**
-     * @brief Returns `true` if exactly one system is currently connected.
+   * @brief Returns `true` if exactly one system is currently connected.
      *
      * Connected means we are receiving heartbeats from this system.
      * It means the same as "discovered" and "not timed out".
@@ -102,28 +104,52 @@ public:
      *
      * @return `true` if exactly one system is connected.
      */
-    bool is_connected() const;
-
-    /**
-     * @brief Returns `true` if a system is currently connected.
-     *
-     * Connected means we are receiving heartbeats from this system.
-     * It means the same as "discovered" and "not timed out".
-     *
-     * @param uuid UUID of system to check.
-     * @return `true` if system is connected.
-     */
-    bool is_connected(uint64_t uuid) const;
-
-
+  bool is_connected() const;
+  
+  /**
+   * @brief Returns `true` if a system is currently connected.
+   *
+   * Connected means we are receiving heartbeats from this system.
+   * It means the same as "discovered" and "not timed out".
+   *
+   * @param uuid UUID of system to check.
+   * @return `true` if system is connected.
+   */
+  bool is_connected(uint64_t uuid) const;
+  
+  
+  
+  /**
+   * @brief Result type returned when adding a connection.
+   *
+   * **Note**: DronecodeSDK does not throw exceptions. Instead a result of this type will be
+   * returned when you add a connection: add_udp_connection().
+   */
+  enum Result {
+	       SUCCESS = 0, /**< @brief %Connection succeeded. */
+	       TIMEOUT, /**< @brief %Connection timed out. */
+	       SOCKET_ERROR, /**< @brief Socket error. */
+	       BIND_ERROR, /**< @brief Bind error. */
+	       SOCKET_CONNECTION_ERROR, /**< @brief Socket connection error. */
+	       CONNECTION_ERROR, /**< @brief %Connection error. */
+	       NOT_IMPLEMENTED, /**< @brief %Connection type not implemented. */
+	       SYSTEM_NOT_CONNECTED, /**< @brief No system is connected. */
+	       SYSTEM_BUSY, /**< @brief %System is busy. */
+	       COMMAND_DENIED, /**< @brief Command is denied. */
+	       DESTINATION_IP_UNKNOWN, /**< @brief %Connection IP is unknown. */
+	       CONNECTIONS_EXHAUSTED, /**< @brief %Connections exhausted. */
+	       CONNECTION_URL_INVALID /**< @brief URL invalid. */
+  };
+    
+  
 private:
-    /* @private. */
-    std::unique_ptr<ConnectionImpl> _impl;
-
-    // Non-copyable
-    Connection(const Connection &) = delete;
-    const Connection &operator=(const Connection &) = delete;
+  /* @private. */
+  std::unique_ptr<ConnectionImpl> _impl;
+  
+  // Non-copyable
+  Connection(const Connection &) = delete;
+  const Connection &operator=(const Connection &) = delete;
   
 };
-
+  
 } // namespace dronecode_sdk
