72b78e3aa71f932d3face8ef3ac49c91845353a0
douglaswsilva <silvadouglasw@gmail.com>
2019-02-28 11:17:51 -0800

    Add SetIntParams.

--- backend/src/plugins/params_raw/params_raw_service_impl.h
+++ backend/src/plugins/params_raw/params_raw_service_impl.h
@@ -9,7 +9,8 @@ class ParamsServiceImpl final : public rpc::params:ParamsService:Service {
 public:
     ParamsServiceImpl(Params &params) : _params(params) {}
 
-    grpc::Status GetIntParams(grpc::ServerContext * /* context */,
+    grpc::Status 
+    GetIntParams(grpc::ServerContext * /* context */,
                             const rpc::params::GetIntParamsRequest *request,
                             rpc::params::GetIntParamsResponse *response) override
     {
@@ -23,16 +24,39 @@ public:
 
                 rcp_params_result->set_result(
                     static_cast<rpc::params::ParamsResult::Result>(result_pair.first));
-                    rpc_params_result->set_result_str(dronecode_sdk::Params::result_str(result_pair.first));
+                rpc_params_result->set_result_str(dronecode_sdk::Params::result_str(result_pair.first));
 
-                    response->set_allocated_params_result(rpc_params_result);
-                    response->set_value(result_pair.second);
-                )
+                response->set_allocated_params_result(rpc_params_result);
+                response->set_value(result_pair.second);
             }
         }
 
         return grpc::Status::OK;
     }
+
+    grpc::Status 
+    SetIntParams(grpc::ServerContext * /* context */,
+                const rpc::params::SetIntParamsRequest *request,
+                rpc::params::SetIntParamsResponse *response) override
+    {
+        if (request != nullptr) {
+            const auto requested_param_name = request->name();
+            const auto requested_param_value = request->value();
+
+            const auto param_result = _params.set_param_int(requested_param_name, requested_param_value);
+
+            if (response != nullptr) {
+                auto *rpc_param_result = new rpc::params::ParamsResult();
+                rpc_param_result->set_result(static_cast<rpc::params::ParamsResult::Result>(param_result));
+                rpc_param_result->set_result_str(dronecode_sdk::Params::result_str(param_result));
+
+                response->set_allocated_params_result(rpc_param_result);
+            }
+        }
+
+        return grpc::Status::OK;
+    }
+
 
 private:
     Params &_params;
