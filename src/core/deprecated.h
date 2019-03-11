55510bc0f37f1aeb889763cccb50311ed735a8e7
Julian Oes <julian@oes.ch>
2019-03-11 16:56:34 +0100

    core: add header for deprecated attribute
    Taken from:
    https://stackoverflow.com/questions/295120/21265197#21265197

---
+++ src/core/deprecated.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#if defined(__GNUC__) || defined(__clang__)
+#define DEPRECATED __attribute__((deprecated))
+#elif defined(_MSC_VER)
+#define DEPRECATED __declspec(deprecated)
+#else
+#pragma message("WARNING: You need to implement DEPRECATED for this compiler")
+#define DEPRECATED
+#endif
