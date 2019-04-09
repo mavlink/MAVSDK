9241705059d12ca9c67162cb8a1fdb9daafbb8dc
Jonas Vautherin <jonas.vautherin@gmail.com>
2019-04-09 19:39:45 +0200

    build dependencies as SUPERBUILD

---
+++ third_party/cmake/build_target.cmake
@@ -0,0 +1,30 @@
+include(ProcessorCount)
+
+function(build_target TARGET_SOURCE_DIR TARGET_BINARY_DIR TARGET_INSTALL_DIR)
+    file(MAKE_DIRECTORY ${TARGET_BINARY_DIR})
+
+    execute_process(
+        COMMAND ${CMAKE_COMMAND}
+            "-G${CMAKE_GENERATOR}"
+            "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
+            "-DCMAKE_FIND_ROOT_PATH=${CMAKE_FIND_ROOT_PATH}"
+            "-DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}"
+            "-DCMAKE_INSTALL_PREFIX=${TARGET_INSTALL_DIR}"
+            "-DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}"
+            "-DANDROID_ABI=${ANDROID_ABI}"
+            "-DANDROID_STL=${ANDROID_STL}"
+            "-DANDROID_PLATFORM=${ANDROID_PLATFORM}"
+            "-DIOS_PLATFORM=${IOS_PLATFORM}"
+            "-DOPENSSL_ROOT_DIR=${OPENSSL_ROOT_DIR}"
+            "-DBUILD_BACKEND=${BUILD_BACKEND}"
+            "${TARGET_SOURCE_DIR}"
+            WORKING_DIRECTORY "${TARGET_BINARY_DIR}"
+    )
+
+    ProcessorCount(NUM_PROCS)
+    set(ENV{MAKEFLAGS} -j${NUM_PROCS})
+
+    execute_process(COMMAND ${CMAKE_COMMAND} --build .
+        WORKING_DIRECTORY ${TARGET_BINARY_DIR}
+    )
+endfunction()
---
+++ third_party/cmake/helpers/build_target.cmake
@@ -0,0 +1,29 @@
+include(ProcessorCount)
+
+function(build_target SRC_DIR TARGET_DIR)
+    file(MAKE_DIRECTORY ${TARGET_DIR}/build)
+
+    execute_process(
+        COMMAND ${CMAKE_COMMAND}
+            -G${CMAKE_GENERATOR}
+            -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+            "-DCMAKE_FIND_ROOT_PATH=${CMAKE_FIND_ROOT_PATH}"
+            "-DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}"
+            -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
+            -DBINARY_DIR=${TARGET_DIR}
+            -DANDROID_ABI=${ANDROID_ABI}
+            -DANDROID_STL=${ANDROID_STL}
+            -DANDROID_PLATFORM=${ANDROID_PLATFORM}
+            -DIOS_PLATFORM=${IOS_PLATFORM}
+            -DOPENSSL_ROOT_DIR=${OPENSSL_ROOT_DIR}
+            ${SRC_DIR}
+        WORKING_DIRECTORY "${TARGET_DIR}/build"
+    )
+
+    ProcessorCount(NUM_PROCS)
+
+    set(ENV{MAKEFLAGS} -j${NUM_PROCS})
+    execute_process(COMMAND ${CMAKE_COMMAND} --build .
+        WORKING_DIRECTORY ${TARGET_DIR}/build
+    )
+endfunction()
