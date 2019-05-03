eaf98b9bd282f31c91c4e497f99abaa8e0be5ca1
Christopher Kim <chris.sw.kim@gmail.com>
2019-05-03 14:14:17 -0400

    Add tests for offboard position ned yaw.
    Remove unique_ptr approach for creating arbitrary structs for testing.

--- backend/test/offboard_service_impl_test.cpp
+++ backend/test/offboard_service_impl_test.cpp
@@ -20,6 +20,9 @@ static constexpr float ARBITRARY_ROLL = 2.5f;
 static constexpr float ARBITRARY_PITCH = 4.0f;
 static constexpr float ARBITRARY_YAW = 3.7f;
 static constexpr float ARBITRARY_THRUST = 0.5f;
+static constexpr float ARBITRARY_NORTH_M = 10.54f;
+static constexpr float ARBITRARY_EAST_M = 5.62f;
+static constexpr float ARBITRARY_DOWN_M = 1.44f;
 
 std::vector<InputPair> generateInputPairs();
 std::string startAndGetTranslatedResult(dronecode_sdk::Offboard::Result start_result);
@@ -29,8 +32,8 @@ class OffboardServiceImplTest : public ::testing::TestWithParam<InputPair> {
 protected:
     void checkReturnsCorrectIsActiveStatus(const bool expected_is_active_status);
 
-    std::unique_ptr<dronecode_sdk::rpc::offboard::AttitudeRate>
-    createArbitraryRPCAttitudeRate() const;
+    dronecode_sdk::rpc::offboard::AttitudeRate createArbitraryRPCAttitudeRate() const;
+    dronecode_sdk::rpc::offboard::PositionNEDYaw createArbitraryRPCPositionNEDYaw() const;
 };
 
 TEST_P(OffboardServiceImplTest, startResultIsTranslatedCorrectly)
@@ -94,7 +97,7 @@ TEST_F(OffboardServiceImplTest, isActiveCallsGetter)
     EXPECT_CALL(offboard, is_active()).Times(1);
     dronecode_sdk::rpc::offboard::IsActiveResponse response;
 
-    offboard.IsActive(nullptr, nullptr, &response);
+    offboardService.IsActive(nullptr, nullptr, &response);
 }
 
 TEST_F(OffboardServiceImplTest, isActiveGetsCorrectValue)
@@ -139,15 +142,15 @@ TEST_F(OffboardServiceImplTest, setAttitudeRateDoesNotFailWithNullResponse)
     dronecode_sdk::rpc::offboard::SetAttitudeRateRequest request;
 
     auto rpc_attitude_rate = createArbitraryRPCAttitudeRate();
-    request.set_allocated_attitude_rate(rpc_attitude_rate.release());
+    request.set_attitude_rate(rpc_attitude_rate);
 
     offboardService.SetAttitudeRate(nullptr, &request, nullptr);
 }
 
-std::unique_ptr<dronecode_sdk::rpc::offboard::AttitudeRate>
+dronecode_sdk::rpc::offboard::AttitudeRate
 OffboardServiceImplTest::createArbitraryRPCAttitudeRate() const
 {
-    auto rpc_attitude_rate = std::unique_ptr<dronecode_sdk::rpc::offboard::AttitudeRate>(
+    auto rpc_attitude_rate = dronecode_sdk::rpc::offboard::AttitudeRate(
         new dronecode_sdk::rpc::offboard::AttitudeRate());
     rpc_attitude_rate->set_roll_deg_s(ARBITRARY_ROLL);
     rpc_attitude_rate->set_pitch_deg_s(ARBITRARY_PITCH);
@@ -165,12 +168,61 @@ TEST_F(OffboardServiceImplTest, setsAttitudeRateCorrectly)
 
     auto rpc_attitude_rate = createArbitraryRPCAttitudeRate();
     const auto expected_attitude_rate =
-        OffboardServiceImpl::translateRPCAttitudeRate(*rpc_attitude_rate);
+        OffboardServiceImpl::translateRPCAttitudeRate(rpc_attitude_rate);
     EXPECT_CALL(offboard, set_attitude_rate(expected_attitude_rate)).Times(1);
 
-    request.set_allocated_attitude_rate(rpc_attitude_rate.release());
+    request.set_attitude_rate(rpc_attitude_rate);
 
     offboardService.SetAttitudeRate(nullptr, &request, nullptr);
+}
+
+TEST_F(OffboardServiceImplTest, setPositionNEDYawDoesNotFailWithAllNullParams)
+{
+    MockOffboard offboard;
+    OffboardServiceImpl offboardService(offboard);
+
+    offboardService.SetPositionNed(nullptr, nullptr, nullptr);
+}
+
+TEST_F(OffboardServiceImplTest, setPositionNEDYawDoesNotFailWithNullResponse)
+{
+    MockOffboard offboard;
+    OffboardServiceImpl offboardService(offboard);
+    dronecode_sdk::rpc::offboard::SetPositionNedRequest request;
+
+    auto rpc_position_ned_yaw = createArbitraryRPCPositionNEDYaw();
+    request.set_position_ned(rpc_position_ned_yaw);
+
+    offboardService.SetPositionNed(nullptr, &request, nullptr);
+}
+
+dronecode_sdk::rpc::offboard::PositionNEDYaw
+OffboardServiceImplTest::createArbitraryRPCPositionNEDYaw() const
+{
+    auto rpc_position_ned_yaw = dronecode_sdk::rpc::offboard::PositionNEDYaw(
+        new dronecode_sdk::rpc::offboard::PositionNEDYaw());
+    rpc_position_ned_yaw->set_north_m(ARBITRARY_NORTH_M);
+    rpc_position_ned_yaw->set_east_m(ARBITRARY_EAST_M);
+    rpc_position_ned_yaw->set_down_m(ARBITRARY_DOWN_M);
+    rpc_position_ned_yaw->set_yaw_deg(ARBITRARY_YAW);
+
+    return rpc_position_ned_yaw;
+}
+
+TEST_F(OffboardServiceImplTest, setsPositionNEDYawCorrectly)
+{
+    MockOffboard offboard;
+    OffboardServiceImpl offboardService(offboard);
+    dronecode_sdk::rpc::offboard::SetPositionNedRequest request;
+
+    auto rpc_position_ned_yaw = createArbitraryRPCPositionNEDYaw();
+    const auto expected_position_ned_yaw =
+        OffboardServiceImpl::translateRPCPositionNEDYaw(rpc_position_ned_yaw);
+    EXPECT_CALL(offboard, set_position_ned(expected_position_ned_yaw)).Times(1);
+
+    request.set_position_ned(rpc_position_ned_yaw);
+
+    offboardService.SetPositionNed(nullptr, &request, nullptr);
 }
 
 std::vector<InputPair> generateInputPairs()
