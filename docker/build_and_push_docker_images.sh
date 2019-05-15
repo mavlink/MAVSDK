6d9b2fb46ea884cdf15ebb65aa9aa90c09beae12
Julian Oes <julian@oes.ch>
2019-05-15 10:08:38 +0200

    docker: add PX4 SITL image on top of Ubuntu 18.04
    This adds a docker image with pre-built PX4 SITL.

--- docker/build_and_push_docker_images.sh
+++ docker/build_and_push_docker_images.sh
@@ -6,11 +6,13 @@ docker pull dronecode/dronecode-sdk-fedora-28
 docker pull dronecode/dronecode-sdk-fedora-29
 docker pull dronecode/dronecode-sdk-ubuntu-16.04
 docker pull dronecode/dronecode-sdk-ubuntu-18.04
+docker pull dronecode/dronecode-sdk-ubuntu-18.04-px4-sitl
 
 docker build -f Dockerfile-Fedora-28 -t dronecode/dronecode-sdk-fedora-28 .
 docker build -f Dockerfile-Fedora-29 -t dronecode/dronecode-sdk-fedora-29 .
 docker build -f Dockerfile-Ubuntu-16.04 -t dronecode/dronecode-sdk-ubuntu-16.04 .
 docker build -f Dockerfile-Ubuntu-18.04 -t dronecode/dronecode-sdk-ubuntu-18.04 .
+docker build -f Dockerfile-Ubuntu-18.04-PX4-SITL -t dronecode/dronecode-sdk-ubuntu-18.04-px4-sitl .
 
 DATE=`date +%Y-%m-%d`
 
@@ -18,6 +20,7 @@ docker tag dronecode/dronecode-sdk-fedora-28:latest dronecode/dronecode-sdk-fedo
 docker tag dronecode/dronecode-sdk-fedora-29:latest dronecode/dronecode-sdk-fedora-29:$DATE
 docker tag dronecode/dronecode-sdk-ubuntu-16.04:latest dronecode/dronecode-sdk-ubuntu-16.04:$DATE
 docker tag dronecode/dronecode-sdk-ubuntu-18.04:latest dronecode/dronecode-sdk-ubuntu-18.04:$DATE
+docker tag dronecode/dronecode-sdk-ubuntu-18.04-px4-sitl:latest dronecode/dronecode-sdk-ubuntu-18.04-px4-sitl:$DATE
 
 docker push dronecode/dronecode-sdk-fedora-28:latest
 docker push dronecode/dronecode-sdk-fedora-28:$DATE
@@ -27,5 +30,7 @@ docker push dronecode/dronecode-sdk-ubuntu-16.04:latest
 docker push dronecode/dronecode-sdk-ubuntu-16.04:$DATE
 docker push dronecode/dronecode-sdk-ubuntu-18.04:latest
 docker push dronecode/dronecode-sdk-ubuntu-18.04:$DATE
+docker push dronecode/dronecode-sdk-ubuntu-18.04-px4-sitl:latest
+docker push dronecode/dronecode-sdk-ubuntu-18.04-px4-sitl:$DATE
 
 echo "Please update tag in Jenkinsfile to '$DATE'"
