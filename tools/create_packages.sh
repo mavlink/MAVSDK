aff214831e8f588e9932e253cbf2a32cf2f9c556
Jonas Vautherin <jonas.vautherin@gmail.com>
2019-05-28 20:05:38 +0200

    improve sh variable conventions

--- tools/create_packages.sh
+++ tools/create_packages.sh
@@ -5,32 +5,31 @@ set -e
 echo "Packaging"
 
 # This creates a version such as "v1.2.3-5-g123abc".
-VERSION=`git describe --always --tags`
+version=`git describe --always --tags`
 # We want to extract 1.2.3 from it.
-VERSION=`echo ${VERSION} | sed 's/v\([0-9]*\.[0-9]*\.[0-9]*\).*$/\1/'`
+version=`echo ${version} | sed 's/v\([0-9]*\.[0-9]*\.[0-9]*\).*$/\1/'`
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-WORKING_DIR=${SCRIPT_DIR}/install
-INSTALL_DIR=${WORKING_DIR}/usr
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+working_dir=${script_dir}/install
+install_dir=${working_dir}/usr
 
-rm -rf ${INSTALL_DIR}
+rm -rf ${install_dir}
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -Bbuild/release-package -H.;
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${install_dir} -Bbuild/release-package -H.;
 make -Cbuild/release-package install -j4;
 
 common_args="--input-type dir  \
-    --version ${VERSION} \
+    --version ${version} \
     --maintainer julian@oes.ch \
     --url https://sdk.dronecode.org \
     --license BSD-3-Clause \
     --force"
 
-
-pushd ${WORKING_DIR}
+pushd ${working_dir}
 
 # We need the relative path of all files.
-LIBRARY_FILES=`find . -type f | cut -sd / -f 2-`
-echo ${LIBRARY_FILES}
+library_files=`find . -type f | cut -sd / -f 2-`
+echo ${library_files}
 
 echo "#!/bin/sh" > run_ldconfig
 
@@ -38,7 +37,7 @@ echo "/sbin/ldconfig" >> run_ldconfig
 if cat /etc/os-release | grep 'Ubuntu'
 then
     echo "Building DEB package"
-    fpm $common_args \
+    fpm ${common_args} \
         --output-type deb \
         --depends libcurl4-openssl-dev \
         --deb-no-default-config-files \
@@ -46,19 +45,19 @@ then
         --provides libdronecode_sdk-dev \
         --after-install run_ldconfig \
         --after-remove run_ldconfig \
-        ${LIBRARY_FILES}
+        ${library_files}
 
-    DIST_VERSION=$(cat /etc/os-release | grep VERSION_ID | sed 's/[^0-9.]*//g')
+    dist_version=$(cat /etc/os-release | grep version_id | sed 's/[^0-9.]*//g')
 
-    for FILE in *_amd64.deb
+    for file in *_amd64.deb
     do
-        mv -v "$FILE" "${SCRIPT_DIR}/${FILE%_amd64.deb}_ubuntu${DIST_VERSION}_amd64.deb"
+        mv -v "${file}" "${script_dir}/${file%_amd64.deb}_ubuntu${dist_version}_amd64.deb"
     done
 
 elif cat /etc/os-release | grep 'Fedora'
 then
     echo "Building RPM package"
-    fpm $common_args \
+    fpm ${common_args} \
         --output-type rpm \
         --depends libcurl-devel \
         --name dronecode_sdk-devel \
@@ -66,13 +65,13 @@ then
         --after-install run_ldconfig \
         --after-remove run_ldconfig \
         --rpm-rpmbuild-define "_build_id_links none" \
-        ${LIBRARY_FILES}
+        ${library_files}
 
-    DIST_VERSION=$(cat /etc/os-release | grep VERSION_ID | sed 's/[^0-9]*//g')
+    dist_version=$(cat /etc/os-release | grep version_id | sed 's/[^0-9]*//g')
 
-    for FILE in *.x86_64.rpm
+    for file in *.x86_64.rpm
     do
-        mv -v "$FILE" "${SCRIPT_DIR}/${FILE%.x86_64.rpm}.fc${DIST_VERSION}-x86_64.rpm"
+        mv -v "${file}" "${script_dir}/${file%.x86_64.rpm}.fc${dist_version}-x86_64.rpm"
     done
 fi
 
@@ -80,4 +79,4 @@ rm run_ldconfig
 
 popd
 
-rm -rf ${INSTALL_DIR}
+rm -rf ${install_dir}
