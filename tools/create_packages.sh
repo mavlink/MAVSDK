ff055b785542fe59ebef0056df662efb78908675
Jonas Vautherin <jonas.vautherin@gmail.com>
2019-06-15 00:35:45 +0200

    travis: fix deb and rpm deployment

--- tools/create_packages.sh
+++ tools/create_packages.sh
@@ -3,80 +3,73 @@
 set -e
 
 echo "Packaging"
+echo "---------"
+
+if [ "$#" -ne 2 ]; then
+  echo "Error: missing path to the 'install' directory (i.e. what was specified as -DCMAKE_INSTALL_PREFIX)" >&2
+  echo "       or output path (where the package will be generated)!" >&2
+  echo "Usage: $0 <path/to/install> <output/path>" >&2
+  exit 1
+fi
+
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+install_dir=$1
+output_dir=$2
+
+working_dir=$(mktemp -d)
+mkdir -p ${working_dir}/install
+cp -r ${install_dir} ${working_dir}/install/usr
+
+# We need the relative path of all files.
+library_files=`find ${working_dir}/install -type f | cut -sd / -f 5-`
+echo "Files to be packaged:"
+echo ${library_files}
 
 # This creates a version such as "v1.2.3-5-g123abc".
 version=`git describe --always --tags`
 # We want to extract 1.2.3 from it.
 version=`echo ${version} | sed 's/v\([0-9]*\.[0-9]*\.[0-9]*\).*$/\1/'`
 
-script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-working_dir=${script_dir}/install
-install_dir=${working_dir}/usr
+echo "#!/bin/sh" > ${working_dir}/run_ldconfig
+echo "/sbin/ldconfig" >> ${working_dir}/run_ldconfig
 
-rm -rf ${install_dir}
+common_args="--chdir ${working_dir}/install \
+             --input-type dir  \
+             --name mavsdk \
+             --version ${version} \
+             --maintainer julian@oes.ch \
+             --url https://sdk.dronecode.org \
+             --license BSD-3-Clause \
+             --after-install ${working_dir}/run_ldconfig \
+             --after-remove ${working_dir}/run_ldconfig \
+             --force"
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${install_dir} -Bbuild/release-package -H.;
-make -Cbuild/release-package install -j4;
-
-common_args="--input-type dir  \
-    --version ${version} \
-    --maintainer julian@oes.ch \
-    --url https://sdk.dronecode.org \
-    --license BSD-3-Clause \
-    --force"
-
-pushd ${working_dir}
-
-# We need the relative path of all files.
-library_files=`find . -type f | cut -sd / -f 2-`
-echo ${library_files}
-
-echo "#!/bin/sh" > run_ldconfig
-
-echo "/sbin/ldconfig" >> run_ldconfig
 if cat /etc/os-release | grep 'Ubuntu'
 then
     echo "Building DEB package"
     fpm ${common_args} \
         --output-type deb \
-        --depends libcurl4-openssl-dev \
         --deb-no-default-config-files \
-        --name libdronecode_sdk-dev \
-        --provides libdronecode_sdk-dev \
-        --after-install run_ldconfig \
-        --after-remove run_ldconfig \
         ${library_files}
 
-    dist_version=$(cat /etc/os-release | grep version_id | sed 's/[^0-9.]*//g')
+    dist_version=$(cat /etc/os-release | grep VERSION_ID | sed 's/[^0-9.]*//g')
 
     for file in *_amd64.deb
     do
-        mv -v "${file}" "${script_dir}/${file%_amd64.deb}_ubuntu${dist_version}_amd64.deb"
+        mv -v "${file}" "${output_dir}/${file%_amd64.deb}_ubuntu${dist_version}_amd64.deb"
     done
-
 elif cat /etc/os-release | grep 'Fedora'
 then
     echo "Building RPM package"
     fpm ${common_args} \
         --output-type rpm \
-        --depends libcurl-devel \
-        --name dronecode_sdk-devel \
-        --provides dronecode_sdk-devel \
-        --after-install run_ldconfig \
-        --after-remove run_ldconfig \
         --rpm-rpmbuild-define "_build_id_links none" \
         ${library_files}
 
-    dist_version=$(cat /etc/os-release | grep version_id | sed 's/[^0-9]*//g')
+    dist_version=$(cat /etc/os-release | grep VERSION_ID | sed 's/[^0-9]*//g')
 
     for file in *.x86_64.rpm
     do
-        mv -v "${file}" "${script_dir}/${file%.x86_64.rpm}.fc${dist_version}-x86_64.rpm"
+        mv -v "${file}" "${output_dir}/${file%.x86_64.rpm}.fc${dist_version}-x86_64.rpm"
     done
 fi
-
-rm run_ldconfig
-
-popd
-
-rm -rf ${install_dir}
--- tools/travis-docker-build.sh
+++ tools/travis-docker-build.sh
@@ -1,28 +1,24 @@
 #!/usr/bin/env bash
 
-# Note: each build (Release, Debug) is done in its own folder,
-#       hence removing the need to clean between them.
-#       However, the third_parties (which are always built in Release
-#       mode with the superbuild) are reused between the builds.
-
 set -e
 
-# Build and run offline tests in Debug mode
-cmake -DCMAKE_BUILD_TYPE=Debug -Bbuild/debug -H.;
-make -Cbuild/debug -j4;
-./build/debug/src/unit_tests_runner --gtest_filter="-CurlTest.*";
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Build and run offline tests in Release mode
-cmake -DCMAKE_BUILD_TYPE=Release -Bbuild/release -H.;
-make -Cbuild/release -j4;
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=build/release/install -DBUILD_BACKEND=ON -Bbuild/release -H.;
+make -Cbuild/release -j4 install;
 ./build/release/src/unit_tests_runner --gtest_filter="-CurlTest.*";
+./build/release/src/backend/test/unit_tests_backend;
 
 # Try to build the external plugin example
 cmake -DCMAKE_BUILD_TYPE=Release -DEXTERNAL_DIR=external_example -Bbuild/external-plugin -H.;
 make -Cbuild/external-plugin -j4;
 
 # Check style
-./tools/fix_style.sh .
+${script_dir}/fix_style.sh ${script_dir}/..
 
 # Generate documentation
-./tools/generate_docs.sh
+${script_dir}/generate_docs.sh
+
+# Generate packages
+${script_dir}/create_packages.sh ${script_dir}/../build/release/install ${script_dir}/../build/release
