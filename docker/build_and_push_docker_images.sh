1d4dd12df12998e03c24f4baadacb8c485b651ed
Julian Oes <julian@oes.ch>
2019-06-17 17:29:41 +0200

    docker: move from mavlink to mavsdk org

--- docker/build_and_push_docker_images.sh
+++ docker/build_and_push_docker_images.sh
@@ -2,35 +2,35 @@
 
 set -e
 
-docker pull mavlink/mavsdk-fedora-28
-docker pull mavlink/mavsdk-fedora-29
-docker pull mavlink/mavsdk-ubuntu-16.04
-docker pull mavlink/mavsdk-ubuntu-18.04
-docker pull mavlink/mavsdk-ubuntu-18.04-px4-sitl
+docker pull mavsdk/mavsdk-fedora-28
+docker pull mavsdk/mavsdk-fedora-29
+docker pull mavsdk/mavsdk-ubuntu-16.04
+docker pull mavsdk/mavsdk-ubuntu-18.04
+docker pull mavsdk/mavsdk-ubuntu-18.04-px4-sitl
 
-docker build -f Dockerfile-Fedora-28 -t mavlink/mavsdk-fedora-28 .
-docker build -f Dockerfile-Fedora-29 -t mavlink/mavsdk-fedora-29 .
-docker build -f Dockerfile-Ubuntu-16.04 -t mavlink/mavsdk-ubuntu-16.04 .
-docker build -f Dockerfile-Ubuntu-18.04 -t mavlink/mavsdk-ubuntu-18.04 .
-docker build -f Dockerfile-Ubuntu-18.04-PX4-SITL -t mavlink/mavsdk-ubuntu-18.04-px4-sitl .
+docker build -f Dockerfile-Fedora-28 -t mavsdk/mavsdk-fedora-28 .
+docker build -f Dockerfile-Fedora-29 -t mavsdk/mavsdk-fedora-29 .
+docker build -f Dockerfile-Ubuntu-16.04 -t mavsdk/mavsdk-ubuntu-16.04 .
+docker build -f Dockerfile-Ubuntu-18.04 -t mavsdk/mavsdk-ubuntu-18.04 .
+docker build -f Dockerfile-Ubuntu-18.04-PX4-SITL -t mavsdk/mavsdk-ubuntu-18.04-px4-sitl .
 
 DATE=`date +%Y-%m-%d`
 
-docker tag mavlink/mavsdk-fedora-28:latest mavlink/mavsdk-fedora-28:$DATE
-docker tag mavlink/mavsdk-fedora-29:latest mavlink/mavsdk-fedora-29:$DATE
-docker tag mavlink/mavsdk-ubuntu-16.04:latest mavlink/mavsdk-ubuntu-16.04:$DATE
-docker tag mavlink/mavsdk-ubuntu-18.04:latest mavlink/mavsdk-ubuntu-18.04:$DATE
-docker tag mavlink/mavsdk-ubuntu-18.04-px4-sitl:latest mavlink/mavsdk-ubuntu-18.04-px4-sitl:$DATE
+docker tag mavsdk/mavsdk-fedora-28:latest mavsdk/mavsdk-fedora-28:$DATE
+docker tag mavsdk/mavsdk-fedora-29:latest mavsdk/mavsdk-fedora-29:$DATE
+docker tag mavsdk/mavsdk-ubuntu-16.04:latest mavsdk/mavsdk-ubuntu-16.04:$DATE
+docker tag mavsdk/mavsdk-ubuntu-18.04:latest mavsdk/mavsdk-ubuntu-18.04:$DATE
+docker tag mavsdk/mavsdk-ubuntu-18.04-px4-sitl:latest mavsdk/mavsdk-ubuntu-18.04-px4-sitl:$DATE
 
-docker push mavlink/mavsdk-fedora-28:latest
-docker push mavlink/mavsdk-fedora-28:$DATE
-docker push mavlink/mavsdk-fedora-29:latest
-docker push mavlink/mavsdk-fedora-29:$DATE
-docker push mavlink/mavsdk-ubuntu-16.04:latest
-docker push mavlink/mavsdk-ubuntu-16.04:$DATE
-docker push mavlink/mavsdk-ubuntu-18.04:latest
-docker push mavlink/mavsdk-ubuntu-18.04:$DATE
-docker push mavlink/mavsdk-ubuntu-18.04-px4-sitl:latest
-docker push mavlink/mavsdk-ubuntu-18.04-px4-sitl:$DATE
+docker push mavsdk/mavsdk-fedora-28:latest
+docker push mavsdk/mavsdk-fedora-28:$DATE
+docker push mavsdk/mavsdk-fedora-29:latest
+docker push mavsdk/mavsdk-fedora-29:$DATE
+docker push mavsdk/mavsdk-ubuntu-16.04:latest
+docker push mavsdk/mavsdk-ubuntu-16.04:$DATE
+docker push mavsdk/mavsdk-ubuntu-18.04:latest
+docker push mavsdk/mavsdk-ubuntu-18.04:$DATE
+docker push mavsdk/mavsdk-ubuntu-18.04-px4-sitl:latest
+docker push mavsdk/mavsdk-ubuntu-18.04-px4-sitl:$DATE
 
 echo "Please update tag in Jenkinsfile to '$DATE'"
