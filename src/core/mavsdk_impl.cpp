a4cba717588d860bf53e22c2d16d3570f73b688f
Julian Oes <julian@oes.ch>
2019-09-21 08:20:24 +0200

    core: the != nullptr test is not needed

--- src/core/mavsdk_impl.cpp
+++ src/core/mavsdk_impl.cpp
@@ -433,7 +433,7 @@ bool MavsdkImpl::does_system_exist(uint8_t system_id)
 
 void MavsdkImpl::notify_on_discover(const uint64_t uuid)
 {
-    if (_on_discover_callback != nullptr) {
+    if (_on_discover_callback) {
         _on_discover_callback(uuid);
     }
 }
@@ -441,7 +441,7 @@ void MavsdkImpl::notify_on_discover(const uint64_t uuid)
 void MavsdkImpl::notify_on_timeout(const uint64_t uuid)
 {
     LogDebug() << "Lost " << uuid;
-    if (_on_timeout_callback != nullptr) {
+    if (_on_timeout_callback) {
         _on_timeout_callback(uuid);
     }
 }
