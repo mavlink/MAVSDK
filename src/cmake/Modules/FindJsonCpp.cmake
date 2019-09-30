298ab32f3fc8c45803e3c67097abdaf55fdb56ae
Jonas Vautherin <jonas.vautherin@gmail.com>
2019-09-30 13:48:50 +0200

    use pkg-config in FindJsonCpp for Ubuntu

--- src/cmake/Modules/FindJsonCpp.cmake
+++ src/cmake/Modules/FindJsonCpp.cmake
@@ -1,4 +1,5 @@
-find_package(jsoncpp CONFIG)
+find_package(jsoncpp CONFIG QUIET)
+
 if(JsonCpp_FOUND)
     message(STATUS "Found jsoncpp via config file: ${JsonCpp_DIR}")
     if(NOT JsonCpp_LIBRARY)
@@ -9,10 +10,19 @@ if(JsonCpp_FOUND)
         endif()
     endif()
 else()
-    find_path(JsonCpp_INCLUDE_DIR NAMES json/json.h)
-    find_library(JsonCpp_LIBRARY jsoncpp)
+    find_package(PkgConfig)
+    pkg_check_modules(PC_JSONCPP jsoncpp)
+
+    find_path(JsonCpp_INCLUDE_DIR
+              NAMES json/json.h
+              HINTS ${PC_JSONCPP_INCLUDEDIR} ${PC_JSONCPP_INCLUDE_DIRS})
+    find_library(JsonCpp_LIBRARY
+                 NAMES jsoncpp
+                 HINTS ${PC_JSONCPP_LIBDIR} ${PC_JSONCPP_LIBRARY_DIRS})
+
     include(FindPackageHandleStandardArgs)
     find_package_handle_standard_args(JsonCpp DEFAULT_MSG JsonCpp_LIBRARY JsonCpp_INCLUDE_DIR)
+
     mark_as_advanced(JsonCpp_INCLUDE_DIR JsonCpp_LIBRARY)
 endif()
 
