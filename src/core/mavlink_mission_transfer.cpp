4d3dafc80dbb34d360f05947247cc76686cdb047
Julian Oes <julian@oes.ch>
2020-02-03 12:51:47 +0100

    core: mostly continued with mission download

--- src/core/mavlink_mission_transfer.cpp
+++ src/core/mavlink_mission_transfer.cpp
@@ -133,10 +133,17 @@ void MAVLinkMissionTransfer::UploadWorkItem::start()
         return;
     }
 
+    _retries_done = 0;
+    _step = Step::SendCount;
     _timeout_handler.add([this]() { process_timeout(); }, timeout_s, &_cookie);
 
-    _next_sequence_expected = 0;
+    _next_sequence = 0;
 
+    send_count();
+}
+
+void MAVLinkMissionTransfer::UploadWorkItem::send_count()
+{
     mavlink_message_t message;
     mavlink_msg_mission_count_pack(
         _config.own_system_id,
@@ -152,6 +159,8 @@ void MAVLinkMissionTransfer::UploadWorkItem::start()
         callback_and_reset(Result::ConnectionError);
         return;
     }
+
+    ++_retries_done;
 }
 
 void MAVLinkMissionTransfer::UploadWorkItem::process_mission_request_int(
@@ -160,10 +169,37 @@ void MAVLinkMissionTransfer::UploadWorkItem::process_mission_request_int(
     mavlink_mission_request_int_t request_int;
     mavlink_msg_mission_request_int_decode(&message, &request_int);
 
+    _step = Step::SendItems;
+
+    if (_next_sequence < request_int.seq) {
+        // We should not go back to a previous one.
+        // TODO: figure out if we should error here.
+        LogWarn() << "mission_request_int: sequence incorrect";
+        return;
+
+    } else if (_next_sequence > request_int.seq) {
+        // We have already sent that one before.
+        if (_retries_done >= retries) {
+            _timeout_handler.remove(_cookie);
+            callback_and_reset(Result::Timeout);
+            return;
+        }
+
+    } else {
+        // Correct one, sending it the first time.
+        _retries_done = 0;
+    }
+
     _timeout_handler.refresh(_cookie);
 
-    if (_next_sequence_expected < request_int.seq) {
-        LogWarn() << "mission_request_int: sequence incorrect";
+    _next_sequence = request_int.seq;
+    send_mission_item();
+}
+
+void MAVLinkMissionTransfer::UploadWorkItem::send_mission_item()
+{
+    if (_next_sequence >= _items.size()) {
+        LogErr() << "send_mission_item: sequence out of bounds";
         return;
     }
 
@@ -174,27 +210,30 @@ void MAVLinkMissionTransfer::UploadWorkItem::process_mission_request_int(
         &new_message,
         _config.target_system_id,
         _config.target_component_id,
-        request_int.seq,
-        _items[request_int.seq].frame,
-        _items[request_int.seq].command,
-        _items[request_int.seq].current,
-        _items[request_int.seq].autocontinue,
-        _items[request_int.seq].param1,
-        _items[request_int.seq].param2,
-        _items[request_int.seq].param3,
-        _items[request_int.seq].param4,
-        _items[request_int.seq].x,
-        _items[request_int.seq].y,
-        _items[request_int.seq].z,
+        _next_sequence,
+        _items[_next_sequence].frame,
+        _items[_next_sequence].command,
+        _items[_next_sequence].current,
+        _items[_next_sequence].autocontinue,
+        _items[_next_sequence].param1,
+        _items[_next_sequence].param2,
+        _items[_next_sequence].param3,
+        _items[_next_sequence].param4,
+        _items[_next_sequence].x,
+        _items[_next_sequence].y,
+        _items[_next_sequence].z,
         MAV_MISSION_TYPE_MISSION);
+    // TODO: above type lacking test
 
-    _next_sequence_expected = request_int.seq + 1;
+    ++_next_sequence;
 
     if (!_sender.send_message(new_message)) {
         _timeout_handler.remove(_cookie);
         callback_and_reset(Result::ConnectionError);
         return;
     }
+
+    ++_retries_done;
 }
 
 void MAVLinkMissionTransfer::UploadWorkItem::process_mission_ack(const mavlink_message_t& message)
@@ -245,7 +284,7 @@ void MAVLinkMissionTransfer::UploadWorkItem::process_mission_ack(const mavlink_m
             return;
     }
 
-    if (_next_sequence_expected == static_cast<int>(_items.size())) {
+    if (_next_sequence == _items.size()) {
         callback_and_reset(Result::Success);
     } else {
         callback_and_reset(Result::ProtocolError);
@@ -254,7 +293,21 @@ void MAVLinkMissionTransfer::UploadWorkItem::process_mission_ack(const mavlink_m
 
 void MAVLinkMissionTransfer::UploadWorkItem::process_timeout()
 {
-    callback_and_reset(Result::Timeout);
+    if (_retries_done >= retries) {
+        callback_and_reset(Result::Timeout);
+        return;
+    }
+
+    switch (_step) {
+        case Step::SendCount:
+            _timeout_handler.add([this]() { process_timeout(); }, timeout_s, &_cookie);
+            send_count();
+            break;
+
+        case Step::SendItems:
+            callback_and_reset(Result::Timeout);
+            break;
+    }
 }
 
 void MAVLinkMissionTransfer::UploadWorkItem::callback_and_reset(Result result)
@@ -262,9 +315,12 @@ void MAVLinkMissionTransfer::UploadWorkItem::callback_and_reset(Result result)
     if (_callback) {
         _callback(result);
     }
+
+    _retries_done = 0;
+    _step = Step::SendCount;
     _callback = nullptr;
     _items.clear();
-    _next_sequence_expected = -1;
+    _next_sequence = 0;
 }
 
 MAVLinkMissionTransfer::DownloadWorkItem::DownloadWorkItem(
@@ -276,7 +332,17 @@ MAVLinkMissionTransfer::DownloadWorkItem::DownloadWorkItem(
     ResultAndItemsCallback callback) :
     WorkItem(config, sender, message_handler, timeout_handler, type),
     _callback(callback)
-{}
+{
+    _message_handler.register_one(
+        MAVLINK_MSG_ID_MISSION_COUNT,
+        [this](const mavlink_message_t& message) { process_mission_count(message); },
+        this);
+
+    _message_handler.register_one(
+        MAVLINK_MSG_ID_MISSION_ITEM_INT,
+        [this](const mavlink_message_t& message) { process_mission_item_int(message); },
+        this);
+}
 
 MAVLinkMissionTransfer::DownloadWorkItem::~DownloadWorkItem()
 {
@@ -286,7 +352,9 @@ MAVLinkMissionTransfer::DownloadWorkItem::~DownloadWorkItem()
 
 void MAVLinkMissionTransfer::DownloadWorkItem::start()
 {
+    _items.clear();
     _started = true;
+    _retries_done = 0;
     _timeout_handler.add([this]() { process_timeout(); }, timeout_s, &_cookie);
     request_list();
 }
@@ -307,12 +375,123 @@ void MAVLinkMissionTransfer::DownloadWorkItem::request_list()
         callback_and_reset(Result::ConnectionError);
         return;
     }
+
+    ++_retries_done;
+}
+
+void MAVLinkMissionTransfer::DownloadWorkItem::request_item()
+{
+    mavlink_message_t message;
+    mavlink_msg_mission_request_int_pack(
+        _config.own_system_id,
+        _config.own_component_id,
+        &message,
+        _config.target_system_id,
+        _config.target_component_id,
+        _next_sequence,
+        _type);
+
+    if (!_sender.send_message(message)) {
+        _timeout_handler.remove(_cookie);
+        callback_and_reset(Result::ConnectionError);
+        return;
+    }
+
+    ++_retries_done;
+}
+
+void MAVLinkMissionTransfer::DownloadWorkItem::send_ack_and_finish()
+{
+    mavlink_message_t message;
+    mavlink_msg_mission_ack_pack(
+        _config.own_system_id,
+        _config.own_component_id,
+        &message,
+        _config.target_system_id,
+        _config.target_component_id,
+        MAV_MISSION_ACCEPTED,
+        _type);
+
+    if (!_sender.send_message(message)) {
+        callback_and_reset(Result::ConnectionError);
+        return;
+    }
+
+    // We do not wait on anything coming back after this.
+    callback_and_reset(Result::Success);
+}
+
+void MAVLinkMissionTransfer::DownloadWorkItem::process_mission_count(
+    const mavlink_message_t& message)
+{
+    mavlink_mission_count_t count;
+    mavlink_msg_mission_count_decode(&message, &count);
+
+    if (count.count == 0) {
+        send_ack_and_finish();
+        _timeout_handler.remove(_cookie);
+        return;
+    }
+
+    _timeout_handler.refresh(_cookie);
+    _next_sequence = 0;
+    _step = Step::RequestItem;
+    _retries_done = 0;
+    _expected_count = count.count;
+    request_item();
+}
+
+void MAVLinkMissionTransfer::DownloadWorkItem::process_mission_item_int(
+    const mavlink_message_t& message)
+{
+    _timeout_handler.refresh(_cookie);
+
+    mavlink_mission_item_int_t item_int;
+    mavlink_msg_mission_item_int_decode(&message, &item_int);
+
+    _items.push_back(ItemInt{item_int.seq,
+                             item_int.frame,
+                             item_int.command,
+                             item_int.current,
+                             item_int.autocontinue,
+                             item_int.param1,
+                             item_int.param2,
+                             item_int.param3,
+                             item_int.param4,
+                             item_int.x,
+                             item_int.y,
+                             item_int.z,
+                             item_int.mission_type});
+
+    if (_next_sequence + 1 == _expected_count) {
+        _timeout_handler.remove(_cookie);
+        send_ack_and_finish();
+
+    } else {
+        _next_sequence = item_int.seq + 1;
+        _retries_done = 0;
+        request_item();
+    }
 }
 
 void MAVLinkMissionTransfer::DownloadWorkItem::process_timeout()
 {
-    LogWarn() << "Timeout!";
-    request_list();
+    if (_retries_done >= retries) {
+        callback_and_reset(Result::Timeout);
+        return;
+    }
+
+    switch (_step) {
+        case Step::RequestList:
+            _timeout_handler.add([this]() { process_timeout(); }, timeout_s, &_cookie);
+            request_list();
+            break;
+
+        case Step::RequestItem:
+            _timeout_handler.add([this]() { process_timeout(); }, timeout_s, &_cookie);
+            request_item();
+            break;
+    }
 }
 
 void MAVLinkMissionTransfer::DownloadWorkItem::callback_and_reset(Result result)
