a87c67f054df2fdb6d1bcb5978e1671dcf70d064
Julian Oes <julian@oes.ch>
2020-02-07 16:28:40 +0100

    geofence: swap over to new transfer class

--- src/plugins/geofence/geofence_impl.cpp
+++ src/plugins/geofence/geofence_impl.cpp
@@ -5,9 +5,7 @@
 
 namespace mavsdk {
 
-GeofenceImpl::GeofenceImpl(System& system) :
-    PluginImplBase(system),
-    _mavlink_geofence_item_messages()
+GeofenceImpl::GeofenceImpl(System& system) : PluginImplBase(system)
 {
     _parent->register_plugin(this);
 }
@@ -17,28 +15,9 @@ GeofenceImpl::~GeofenceImpl()
     _parent->unregister_plugin(this);
 }
 
-void GeofenceImpl::init()
-{
-    using namespace std::placeholders; // for `_1`
+void GeofenceImpl::init() {}
 
-    _parent->register_mavlink_message_handler(
-        MAVLINK_MSG_ID_MISSION_REQUEST,
-        std::bind(&GeofenceImpl::process_mission_request, this, _1),
-        this);
-
-    _parent->register_mavlink_message_handler(
-        MAVLINK_MSG_ID_MISSION_REQUEST_INT,
-        std::bind(&GeofenceImpl::process_mission_request_int, this, _1),
-        this);
-
-    _parent->register_mavlink_message_handler(
-        MAVLINK_MSG_ID_MISSION_ACK, std::bind(&GeofenceImpl::process_mission_ack, this, _1), this);
-}
-
-void GeofenceImpl::deinit()
-{
-    _parent->unregister_all_mavlink_message_handlers(this);
-}
+void GeofenceImpl::deinit() {}
 
 void GeofenceImpl::enable() {}
 
@@ -48,131 +27,24 @@ void GeofenceImpl::send_geofence_async(
     const std::vector<std::shared_ptr<Geofence::Polygon>>& polygons,
     const Geofence::result_callback_t& callback)
 {
-    if (_active) {
-        report_geofence_result(callback, Geofence::Result::BUSY);
-        return;
-    }
+    // We can just create these items on the stack because they get copied
+    // later in the MAVLinkMissionTransfer constructor.
+    const auto items = assemble_items(polygons);
 
-    if (!_parent->does_support_mission_int()) {
-        LogDebug() << "Mission int messages not supported";
-        report_geofence_result(callback, Geofence::Result::ERROR);
-        return;
-    }
-
-    _active = true;
-
-    assemble_mavlink_messages(polygons);
-
-    LogDebug() << "size afterwards is: " << _mavlink_geofence_item_messages.size();
-
-    mavlink_message_t message;
-    mavlink_msg_mission_count_pack(
-        _parent->get_own_system_id(),
-        _parent->get_own_component_id(),
-        &message,
-        _parent->get_system_id(),
-        _parent->get_autopilot_id(),
-        _mavlink_geofence_item_messages.size(),
-        MAV_MISSION_TYPE_FENCE);
-
-    LogDebug() << "About to send " << _mavlink_geofence_item_messages.size() << " geofence items";
-
-    if (!_parent->send_message(message)) {
-        report_geofence_result(callback, Geofence::Result::ERROR);
-    }
-
-    _result_callback = callback;
-
-    _parent->register_timeout_handler(
-        std::bind(&GeofenceImpl::timeout_happened, this), 2.0, &_timeout_cookie);
+    _parent->mission_transfer().upload_items_async(
+        MAV_MISSION_TYPE_FENCE, items, [this, callback](MAVLinkMissionTransfer::Result result) {
+            auto converted_result = convert_result(result);
+            _parent->call_user_callback(
+                [callback, converted_result]() { callback(converted_result); });
+        });
 }
 
-void GeofenceImpl::process_mission_request(const mavlink_message_t& unused)
+std::vector<MAVLinkMissionTransfer::ItemInt>
+GeofenceImpl::assemble_items(const std::vector<std::shared_ptr<Geofence::Polygon>>& polygons)
 {
-    // We only support int, so we nack this and thus tell the autopilot to use int.
-    UNUSED(unused);
+    std::vector<MAVLinkMissionTransfer::ItemInt> items;
 
-    mavlink_message_t message;
-    mavlink_msg_mission_ack_pack(
-        _parent->get_own_system_id(),
-        _parent->get_own_component_id(),
-        &message,
-        _parent->get_system_id(),
-        _parent->get_autopilot_id(),
-        MAV_MISSION_UNSUPPORTED,
-        MAV_MISSION_TYPE_FENCE);
-
-    _parent->send_message(message);
-
-    // Reset the timeout because we're still communicating.
-    _parent->refresh_timeout_handler(this);
-}
-
-void GeofenceImpl::process_mission_request_int(const mavlink_message_t& message)
-{
-    if (!_active) {
-        LogDebug() << "Ignore geofence request, currently inactive";
-        return;
-    }
-
-    mavlink_mission_request_int_t mission_request_int;
-    mavlink_msg_mission_request_int_decode(&message, &mission_request_int);
-
-    if (mission_request_int.target_system != _parent->get_own_system_id() &&
-        mission_request_int.target_component != _parent->get_own_component_id()) {
-        LogDebug() << "Ignore geofence request int that is not for us";
-        return;
-    }
-
-    if (mission_request_int.mission_type != MAV_MISSION_TYPE_FENCE) {
-        LogDebug() << "Ignore mission request that is not a geofence";
-        return;
-    }
-
-    send_geofence_item(mission_request_int.seq);
-
-    // Reset the timeout because we're still communicating.
-    _parent->refresh_timeout_handler(this);
-}
-
-void GeofenceImpl::process_mission_ack(const mavlink_message_t& message)
-{
-    if (!_active) {
-        LogDebug() << "Ignore mission ack because not active";
-        return;
-    }
-
-    mavlink_mission_ack_t mission_ack;
-    mavlink_msg_mission_ack_decode(&message, &mission_ack);
-
-    if (mission_ack.target_system != _parent->get_own_system_id() &&
-        mission_ack.target_component != _parent->get_own_component_id()) {
-        LogDebug() << "Ignore geofence ack that is not for us";
-        return;
-    }
-
-    // We got some response, so it wasn't a timeout and we can remove it.
-    _parent->unregister_timeout_handler(this);
-
-    if (mission_ack.type == MAV_MISSION_ACCEPTED) {
-        report_geofence_result(_result_callback, Geofence::Result::SUCCESS);
-        LogDebug() << "Sucess, done";
-
-    } else if (mission_ack.type == MAV_MISSION_NO_SPACE) {
-        LogDebug() << "Error: too many geofence items: " << int(mission_ack.type);
-        report_geofence_result(_result_callback, Geofence::Result::TOO_MANY_GEOFENCE_ITEMS);
-    } else {
-        LogDebug() << "Error: unknown geofence ack: " << int(mission_ack.type);
-        report_geofence_result(_result_callback, Geofence::Result::ERROR);
-    }
-}
-
-void GeofenceImpl::assemble_mavlink_messages(
-    const std::vector<std::shared_ptr<Geofence::Polygon>>& polygons)
-{
-    // TODO: delete all entries first
-    _mavlink_geofence_item_messages.clear();
-
+    uint16_t sequence = 0;
     for (auto& polygon : polygons) {
         uint16_t command;
         switch (polygon->type) {
@@ -183,64 +55,70 @@ void GeofenceImpl::assemble_mavlink_messages(
                 command = MAV_CMD_NAV_FENCE_POLYGON_VERTEX_EXCLUSION;
                 break;
             default:
-                LogDebug() << "Unknown type";
-                return;
+                LogErr() << "Unknown type";
+                continue;
         }
 
         for (auto& point : polygon->points) {
-            std::shared_ptr<mavlink_message_t> message(new mavlink_message_t());
-            mavlink_msg_mission_item_int_pack(
-                _parent->get_own_system_id(),
-                _parent->get_own_component_id(),
-                message.get(),
-                _parent->get_system_id(),
-                _parent->get_autopilot_id(),
-                _mavlink_geofence_item_messages.size(),
-                MAV_FRAME_GLOBAL_INT,
-                command,
-                0, // current
-                0, // autocontinue
-                float(polygon->points.size()), // vertex count
-                0.0f,
-                0.0f,
-                0.0f,
-                int32_t(std::round(point.latitude_deg * 1e7)),
-                int32_t(std::round(point.longitude_deg * 1e7)),
-                0.0f,
-                MAV_MISSION_TYPE_FENCE);
-            _mavlink_geofence_item_messages.push_back(message);
+            // FIXME: check if these two  make sense.
+            const uint8_t current = (sequence == 0 ? 1 : 0);
+            const uint8_t autocontinue = 0;
+            const float param1 = float(polygon->points.size());
+
+            items.push_back(
+                MAVLinkMissionTransfer::ItemInt{sequence,
+                                                MAV_FRAME_GLOBAL_INT,
+                                                command,
+                                                current,
+                                                autocontinue,
+                                                param1,
+                                                0.0f,
+                                                0.0f,
+                                                0.0f,
+                                                int32_t(std::round(point.latitude_deg * 1e7)),
+                                                int32_t(std::round(point.longitude_deg * 1e7)),
+                                                0.0f,
+                                                MAV_MISSION_TYPE_FENCE});
+            ++sequence;
         }
     }
+    return items;
 }
 
-void GeofenceImpl::timeout_happened()
+Geofence::Result GeofenceImpl::convert_result(MAVLinkMissionTransfer::Result result)
 {
-    LogDebug() << "timeout happened";
-    _active = false;
-
-    report_geofence_result(_result_callback, Geofence::Result::TIMEOUT);
-}
-
-void GeofenceImpl::send_geofence_item(uint16_t seq)
-{
-    LogDebug() << "Send geofence item " << int(seq);
-    if (seq >= _mavlink_geofence_item_messages.size()) {
-        LogDebug() << "Geofence item requested out of bounds.";
-        return;
+    switch (result) {
+        case MAVLinkMissionTransfer::Result::Success:
+            return Geofence::Result::SUCCESS;
+        case MAVLinkMissionTransfer::Result::ConnectionError:
+            return Geofence::Result::ERROR; // FIXME
+        case MAVLinkMissionTransfer::Result::Denied:
+            return Geofence::Result::ERROR; // FIXME
+        case MAVLinkMissionTransfer::Result::TooManyMissionItems:
+            return Geofence::Result::TOO_MANY_GEOFENCE_ITEMS;
+        case MAVLinkMissionTransfer::Result::Timeout:
+            return Geofence::Result::TIMEOUT;
+        case MAVLinkMissionTransfer::Result::Unsupported:
+            return Geofence::Result::ERROR; // FIXME
+        case MAVLinkMissionTransfer::Result::UnsupportedFrame:
+            return Geofence::Result::ERROR; // FIXME
+        case MAVLinkMissionTransfer::Result::NoMissionAvailable:
+            return Geofence::Result::INVALID_ARGUMENT; // FIXME
+        case MAVLinkMissionTransfer::Result::Cancelled:
+            return Geofence::Result::ERROR; // FIXME
+        case MAVLinkMissionTransfer::Result::MissionTypeNotConsistent:
+            return Geofence::Result::INVALID_ARGUMENT; // FIXME
+        case MAVLinkMissionTransfer::Result::InvalidSequence:
+            return Geofence::Result::INVALID_ARGUMENT; // FIXME
+        case MAVLinkMissionTransfer::Result::CurrentInvalid:
+            return Geofence::Result::INVALID_ARGUMENT; // FIXME
+        case MAVLinkMissionTransfer::Result::ProtocolError:
+            return Geofence::Result::ERROR; // FIXME
+        case MAVLinkMissionTransfer::Result::InvalidParam:
+            return Geofence::Result::INVALID_ARGUMENT; // FIXME
+        default:
+            return Geofence::Result::UNKNOWN;
     }
-
-    _parent->send_message(*_mavlink_geofence_item_messages.at(seq));
-}
-
-void GeofenceImpl::report_geofence_result(
-    const Geofence::result_callback_t& callback, Geofence::Result result)
-{
-    if (callback == nullptr) {
-        LogDebug() << "Callback is not set";
-        return;
-    }
-
-    callback(result);
 }
 
 } // namespace mavsdk
--- src/plugins/geofence/geofence_impl.h
+++ src/plugins/geofence/geofence_impl.h
@@ -31,27 +31,10 @@ public:
     const GeofenceImpl& operator=(const GeofenceImpl&) = delete;
 
 private:
-    void assemble_mavlink_messages(const std::vector<std::shared_ptr<Geofence::Polygon>>& polygons);
+    std::vector<MAVLinkMissionTransfer::ItemInt>
+    assemble_items(const std::vector<std::shared_ptr<Geofence::Polygon>>& polygons);
 
-    void timeout_happened();
-
-    void process_mission_request(const mavlink_message_t& message);
-    void process_mission_request_int(const mavlink_message_t& message);
-    void process_mission_ack(const mavlink_message_t& message);
-    void send_geofence_item(uint16_t seq);
-
-    static void
-    report_geofence_result(const Geofence::result_callback_t& callback, Geofence::Result result);
-
-    void receive_command_result(
-        MAVLinkCommands::Result command_result, const Geofence::result_callback_t& callback);
-
-    Geofence::result_callback_t _result_callback = nullptr;
-
-    std::vector<std::shared_ptr<mavlink_message_t>> _mavlink_geofence_item_messages;
-
-    void* _timeout_cookie{nullptr};
-    std::atomic<bool> _active{false};
+    static Geofence::Result convert_result(MAVLinkMissionTransfer::Result result);
 };
 
 } // namespace mavsdk
