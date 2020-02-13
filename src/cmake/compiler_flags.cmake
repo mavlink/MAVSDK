de4a2ef436bc379cf709210a06f67bcbb5a00a3c
Julian Oes <julian@oes.ch>
2020-02-13 17:10:14 +0100

    cmake: disable false positives with GCC 4.8.3

--- src/cmake/compiler_flags.cmake
+++ src/cmake/compiler_flags.cmake
@@ -29,7 +29,7 @@ else()
 
     if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
         if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS 5)
-            set(warnings "${warnings} -Wno-shadow")
+            set(warnings "${warnings} -Wno-shadow -Wno-effc++")
         endif()
         if(CMAKE_CXX_COMPILER_VERSION VERSION_GREATER 6)
             set(warnings "${warnings} -Wduplicated-cond -Wnull-dereference")
