c86fb6829d197ea5529bb79ec884b11ed349f53b
Jonas Vautherin <jonas.vautherin@gmail.com>
2020-03-21 20:30:59 +0100

    Add templates for action.cpp and the corresponding generated file

--- src/plugins/action/action.cpp
+++ src/plugins/action/action.cpp
@@ -7,9 +7,20 @@ Action::Action(System& system) : PluginBase(), _impl{new ActionImpl(system)} {}
 
 Action::~Action() {}
 
+
+void Action::arm_async(const result_callback_t callback)
+{
+    _impl->arm_async(callback);
+}
+
 Action::Result Action::arm() const
 {
     return _impl->arm();
+}
+
+void Action::disarm_async(const result_callback_t callback)
+{
+    _impl->disarm_async(callback);
 }
 
 Action::Result Action::disarm() const
@@ -17,19 +28,9 @@ Action::Result Action::disarm() const
     return _impl->disarm();
 }
 
-Action::Result Action::kill() const
+void Action::takeoff_async(const result_callback_t callback)
 {
-    return _impl->kill();
-}
-
-Action::Result Action::reboot() const
-{
-    return _impl->reboot();
-}
-
-Action::Result Action::shutdown() const
-{
-    return _impl->shutdown();
+    _impl->takeoff_async(callback);
 }
 
 Action::Result Action::takeoff() const
@@ -37,15 +38,49 @@ Action::Result Action::takeoff() const
     return _impl->takeoff();
 }
 
+void Action::land_async(const result_callback_t callback)
+{
+    _impl->land_async(callback);
+}
+
 Action::Result Action::land() const
 {
     return _impl->land();
 }
 
-Action::Result Action::goto_location(
-    double latitude_deg, double longitude_deg, float altitude_amsl_m, float yaw_deg)
+void Action::reboot_async(const result_callback_t callback)
 {
-    return _impl->goto_location(latitude_deg, longitude_deg, altitude_amsl_m, yaw_deg);
+    _impl->reboot_async(callback);
+}
+
+Action::Result Action::reboot() const
+{
+    return _impl->reboot();
+}
+
+void Action::shutdown_async(const result_callback_t callback)
+{
+    _impl->shutdown_async(callback);
+}
+
+Action::Result Action::shutdown() const
+{
+    return _impl->shutdown();
+}
+
+void Action::kill_async(const result_callback_t callback)
+{
+    _impl->kill_async(callback);
+}
+
+Action::Result Action::kill() const
+{
+    return _impl->kill();
+}
+
+void Action::return_to_launch_async(const result_callback_t callback)
+{
+    _impl->return_to_launch_async(callback);
 }
 
 Action::Result Action::return_to_launch() const
@@ -53,9 +88,29 @@ Action::Result Action::return_to_launch() const
     return _impl->return_to_launch();
 }
 
+void Action::goto_location_async(double latitude_deg, double longitude_deg, float absolute_altitude_m, float yaw_deg, const result_callback_t callback)
+{
+    _impl->goto_location_async(latitude_deg, longitude_deg, absolute_altitude_m, yaw_deg, callback);
+}
+
+Action::Result Action::goto_location(double latitude_deg, double longitude_deg, float absolute_altitude_m, float yaw_deg) const
+{
+    return _impl->goto_location(latitude_deg, longitude_deg, absolute_altitude_m, yaw_deg);
+}
+
+void Action::transition_to_fixedwing_async(const result_callback_t callback)
+{
+    _impl->transition_to_fixedwing_async(callback);
+}
+
 Action::Result Action::transition_to_fixedwing() const
 {
     return _impl->transition_to_fixedwing();
+}
+
+void Action::transition_to_multicopter_async(const result_callback_t callback)
+{
+    _impl->transition_to_multicopter_async(callback);
 }
 
 Action::Result Action::transition_to_multicopter() const
@@ -63,49 +118,9 @@ Action::Result Action::transition_to_multicopter() const
     return _impl->transition_to_multicopter();
 }
 
-void Action::arm_async(result_callback_t callback)
+void Action::get_takeoff_altitude_async(const altitude_callback_t callback)
 {
-    _impl->arm_async(callback);
-}
-
-void Action::disarm_async(result_callback_t callback)
-{
-    _impl->disarm_async(callback);
-}
-
-void Action::kill_async(result_callback_t callback)
-{
-    _impl->kill_async(callback);
-}
-
-void Action::takeoff_async(result_callback_t callback)
-{
-    _impl->takeoff_async(callback);
-}
-
-void Action::land_async(result_callback_t callback)
-{
-    _impl->land_async(callback);
-}
-
-void Action::return_to_launch_async(result_callback_t callback)
-{
-    _impl->return_to_launch_async(callback);
-}
-
-void Action::transition_to_multicopter_async(result_callback_t callback)
-{
-    _impl->transition_to_multicopter_async(callback);
-}
-
-void Action::transition_to_fixedwing_async(result_callback_t callback)
-{
-    _impl->transition_to_fixedwing_async(callback);
-}
-
-Action::Result Action::set_takeoff_altitude(float relative_altitude_m)
-{
-    return _impl->set_takeoff_altitude(relative_altitude_m);
+    _impl->get_takeoff_altitude_async(callback);
 }
 
 std::pair<Action::Result, float> Action::get_takeoff_altitude() const
@@ -113,55 +128,92 @@ std::pair<Action::Result, float> Action::get_takeoff_altitude() const
     return _impl->get_takeoff_altitude();
 }
 
-Action::Result Action::set_max_speed(float speed_m_s)
+void Action::set_takeoff_altitude_async(float altitude, const result_callback_t callback)
 {
-    return _impl->set_max_speed(speed_m_s);
+    _impl->set_takeoff_altitude_async(altitude, callback);
 }
 
-std::pair<Action::Result, float> Action::get_max_speed() const
+Action::Result Action::set_takeoff_altitude(float altitude) const
 {
-    return _impl->get_max_speed();
+    return _impl->set_takeoff_altitude(altitude);
 }
 
-Action::Result Action::set_return_to_launch_return_altitude(float relative_altitude_m)
+void Action::get_maximum_speed_async(const speed_callback_t callback)
 {
-    return _impl->set_return_to_launch_return_altitude(relative_altitude_m);
+    _impl->get_maximum_speed_async(callback);
 }
 
-std::pair<Action::Result, float> Action::get_return_to_launch_return_altitude() const
+std::pair<Action::Result, float> Action::get_maximum_speed() const
 {
-    return _impl->get_return_to_launch_return_altitude();
+    return _impl->get_maximum_speed();
 }
+
+void Action::set_maximum_speed_async(float speed, const result_callback_t callback)
+{
+    _impl->set_maximum_speed_async(speed, callback);
+}
+
+Action::Result Action::set_maximum_speed(float speed) const
+{
+    return _impl->set_maximum_speed(speed);
+}
+
+void Action::get_return_to_launch_altitude_async(const relative_altitude_m_callback_t callback)
+{
+    _impl->get_return_to_launch_altitude_async(callback);
+}
+
+std::pair<Action::Result, float> Action::get_return_to_launch_altitude() const
+{
+    return _impl->get_return_to_launch_altitude();
+}
+
+void Action::set_return_to_launch_altitude_async(float relative_altitude_m, const result_callback_t callback)
+{
+    _impl->set_return_to_launch_altitude_async(relative_altitude_m, callback);
+}
+
+Action::Result Action::set_return_to_launch_altitude(float relative_altitude_m) const
+{
+    return _impl->set_return_to_launch_altitude(relative_altitude_m);
+}
+
+
+
 
 const char* Action::result_str(Action::Result result)
 {
     switch (result) {
-        case Action::Result::SUCCESS:
-            return "Success";
-        case Action::Result::NO_SYSTEM:
-            return "No system";
-        case Action::Result::CONNECTION_ERROR:
+        case Action::Result::Unknown:
+            return "Unknown error";
+        case Action::Result::Success:
+            return "Success: the action command was accepted by the vehicle";
+        case Action::Result::NoSystem:
+            return "No system is connected";
+        case Action::Result::ConnectionError:
             return "Connection error";
-        case Action::Result::BUSY:
-            return "Busy";
-        case Action::Result::COMMAND_DENIED:
-            return "Command denied";
-        case Action::Result::COMMAND_DENIED_LANDED_STATE_UNKNOWN:
-            return "Command denied, landed state is unknown";
-        case Action::Result::COMMAND_DENIED_NOT_LANDED:
-            return "Command denied, not landed";
-        case Action::Result::TIMEOUT:
-            return "Timeout";
-        case Action::Result::VTOL_TRANSITION_SUPPORT_UNKNOWN:
-            return "VTOL transition unknown";
-        case Action::Result::NO_VTOL_TRANSITION_SUPPORT:
-            return "No VTOL transition support";
-        case Action::Result::PARAMETER_ERROR:
-            return "Parameter error";
-        case Action::Result::UNKNOWN:
+        case Action::Result::Busy:
+            return "Vehicle is busy";
+        case Action::Result::CommandDenied:
+            return "Command refused by vehicle";
+        case Action::Result::CommandDeniedLandedStateUnknown:
+            return "Command refused because landed state is unknown";
+        case Action::Result::CommandDeniedNotLanded:
+            return "Command refused because vehicle not landed";
+        case Action::Result::Timeout:
+            return "Request timed out";
+        case Action::Result::VtolTransitionSupportUnknown:
+            return "Hybrid/VTOL transition refused because VTOL support is unknown";
+        case Action::Result::NoVtolTransitionSupport:
+            return "Vehicle does not support hybrid/VTOL transitions";
+        case Action::Result::ParameterError:
+            return "Error getting or setting parameter";
         default:
             return "Unknown";
     }
 }
+
+
+
 
 } // namespace mavsdk
