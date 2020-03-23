266e23d7af42f165a07862b418805ae8c9cf8c4a
Hamish Willee <hamishwillee@gmail.com>
2020-03-23 15:48:55 +1100

    Camera: Fix up docs link

--- src/plugins/camera/include/plugins/camera/camera.h
+++ src/plugins/camera/include/plugins/camera/camera.h
@@ -25,7 +25,7 @@ class System;
 
 /**
  * @brief The Camera class can be used to manage cameras that implement the
- * MAVLink Camera Protocol: https://mavlink.io/en/protocol/camera.html.
+ * MAVLink Camera Protocol: https://mavlink.io/en/services/camera.html.
  *
  * Currently only a single camera is supported.
  * When multiple cameras are supported the plugin will need to be
