3252f0553b22dc5721f2a0a95f7a6a869f0c3ad0
Jonas Vautherin <jonas.vautherin@gmail.com>
2020-04-15 15:02:25 +0200

    print_mode -> observe_mode because print suggests no side effects

--- src/integration_tests/telemetry_modes.cpp
+++ src/integration_tests/telemetry_modes.cpp
@@ -6,7 +6,7 @@
 
 using namespace mavsdk;
 
-void print_mode(Telemetry::FlightMode flight_mode);
+void observe_mode(Telemetry::FlightMode flight_mode);
 static Telemetry::FlightMode _flight_mode = Telemetry::FlightMode::Unknown;
 
 TEST_F(SitlTest, TelemetryFlightModes)
@@ -22,7 +22,7 @@ TEST_F(SitlTest, TelemetryFlightModes)
     auto telemetry = std::make_shared<Telemetry>(system);
     auto action = std::make_shared<Action>(system);
 
-    telemetry->flight_mode_async(std::bind(&print_mode, std::placeholders::_1));
+    telemetry->flight_mode_async(std::bind(&observe_mode, std::placeholders::_1));
 
     while (!telemetry->health_all_ok()) {
         std::cout << "waiting for system to be ready" << std::endl;
@@ -39,7 +39,7 @@ TEST_F(SitlTest, TelemetryFlightModes)
     ASSERT_EQ(_flight_mode, Telemetry::FlightMode::Land);
 }
 
-void print_mode(Telemetry::FlightMode flight_mode)
+void observe_mode(Telemetry::FlightMode flight_mode)
 {
     std::cout << "Got FlightMode: " << flight_mode << std::endl;
     _flight_mode = flight_mode;
