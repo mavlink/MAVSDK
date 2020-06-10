e8366c9ec77142bdb0d3e28b441566046d16c78b
Julian Oes <julian@oes.ch>
2020-06-10 18:44:58 +0200

    Add failure plugin

---
+++ src/plugins/failure/failure_impl.cpp
@@ -0,0 +1,87 @@
+#include "failure_impl.h"
+
+namespace mavsdk {
+
+FailureImpl::FailureImpl(System& system) : PluginImplBase(system)
+{
+    _parent->register_plugin(this);
+}
+
+FailureImpl::~FailureImpl()
+{
+    _parent->unregister_plugin(this);
+}
+
+void FailureImpl::init() {}
+
+void FailureImpl::deinit() {}
+
+void FailureImpl::enable()
+{
+    _parent->get_param_int_async(
+        "SYS_FAILURE_EN",
+        [this](MAVLinkParameters::Result result, int32_t value) {
+            if (result == MAVLinkParameters::Result::Success) {
+                if (value == 1) {
+                    _enabled = EnabledState::Enabled;
+                } else if (value == 0) {
+                    _enabled = EnabledState::Disabled;
+                } else {
+                    _enabled = EnabledState::Unknown;
+                }
+            } else {
+                _enabled = EnabledState::Unknown;
+            }
+        },
+        this);
+}
+
+void FailureImpl::disable()
+{
+    _enabled = EnabledState::Init;
+}
+
+Failure::Result
+FailureImpl::inject(Failure::FailureUnit failure_unit, Failure::FailureType failure_type)
+{
+    while (_enabled == EnabledState::Init) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+
+    // If the param is unknown we ignore it and try anyway.
+    if (_enabled == EnabledState::Disabled) {
+        return Failure::Result::Disabled;
+    }
+
+    MAVLinkCommands::CommandLong command{};
+
+    command.command = MAV_CMD_INJECT_FAILURE;
+    command.params.param1 = static_cast<float>(failure_unit);
+    command.params.param2 = static_cast<float>(failure_type);
+    command.target_component_id = _parent->get_autopilot_id();
+
+    return failure_result_from_command_result(_parent->send_command(command));
+}
+
+Failure::Result
+FailureImpl::failure_result_from_command_result(MAVLinkCommands::Result command_result)
+{
+    switch (command_result) {
+        case MAVLinkCommands::Result::Success:
+            return Failure::Result::Success;
+        case MAVLinkCommands::Result::NoSystem:
+            return Failure::Result::NoSystem;
+        case MAVLinkCommands::Result::ConnectionError:
+            return Failure::Result::ConnectionError;
+        case MAVLinkCommands::Result::CommandDenied:
+            return Failure::Result::Denied;
+        case MAVLinkCommands::Result::Unsupported:
+            return Failure::Result::Unsupported;
+        case MAVLinkCommands::Result::Timeout:
+            return Failure::Result::Timeout;
+        default:
+            return Failure::Result::Unknown;
+    }
+}
+
+} // namespace mavsdk
