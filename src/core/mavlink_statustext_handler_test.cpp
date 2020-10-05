41a46bef11028f3af6f4252ff96143907e78421b
Julian Oes <julian@oes.ch>
2020-10-05 15:38:53 +0200

    build_system: only use pragma with gcc
    This should fix the MSVC build.

--- src/core/mavlink_statustext_handler_test.cpp
+++ src/core/mavlink_statustext_handler_test.cpp
@@ -7,8 +7,10 @@ using namespace mavsdk;
 // We need to use strncpy without zero termination, so with possible
 // stringop-truncation here, on purpose because the MAVLink message structure
 // of statustext.text does not include the zero termination.
+#if defined(__GNUC__)
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wstringop-truncation"
+#endif // defined(__GNUC__)
 
 TEST(MavlinkStatustextHandler, Severities)
 {
@@ -266,4 +268,6 @@ TEST(MavlinkStatustextHandler, MultiStatustextConsecutive)
     }
 }
 
+#if defined(__GNUC__)
 #pragma GCC diagnostic pop
+#endif // defined(__GNUC__)
