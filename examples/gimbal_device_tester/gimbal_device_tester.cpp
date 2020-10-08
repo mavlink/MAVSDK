03c5723917f3e984aa4dada7afa9e459ccb155a8
Julian Oes <julian@oes.ch>
2020-10-08 16:08:44 +0200

    gimbal_device_tester: add yaw follow/lock tests

--- examples/gimbal_device_tester/gimbal_device_tester.cpp
+++ examples/gimbal_device_tester/gimbal_device_tester.cpp
@@ -14,15 +14,28 @@
 
 using namespace mavsdk;
 
-struct Attitude {
+struct GimbalAttitude {
     mutable std::mutex mutex {};
     float roll_deg {NAN};
     float pitch_deg {NAN};
     float yaw_deg {NAN};
 };
 
+struct VehicleAttitude {
+    mutable std::mutex mutex {};
+    float roll_deg {0.0f};
+    float pitch_deg {0.0f};
+    float yaw_deg {0.0f};
+};
+
+enum class Mode {
+    Follow,
+    Lock
+};
+
 struct AttitudeSetpoint {
     mutable std::mutex mutex {};
+    Mode mode {Mode::Follow};
     float roll_deg {0.0f};
     float pitch_deg {0.0f};
     float yaw_deg {0.0f};
@@ -30,6 +43,9 @@ struct AttitudeSetpoint {
 
 static void usage(const std::string& bin_name);
 static bool request_gimbal_device_information(MavlinkPassthrough& mavlink_passthrough);
+static void subscribe_to_gimbal_device_attitude_status(MavlinkPassthrough& mavlink_passthrough, GimbalAttitude& gimbal_attitude);
+static bool test_device_information(MavlinkPassthrough& mavlink_passthrough);
+static bool wait_for_yaw_estimator_to_converge(const GimbalAttitude& gimbal_attitude);
 static float degrees(float radians);
 static float radians(float radians);
 
@@ -40,9 +56,11 @@ class Sender
 public:
     explicit Sender(
             MavlinkPassthrough& mavlink_passthrough,
-            AttitudeSetpoint &attitude_setpoint) :
+            AttitudeSetpoint& attitude_setpoint,
+            VehicleAttitude& vehicle_attitude) :
         _mavlink_passthrough(mavlink_passthrough),
         _attitude_setpoint(attitude_setpoint),
+        _vehicle_attitude(vehicle_attitude),
         _thread(&Sender::run, this)
     {}
 
@@ -64,7 +82,15 @@ public:
 private:
     void send_autopilot_state_for_gimbal_device()
     {
-        const float q[4] {1, 0, 0, 0};
+        float q[4];
+        {
+            std::lock_guard<std::mutex> lock(_vehicle_attitude.mutex);
+            mavlink_euler_to_quaternion(
+                    radians(_vehicle_attitude.roll_deg),
+                    radians(_vehicle_attitude.pitch_deg),
+                    radians(_vehicle_attitude.yaw_deg),
+                    q);
+        }
 
         const uint16_t estimator_status =
             ESTIMATOR_ATTITUDE |
@@ -99,7 +125,7 @@ private:
 
     void send_gimbal_device_set_attitude()
     {
-        const uint16_t flags =
+        uint16_t flags =
             GIMBAL_DEVICE_FLAGS_ROLL_LOCK |
             GIMBAL_DEVICE_FLAGS_PITCH_LOCK;
 
@@ -111,6 +137,10 @@ private:
                 radians(_attitude_setpoint.pitch_deg),
                 radians(_attitude_setpoint.yaw_deg),
                 q);
+
+            if (_attitude_setpoint.mode == Mode::Lock) {
+                flags |= GIMBAL_DEVICE_FLAGS_YAW_LOCK;
+            }
         }
 
         mavlink_message_t message;
@@ -132,18 +162,135 @@ private:
 
     MavlinkPassthrough& _mavlink_passthrough;
     AttitudeSetpoint& _attitude_setpoint;
+    VehicleAttitude& _vehicle_attitude;
     std::thread _thread;
     std::atomic<bool> _should_exit {false};
 };
 
-static void subscribe_to_gimbal_device_attitude_status(MavlinkPassthrough& mavlink_passthrough, Attitude& attitude);
+class Tester
+{
+public:
+    explicit Tester(AttitudeSetpoint& attitude_setpoint, VehicleAttitude& vehicle_attitude, GimbalAttitude& gimbal_attitude) :
+        _attitude_setpoint(attitude_setpoint),
+        _vehicle_attitude(vehicle_attitude),
+        _gimbal_attitude(gimbal_attitude)
+    {}
 
-static bool test_device_information(MavlinkPassthrough& mavlink_passthrough);
-static bool wait_for_yaw_estimator_to_converge(const Attitude& attitude);
-static bool test_pitch(const Attitude& attitude, AttitudeSetpoint& attitude_setpoint);
-static bool test_yaw_follow(MavlinkPassthrough& mavlink_passthrough, const Attitude& attitude);
-static bool test_yaw_lock(MavlinkPassthrough& mavlink_passthrough, const Attitude& attitude);
-static bool test_pitch_yaw(const std::string& description, float pitch_deg, float yaw_deg, const Attitude& attitude, AttitudeSetpoint& attitude_setpoint);
+    bool test_pitch()
+    {
+        return test_pitch_yaw("Look forward", 0.0f, 0.0f, Mode::Follow) &&
+            test_pitch_yaw("Tilt 45 degrees down", -45.0f, 0.0f, Mode::Follow) &&
+            test_pitch_yaw("Tilt 90 degrees down", -90.0f, 0.0f, Mode::Follow) &&
+            test_pitch_yaw("Tilt 20 degrees up", 20.0f, 0.0f, Mode::Follow) &&
+            test_pitch_yaw("Look forward again", 0.0f, 0.0f, Mode::Follow);
+    }
+
+    bool test_yaw_follow()
+    {
+        if (!test_pitch_yaw("Switch to follow mode", 0.0f, 0.0f, Mode::Follow)) {
+            return false;
+        }
+
+        std::cout << test_prefix << "Yaw 20 degrees to the right... ";
+        {
+            std::lock_guard<std::mutex> lock(_vehicle_attitude.mutex);
+            _vehicle_attitude.yaw_deg = 20.0f;
+        }
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        std::cout << "DONE\n";
+
+        return test_pitch_yaw("Look to the right", 0.0f, 90.0f, Mode::Follow) &&
+            test_pitch_yaw("Look 60 degrees to the left", 0.0f, -90.0f, Mode::Follow) &&
+            test_pitch_yaw("Tilt 45 degrees down", -45.0f, -90.0f, Mode::Follow) &&
+            test_pitch_yaw("Look 60 degrees to the right", -45.0f, 90.0f, Mode::Follow) &&
+            test_pitch_yaw("Look forward again", 0.0f, 0.0f, Mode::Follow);
+    }
+
+    bool test_yaw_lock()
+    {
+        if (!test_pitch_yaw("Switch to lock mode", 0.0f, 0.0f, Mode::Follow)) {
+            return false;
+        }
+
+        std::cout << test_prefix << "Yaw 30 degrees to the left... ";
+        {
+            std::lock_guard<std::mutex> lock(_vehicle_attitude.mutex);
+            _vehicle_attitude.yaw_deg = -30.0f;
+        }
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        std::cout << "DONE\n";
+
+        return test_pitch_yaw("Look to the right", 0.0f, 90.0f, Mode::Lock) &&
+            test_pitch_yaw("Look 60 degrees to the left", 0.0f, -90.0f, Mode::Lock) &&
+            test_pitch_yaw("Tilt 45 degrees down", -45.0f, -90.0f, Mode::Lock) &&
+            test_pitch_yaw("Look 60 degrees to the right", -45.0f, 90.0f, Mode::Lock) &&
+            test_pitch_yaw("Look forward again", 0.0f, 0.0f, Mode::Lock);
+    }
+
+    bool test_pitch_yaw(const std::string& description, float pitch_deg, float yaw_deg, Mode mode)
+    {
+        std::cout << test_prefix << description << "... " << std::flush;
+        {
+            std::lock_guard<std::mutex> lock(_attitude_setpoint.mutex);
+            _attitude_setpoint.pitch_deg = pitch_deg;
+            _attitude_setpoint.yaw_deg = yaw_deg;
+            _attitude_setpoint.mode = mode;
+        }
+
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+
+        const float margin_deg = 2.0f;
+
+        if (mode == Mode::Follow) {
+            std::lock_guard<std::mutex> lock(_vehicle_attitude.mutex);
+            yaw_deg += _vehicle_attitude.yaw_deg;
+        }
+
+        std::lock_guard<std::mutex> lock(_gimbal_attitude.mutex);
+
+        bool pitch_fail = false;
+        bool yaw_fail = false;
+
+        // TODO: We should do this check in quaternion to avoid gimbal locks.
+        //       For now we avoid the check close to it.
+
+        if (_gimbal_attitude.pitch_deg > pitch_deg + margin_deg) {
+            pitch_fail = true;
+        } else if (_gimbal_attitude.pitch_deg < pitch_deg - margin_deg) {
+            pitch_fail = true;
+        }
+
+        if (_gimbal_attitude.pitch_deg < 80.0f && _gimbal_attitude.pitch_deg > -80.0) {
+            if (_gimbal_attitude.yaw_deg > yaw_deg + margin_deg) {
+                yaw_fail = true;
+            } else if (_gimbal_attitude.yaw_deg < yaw_deg - margin_deg) {
+                yaw_fail = true;
+            }
+        }
+
+        if (pitch_fail || yaw_fail) {
+            std::cout << "FAIL\n";
+        } else {
+            std::cout << "PASS\n";
+        }
+
+        if (pitch_fail) {
+            std::cout << "-> pitch is " << _gimbal_attitude.pitch_deg << " deg instead of " << pitch_deg << " deg\n";
+        }
+
+        if (yaw_fail) {
+            std::cout << "-> yaw is " << _gimbal_attitude.yaw_deg << " deg instead of " << yaw_deg << " deg\n";
+        }
+
+        return !(pitch_fail || yaw_fail);
+    }
+
+
+private:
+    AttitudeSetpoint& _attitude_setpoint;
+    VehicleAttitude& _vehicle_attitude;
+    GimbalAttitude& _gimbal_attitude;
+};
 
 int main(int argc, char** argv)
 {
@@ -189,38 +336,41 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    Attitude attitude {};
-    subscribe_to_gimbal_device_attitude_status(mavlink_passthrough, attitude);
+    GimbalAttitude gimbal_attitude {};
+    subscribe_to_gimbal_device_attitude_status(mavlink_passthrough, gimbal_attitude);
 
     AttitudeSetpoint attitude_setpoint {};
-    Sender sender(mavlink_passthrough, attitude_setpoint);
+    VehicleAttitude vehicle_attitude {};
+    Sender sender(mavlink_passthrough, attitude_setpoint, vehicle_attitude);
 
-    if (!wait_for_yaw_estimator_to_converge(attitude)) {
+    if (!wait_for_yaw_estimator_to_converge(gimbal_attitude)) {
         return 1;
     }
 
-    if (!test_pitch(attitude, attitude_setpoint)) {
+    Tester tester(attitude_setpoint, vehicle_attitude, gimbal_attitude);
+
+    if (!tester.test_pitch()) {
         return 1;
     }
 
-    if (!test_yaw_follow(mavlink_passthrough, attitude)) {
+    if (!tester.test_yaw_follow()) {
         return 1;
     }
 
-    if (!test_yaw_lock(mavlink_passthrough, attitude)) {
+    if (!tester.test_yaw_lock()) {
         return 1;
     }
 
     return 0;
 }
 
-bool wait_for_yaw_estimator_to_converge(const Attitude& attitude)
+bool wait_for_yaw_estimator_to_converge(const GimbalAttitude& gimbal_attitude)
 {
     std::cout << test_prefix << "Waiting for yaw estimator to converge..." << std::flush;
     for (unsigned i = 0; i < 200; ++i) {
         {
-            std::lock_guard<std::mutex> lock(attitude.mutex);
-            if (attitude.yaw_deg < 1.0f && attitude.yaw_deg > -1.0f) {
+            std::lock_guard<std::mutex> lock(gimbal_attitude.mutex);
+            if (gimbal_attitude.yaw_deg < 1.0f && gimbal_attitude.yaw_deg > -1.0f) {
                 std::cout << "PASS" << std::endl;
                 return true;
             }
@@ -264,77 +414,6 @@ bool test_device_information(MavlinkPassthrough& mavlink_passthrough)
     return true;
 }
 
-bool test_pitch(const Attitude& attitude, AttitudeSetpoint& attitude_setpoint)
-{
-    return test_pitch_yaw("Look forward", 0.0f, 0.0f, attitude, attitude_setpoint) &&
-        test_pitch_yaw("Tilt 45 degrees down", -45.0f, 0.0f, attitude, attitude_setpoint) &&
-        test_pitch_yaw("Tilt 90 degrees down", -90.0f, 0.0f, attitude, attitude_setpoint) &&
-        test_pitch_yaw("Tilt 20 degrees up", 20.0f, 0.0f, attitude, attitude_setpoint) &&
-        test_pitch_yaw("Look forward again", 0.0f, 0.0f, attitude, attitude_setpoint);
-}
-
-bool test_yaw_follow(MavlinkPassthrough& mavlink_passthrough, const Attitude& attitude)
-{
-    return true;
-}
-
-bool test_yaw_lock(MavlinkPassthrough& mavlink_passthrough, const Attitude&  attitude)
-{
-    return true;
-}
-
-bool test_pitch_yaw(const std::string& description, float pitch_deg, float yaw_deg, const Attitude& attitude, AttitudeSetpoint& attitude_setpoint)
-{
-    std::cout << test_prefix << description << "... " << std::flush;
-    {
-        std::lock_guard<std::mutex> lock(attitude_setpoint.mutex);
-        attitude_setpoint.pitch_deg = pitch_deg;
-        attitude_setpoint.yaw_deg = yaw_deg;
-    }
-
-    std::this_thread::sleep_for(std::chrono::seconds(2));
-
-    const float margin_deg = 2.0f;
-
-    std::lock_guard<std::mutex> lock(attitude.mutex);
-
-    bool pitch_fail = false;
-    bool yaw_fail = false;
-
-    // TODO: We should do this check in quaternion to avoid gimbal locks.
-    //       For now we avoid the check close to it.
-
-    if (attitude.pitch_deg > pitch_deg + margin_deg) {
-        pitch_fail = true;
-    } else if (attitude.pitch_deg < pitch_deg - margin_deg) {
-        pitch_fail = true;
-    }
-
-    if (attitude.pitch_deg < 80.0f && attitude.pitch_deg > -80.0) {
-        if (attitude.yaw_deg > yaw_deg + margin_deg) {
-            yaw_fail = true;
-        } else if (attitude.yaw_deg < yaw_deg - margin_deg) {
-            yaw_fail = true;
-        }
-    }
-
-    if (pitch_fail || yaw_fail) {
-        std::cout << "FAIL\n";
-    } else {
-        std::cout << "PASS\n";
-    }
-
-    if (pitch_fail) {
-        std::cout << "-> pitch is " << attitude.pitch_deg << " deg instead of " << pitch_deg << " deg\n";
-    }
-
-    if (yaw_fail) {
-        std::cout << "-> yaw is " << attitude.yaw_deg << " deg instead of " << yaw_deg << " deg\n";
-    }
-
-    return !(pitch_fail || yaw_fail);
-}
-
 bool request_gimbal_device_information(MavlinkPassthrough& mavlink_passthrough)
 {
     MavlinkPassthrough::CommandLong command;
@@ -346,21 +425,21 @@ bool request_gimbal_device_information(MavlinkPassthrough& mavlink_passthrough)
     return (mavlink_passthrough.send_command_long(command) == MavlinkPassthrough::Result::Success);
 }
 
-void subscribe_to_gimbal_device_attitude_status(MavlinkPassthrough& mavlink_passthrough, Attitude& attitude)
+void subscribe_to_gimbal_device_attitude_status(MavlinkPassthrough& mavlink_passthrough, GimbalAttitude& gimbal_attitude)
 {
     mavlink_passthrough.subscribe_message_async(
         MAVLINK_MSG_ID_GIMBAL_DEVICE_ATTITUDE_STATUS,
-        [&attitude](const mavlink_message_t& message) {
+        [&gimbal_attitude](const mavlink_message_t& message) {
         mavlink_gimbal_device_attitude_status_t attitude_status;
         mavlink_msg_gimbal_device_attitude_status_decode(&message, &attitude_status);
 
         float roll_rad, pitch_rad, yaw_rad;
         mavlink_quaternion_to_euler(attitude_status.q, &roll_rad, &pitch_rad, &yaw_rad);
 
-        std::lock_guard<std::mutex> lock(attitude.mutex);
-        attitude.roll_deg = degrees(roll_rad);
-        attitude.pitch_deg = degrees(pitch_rad);
-        attitude.yaw_deg = degrees(yaw_rad);
+        std::lock_guard<std::mutex> lock(gimbal_attitude.mutex);
+        gimbal_attitude.roll_deg = degrees(roll_rad);
+        gimbal_attitude.pitch_deg = degrees(pitch_rad);
+        gimbal_attitude.yaw_deg = degrees(yaw_rad);
     });
 }
 
