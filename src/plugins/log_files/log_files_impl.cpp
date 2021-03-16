c0c189f6ab8e00a0229d4863daaff43aea98c51b
Jonas Vautherin <jonas.vautherin@gmail.com>
2021-03-16 17:52:11 +0100

    Fix style

--- src/plugins/log_files/log_files_impl.cpp
+++ src/plugins/log_files/log_files_impl.cpp
@@ -8,13 +8,13 @@
 #include <cstring>
 
 #if __has_include(<filesystem>)
-  #include <filesystem>
-  namespace fs = std::filesystem;
+#include <filesystem>
+namespace fs = std::filesystem;
 #elif __has_include(<experimental/filesystem>)
-  #include <experimental/filesystem>
-  namespace fs = std::experimental::filesystem;
+#include <experimental/filesystem>
+namespace fs = std::experimental::filesystem;
 #else
-  error "Missing the <filesystem> header."
+error "Missing the <filesystem> header."
 #endif
 
 namespace mavsdk {
