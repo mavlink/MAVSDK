c84ba01f7f8b911752b99c52d2c9addd4f6d22ef
Julian Oes <julian@oes.ch>
2021-03-25 13:40:34 +0100

    Add ServerUtility plugin
    This adds a plugin to help with various general helpers for MAVSDK used
    onboard. For now it just helps sending statustexts.

---
+++ src/plugins/server_utility/server_utility_impl.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "plugins/server_utility/server_utility.h"
+#include "plugin_impl_base.h"
+#include <atomic>
+#include <optional>
+
+namespace mavsdk {
+
+class ServerUtilityImpl : public PluginImplBase {
+public:
+    explicit ServerUtilityImpl(System& system);
+    explicit ServerUtilityImpl(std::shared_ptr<System> system);
+    ~ServerUtilityImpl();
+
+    void init() override;
+    void deinit() override;
+
+    void enable() override;
+    void disable() override;
+
+    ServerUtility::Result send_status_text(ServerUtility::StatusTextType type, std::string text);
+
+    std::optional<MAV_SEVERITY>
+    mav_severity_from_status_text_type(ServerUtility::StatusTextType type);
+
+private:
+    std::atomic<uint16_t> _unique_send_id{1};
+};
+
+} // namespace mavsdk
