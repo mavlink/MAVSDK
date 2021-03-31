faadb15a08d91ed45f601f6eb8c314ac055fa479
DoppleGangster <bobet_cow@yahoo.com>
2021-03-31 11:57:52 -0400

    Add transponder plugin

---
+++ src/plugins/transponder/transponder_impl.cpp
@@ -0,0 +1,117 @@
+#include "transponder_impl.h"
+
+namespace mavsdk {
+
+TransponderImpl::TransponderImpl(System& system) : PluginImplBase(system)
+{
+    _parent->register_plugin(this);
+}
+
+TransponderImpl::TransponderImpl(std::shared_ptr<System> system) : PluginImplBase(system)
+{
+    _parent->register_plugin(this);
+}
+
+TransponderImpl::~TransponderImpl()
+{
+    _parent->unregister_plugin(this);
+}
+
+void TransponderImpl::init() {}
+
+void TransponderImpl::deinit() {}
+
+void TransponderImpl::enable() {}
+
+void TransponderImpl::disable() {}
+
+Transponder::Result TransponderImpl::set_rate_transponder(double rate_hz)
+{
+    return transponder_result_from_command_result(
+        _parent->set_msg_rate(MAVLINK_MSG_ID_ADSB_VEHICLE, rate_hz));
+}
+
+void TransponderImpl::set_rate_transponder_async(double rate_hz, const Transponder::ResultCallback callback)
+{
+    _parent->set_msg_rate_async(
+    MAVLINK_MSG_ID_ADSB_VEHICLE,
+    rate_hz,
+    std::bind(&TransponderImpl::command_result_callback, std::placeholders::_1, callback));
+}
+
+Transponder::AdsbVehicle
+TransponderImpl::transponder() const
+{
+    std::lock_guard<std::mutex> lock(_transponder_mutex);
+    return _transponder;
+}
+
+void TransponderImpl::subscribe_transponder(Transponder::TransponderCallback callback)
+{
+    std::lock_guard<std::mutex> lock(_subscription_mutex);
+    _transponder_subscription = callback;
+}
+
+void TransponderImpl::set_transponder(Transponder::AdsbVehicle transponder)
+{
+    std::lock_guard<std::mutex> lock(_transponder_mutex);
+    _transponder = transponder;
+}
+
+void TransponderImpl::process_transponder(const mavlink_message_t& message)
+{
+    mavlink_adsb_vehicle_t local_adsb_vehicle;
+    mavlink_msg_adsb_vehicle_decode(&message, &local_adsb_vehicle);
+
+    Transponder::AdsbVehicle adsbVehicle;
+    adsbVehicle.icao_address = local_adsb_vehicle.ICAO_address;
+    adsbVehicle.latitude_deg = local_adsb_vehicle.lat;
+    adsbVehicle.longitude_deg = local_adsb_vehicle.lon;
+    adsbVehicle.absolute_altitude_m = local_adsb_vehicle.altitude;
+    adsbVehicle.heading_deg = local_adsb_vehicle.heading;
+    adsbVehicle.horizontal_velocity_m_s = local_adsb_vehicle.hor_velocity;
+    adsbVehicle.vertical_velocity_m_s = local_adsb_vehicle.ver_velocity;
+    adsbVehicle.callsign = local_adsb_vehicle.callsign;
+    adsbVehicle.emitter_type = Transponder::AdsbEmitterType(local_adsb_vehicle.emitter_type);
+    adsbVehicle.squawk = local_adsb_vehicle.squawk;
+
+    set_transponder(adsbVehicle);
+
+    std::lock_guard<std::mutex> lock(_subscription_mutex);
+    if (_transponder_subscription) {
+        auto callback = _transponder_subscription;
+        auto arg = transponder();
+        _parent->call_user_callback([callback, arg]() { callback(arg); });
+    }
+}
+
+Transponder::Result
+TransponderImpl::transponder_result_from_command_result(MavlinkCommandSender::Result command_result)
+{
+    switch (command_result) {
+        case MavlinkCommandSender::Result::Success:
+            return Transponder::Result::Success;
+        case MavlinkCommandSender::Result::NoSystem:
+            return Transponder::Result::NoSystem;
+        case MavlinkCommandSender::Result::ConnectionError:
+            return Transponder::Result::ConnectionError;
+        case MavlinkCommandSender::Result::Busy:
+            return Transponder::Result::Busy;
+        case MavlinkCommandSender::Result::CommandDenied:
+            return Transponder::Result::CommandDenied;
+        case MavlinkCommandSender::Result::Timeout:
+            return Transponder::Result::Timeout;
+        default:
+            return Transponder::Result::Unknown;
+    }
+}
+
+void TransponderImpl::command_result_callback(
+    MavlinkCommandSender::Result command_result, const Transponder::ResultCallback& callback)
+{
+    Transponder::Result action_result = transponder_result_from_command_result(command_result);
+
+    callback(action_result);
+}
+
+} // namespace mavsdk
