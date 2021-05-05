b09f4474f748b836d509fa90fceefded88682202
Julian Oes <julian@oes.ch>
2021-05-05 16:52:12 +0200

    examples: modernize set_actuator example

--- examples/set_actuator/set_actuator.cpp
+++ examples/set_actuator/set_actuator.cpp
@@ -12,63 +12,75 @@
 
 using namespace mavsdk;
 
-#define ERROR_CONSOLE_TEXT "\033[31m" // Turn text on console red
-#define NORMAL_CONSOLE_TEXT "\033[0m" // Restore normal console color
-
 void usage(std::string bin_name)
 {
-    std::cout << NORMAL_CONSOLE_TEXT << "Usage :" << bin_name << " <connection_url>"
-              << " <actuator_index> <actuator_value>" << std::endl
-              << "Connection URL format should be :" << std::endl
-              << " For TCP : tcp://[server_host][:server_port]" << std::endl
-              << " For UDP : udp://[bind_host][:bind_port]" << std::endl
-              << " For Serial : serial:///path/to/serial/dev[:baudrate]" << std::endl
-              << "For example, to connect to the simulator use URL: udp://:14540" << std::endl;
+    std::cerr << "Usage :" << bin_name << " <connection_url> <actuator_index> <actuator_value>\n"
+              << "Connection URL format should be :\n"
+              << '\n'
+              << " For TCP : tcp://[server_host][:server_port]\n"
+              << '\n'
+              << " For UDP : udp://[bind_host][:bind_port]\n"
+              << '\n'
+              << " For Serial : serial:///path/to/serial/dev[:baudrate]\n"
+              << '\n'
+              << "For example, to connect to the simulator use URL: udp://:14540\n";
 }
 
 int main(int argc, char** argv)
 {
-    Mavsdk mavsdk;
-    std::string connection_url;
-    ConnectionResult connection_result;
-    int index;
-    float value;
-
-    if (argc == 4) {
-        connection_url = argv[1];
-        connection_result = mavsdk.add_any_connection(connection_url);
-        index = std::stod(argv[2]);
-        value = std::stof(argv[3]);
-    } else {
+    if (argc != 4) {
         usage(argv[0]);
         return 1;
     }
 
+    const std::string connection_url = argv[1];
+    const int index = std::stod(argv[2]);
+    const float value = std::stof(argv[3]);
+
+    Mavsdk mavsdk;
+    const ConnectionResult connection_result = mavsdk.add_any_connection(connection_url);
+
     if (connection_result != ConnectionResult::Success) {
-        std::cout << "Connection failed: " << connection_result << std::endl;
+        std::cerr << "Connection failed: " << connection_result << '\n';
         return 1;
     }
 
-    std::promise<void> prom;
-    std::future<void> fut = prom.get_future();
-    std::cout << "Waiting to discover system..." << std::endl;
-    mavsdk.register_on_discover([&prom](uint64_t /* uuid*/) { prom.set_value(); });
+    std::cout << "Waiting to discover system...\n";
+    auto prom = std::promise<std::shared_ptr<System>>{};
+    auto fut = prom.get_future();
 
-    if (fut.wait_for(std::chrono::seconds(2)) != std::future_status::ready) {
-        std::cout << "No device found, exiting." << std::endl;
+    // We wait for new systems to be discovered, once we find one that has an
+    // autopilot, we decide to use it.
+    mavsdk.subscribe_on_new_system([&mavsdk, &prom]() {
+        auto system = mavsdk.systems().back();
+
+        if (system->has_autopilot()) {
+            std::cout << "Discovered autopilot\n";
+
+            // Unsubscribe again as we only want to find one system.
+            mavsdk.subscribe_on_new_system(nullptr);
+            prom.set_value(system);
+        }
+    });
+
+    // We usually receive heartbeats at 1Hz, therefore we should find a
+    // system after around 3 seconds max, surely.
+    if (fut.wait_for(std::chrono::seconds(3)) == std::future_status::timeout) {
+        std::cerr << "No autopilot found, exiting.\n";
         return 1;
     }
 
-    System& system = mavsdk.system();
+    // Get discovered system now.
+    auto system = fut.get();
 
-    auto action = std::make_shared<Action>(system);
+    // Instantiate plugins.
+    auto action = Action{system};
 
-    std::cout << "Setting actuator..." << std::endl;
-    const Action::Result set_actuator_result = action->set_actuator(index, value);
+    std::cout << "Setting actuator...\n";
+    const Action::Result set_actuator_result = action.set_actuator(index, value);
 
     if (set_actuator_result != Action::Result::Success) {
-        std::cout << ERROR_CONSOLE_TEXT << "Setting actuator failed:" << set_actuator_result
-                  << NORMAL_CONSOLE_TEXT << std::endl;
+        std::cerr << "Setting actuator failed:" << set_actuator_result << '\n';
         return 1;
     }
 
