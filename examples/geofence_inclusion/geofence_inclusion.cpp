d5b268562bf5f424982f0dd7e191c070512671bd
Julian Oes <julian@oes.ch>
2021-07-29 13:37:40 +0200

    examples: pass through geofence_inclusion example

--- examples/geofence_inclusion/geofence_inclusion.cpp
+++ examples/geofence_inclusion/geofence_inclusion.cpp
@@ -1,18 +1,11 @@
-/**
- * @file geofence_inclusion.cpp
- *
- * @brief Demonstrates how to Add & Upload geofence using the Dronecode SDK.
- * The example is summarised below:
- * 1. Adds points to geofence.
- * 2. Uploads the geofence mission.
- *
- * @author Jonathan Zaturensky <jonathan@airmap.com>,
- * @date 2019-05-09
- */
+//
+// Demonstrates how to Add & Upload geofence using MAVSDK.
+// The example is summarised below:
+// 1. Adds points to geofence.
+// 2. Uploads the geofence mission.
+//
 
 #include <mavsdk/mavsdk.h>
-#include <mavsdk/plugins/action/action.h>
-#include <mavsdk/plugins/mission/mission.h>
 #include <mavsdk/plugins/telemetry/telemetry.h>
 #include <mavsdk/plugins/geofence/geofence.h>
 
@@ -22,88 +15,90 @@
 #include <iostream>
 #include <thread>
 
-#define ERROR_CONSOLE_TEXT "\033[31m" // Turn text on console red
-#define TELEMETRY_CONSOLE_TEXT "\033[34m" // Turn text on console blue
-#define NORMAL_CONSOLE_TEXT "\033[0m" // Restore normal console colour
-
 using namespace mavsdk;
 using std::chrono::seconds;
-using std::chrono::milliseconds;
 using std::this_thread::sleep_for;
 
 static Geofence::Point add_point(double latitude_deg, double longitude_deg);
 
-void usage(std::string bin_name)
+void usage(const std::string& bin_name)
 {
-    std::cout << NORMAL_CONSOLE_TEXT << "Usage : " << bin_name << " <connection_url>" << std::endl
-              << "Connection URL format should be :" << std::endl
-              << " For TCP : tcp://[server_host][:server_port]" << std::endl
-              << " For UDP : udp://[bind_host][:bind_port]" << std::endl
-              << " For Serial : serial:///path/to/serial/dev[:baudrate]" << std::endl
-              << "For example, to connect to the simulator use URL: udp://:14540" << std::endl;
+    std::cerr << "Usage : " << bin_name << " <connection_url>\n"
+              << "Connection URL format should be :\n"
+              << " For TCP : tcp://[server_host][:server_port]\n"
+              << " For UDP : udp://[bind_host][:bind_port]\n"
+              << " For Serial : serial:///path/to/serial/dev[:baudrate]\n"
+              << "For example, to connect to the simulator use URL: udp://:14540\n";
+}
+
+std::shared_ptr<System> get_system(Mavsdk& mavsdk)
+{
+    std::cout << "Waiting to discover system...\n";
+    auto prom = std::promise<std::shared_ptr<System>>{};
+    auto fut = prom.get_future();
+
+    // We wait for new systems to be discovered, once we find one that has an
+    // autopilot, we decide to use it.
+    mavsdk.subscribe_on_new_system([&mavsdk, &prom]() {
+        auto system = mavsdk.systems().back();
+
+        if (system->has_autopilot()) {
+            std::cout << "Discovered autopilot\n";
+
+            // Unsubscribe again as we only want to find one system.
+            mavsdk.subscribe_on_new_system(nullptr);
+            prom.set_value(system);
+        }
+    });
+
+    // We usually receive heartbeats at 1Hz, therefore we should find a
+    // system after around 3 seconds max, surely.
+    if (fut.wait_for(seconds(3)) == std::future_status::timeout) {
+        std::cerr << "No autopilot found.\n";
+        return {};
+    }
+
+    // Get discovered system now.
+    return fut.get();
 }
 
 int main(int argc, char** argv)
 {
-    Mavsdk mavsdk;
-
-    {
-        auto prom = std::promise<void>{};
-        auto future_result = prom.get_future();
-
-        std::cout << "Waiting to discover system..." << std::endl;
-        mavsdk.subscribe_on_new_system([&mavsdk, &prom]() {
-            const auto system = mavsdk.systems().at(0);
-
-            if (system->is_connected()) {
-                std::cout << "Discovered system" << std::endl;
-                prom.set_value();
-            } else {
-                std::cout << "System timed out" << std::endl;
-                std::cout << "Exiting." << std::endl;
-                exit(0);
-            }
-        });
-
-        std::string connection_url;
-        ConnectionResult connection_result;
-
-        if (argc == 2) {
-            connection_url = argv[1];
-            connection_result = mavsdk.add_any_connection(connection_url);
-        } else {
-            usage(argv[0]);
-            return 1;
-        }
-
-        if (connection_result != ConnectionResult::Success) {
-            std::cout << ERROR_CONSOLE_TEXT << "Connection failed: " << connection_result
-                      << NORMAL_CONSOLE_TEXT << std::endl;
-            return 1;
-        }
-
-        future_result.get();
+    if (argc != 2) {
+        usage(argv[0]);
+        return 1;
     }
 
-    auto system = mavsdk.systems().at(0);
-    auto action = Action{system};
-    auto mission = Mission{system};
+    Mavsdk mavsdk;
+    ConnectionResult connection_result = mavsdk.add_any_connection(argv[1]);
+
+    if (connection_result != ConnectionResult::Success) {
+        std::cerr << "Connection failed: " << connection_result << '\n';
+        return 1;
+    }
+
+    auto system = get_system(mavsdk);
+    if (!system) {
+        return 1;
+    }
+
+    // Instantiate plugins.
     auto telemetry = Telemetry{system};
     auto geofence = Geofence{system};
 
     while (!telemetry.health_all_ok()) {
-        std::cout << "Waiting for system to be ready" << std::endl;
+        std::cout << "Waiting for system to be ready\n";
         sleep_for(seconds(1));
     }
 
-    std::cout << "System ready" << std::endl;
-    std::cout << "Creating and uploading geofence" << std::endl;
+    std::cout << "System ready\n";
+    std::cout << "Creating and uploading geofence\n";
 
     std::vector<Geofence::Point> points;
-    points.push_back(add_point(47.39929240, 8.54296524));
-    points.push_back(add_point(47.39696482, 8.54161340));
-    points.push_back(add_point(47.39626761, 8.54527193));
-    points.push_back(add_point(47.39980072, 8.54736050));
+    points.emplace_back(add_point(47.39929240, 8.54296524));
+    points.emplace_back(add_point(47.39696482, 8.54161340));
+    points.emplace_back(add_point(47.39626761, 8.54527193));
+    points.emplace_back(add_point(47.39980072, 8.54736050));
 
     std::vector<Geofence::Polygon> polygons;
     Geofence::Polygon new_polygon{};
@@ -113,15 +108,15 @@ int main(int argc, char** argv)
     polygons.push_back(new_polygon);
 
     {
-        std::cout << "Uploading geofence..." << std::endl;
+        std::cout << "Uploading geofence...\n";
 
         const Geofence::Result result = geofence.upload_geofence(polygons);
 
         if (result != Geofence::Result::Success) {
-            std::cout << "Geofence upload failed (" << result << "), exiting." << std::endl;
+            std::cerr << "Geofence upload failed: " << result << ", exiting.\n";
             return 1;
         }
-        std::cout << "Geofence uploaded." << std::endl;
+        std::cout << "Geofence uploaded.\n";
     }
 
     return 0;
