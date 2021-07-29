4ba7d0abe8bd0c58a131f392b4a7b759ec676dcf
Julian Oes <julian@oes.ch>
2021-07-29 13:40:07 +0200

    examples: pass through takeoff_and_land example

--- examples/takeoff_and_land/takeoff_and_land.cpp
+++ examples/takeoff_and_land/takeoff_and_land.cpp
@@ -1,7 +1,6 @@
 //
-// Simple example to demonstrate how to use the MAVSDK.
+// Simple example to demonstrate how takeoff and land using MAVSDK.
 //
-// Author: Julian Oes <julian@oes.ch>
 
 #include <chrono>
 #include <cstdint>
@@ -17,49 +16,29 @@ using namespace mavsdk;
 using std::chrono::seconds;
 using std::this_thread::sleep_for;
 
-#define ERROR_CONSOLE_TEXT "\033[31m" // Turn text on console red
-#define TELEMETRY_CONSOLE_TEXT "\033[34m" // Turn text on console blue
-#define NORMAL_CONSOLE_TEXT "\033[0m" // Restore normal console colour
-
-void usage(std::string bin_name)
+void usage(const std::string& bin_name)
 {
-    std::cout << NORMAL_CONSOLE_TEXT << "Usage : " << bin_name << " <connection_url>" << std::endl
-              << "Connection URL format should be :" << std::endl
-              << " For TCP : tcp://[server_host][:server_port]" << std::endl
-              << " For UDP : udp://[bind_host][:bind_port]" << std::endl
-              << " For Serial : serial:///path/to/serial/dev[:baudrate]" << std::endl
-              << "For example, to connect to the simulator use URL: udp://:14540" << std::endl;
+    std::cerr << "Usage : " << bin_name << " <connection_url>\n"
+              << "Connection URL format should be :\n"
+              << " For TCP : tcp://[server_host][:server_port]\n"
+              << " For UDP : udp://[bind_host][:bind_port]\n"
+              << " For Serial : serial:///path/to/serial/dev[:baudrate]\n"
+              << "For example, to connect to the simulator use URL: udp://:14540\n";
 }
 
-int main(int argc, char** argv)
+std::shared_ptr<System> get_system(Mavsdk& mavsdk)
 {
-    Mavsdk mavsdk;
-    std::string connection_url;
-    ConnectionResult connection_result;
-
-    if (argc == 2) {
-        connection_url = argv[1];
-        connection_result = mavsdk.add_any_connection(connection_url);
-    } else {
-        usage(argv[0]);
-        return 1;
-    }
-
-    if (connection_result != ConnectionResult::Success) {
-        std::cout << ERROR_CONSOLE_TEXT << "Connection failed: " << connection_result
-                  << NORMAL_CONSOLE_TEXT << std::endl;
-        return 1;
-    }
-
-    std::cout << "Waiting to discover system..." << std::endl;
+    std::cout << "Waiting to discover system...\n";
     auto prom = std::promise<std::shared_ptr<System>>{};
     auto fut = prom.get_future();
 
+    // We wait for new systems to be discovered, once we find one that has an
+    // autopilot, we decide to use it.
     mavsdk.subscribe_on_new_system([&mavsdk, &prom]() {
         auto system = mavsdk.systems().back();
 
         if (system->has_autopilot()) {
-            std::cout << "Discovered autopilot" << std::endl;
+            std::cout << "Discovered autopilot\n";
 
             // Unsubscribe again as we only want to find one system.
             mavsdk.subscribe_on_new_system(nullptr);
@@ -68,80 +47,95 @@ int main(int argc, char** argv)
     });
 
     // We usually receive heartbeats at 1Hz, therefore we should find a
-    // system after around 3 seconds.
+    // system after around 3 seconds max, surely.
     if (fut.wait_for(seconds(3)) == std::future_status::timeout) {
-        std::cout << ERROR_CONSOLE_TEXT << "No autopilot found, exiting." << NORMAL_CONSOLE_TEXT
-                  << std::endl;
+        std::cerr << "No autopilot found.\n";
+        return {};
+    }
+
+    // Get discovered system now.
+    return fut.get();
+}
+
+int main(int argc, char** argv)
+{
+    if (argc != 2) {
+        usage(argv[0]);
         return 1;
     }
 
-    auto system = fut.get();
+    Mavsdk mavsdk;
+    ConnectionResult connection_result = mavsdk.add_any_connection(argv[1]);
 
+    if (connection_result != ConnectionResult::Success) {
+        std::cerr << "Connection failed: " << connection_result << '\n';
+        return 1;
+    }
+
+    auto system = get_system(mavsdk);
+    if (!system) {
+        return 1;
+    }
+
+    // Instantiate pluginis.
     auto telemetry = Telemetry{system};
     auto action = Action{system};
 
     // We want to listen to the altitude of the drone at 1 Hz.
-    const Telemetry::Result set_rate_result = telemetry.set_rate_position(1.0);
+    const auto set_rate_result = telemetry.set_rate_position(1.0);
     if (set_rate_result != Telemetry::Result::Success) {
-        std::cout << ERROR_CONSOLE_TEXT << "Setting rate failed:" << set_rate_result
-                  << NORMAL_CONSOLE_TEXT << std::endl;
+        std::cerr << "Setting rate failed: " << set_rate_result << '\n';
         return 1;
     }
 
     // Set up callback to monitor altitude while the vehicle is in flight
     telemetry.subscribe_position([](Telemetry::Position position) {
-        std::cout << TELEMETRY_CONSOLE_TEXT // set to blue
-                  << "Altitude: " << position.relative_altitude_m << " m"
-                  << NORMAL_CONSOLE_TEXT // set to default color again
-                  << std::endl;
+        std::cout << "Altitude: " << position.relative_altitude_m << " m\n";
     });
 
-    // Check if vehicle is ready to arm
+    // Check until vehicle is ready to arm
     while (telemetry.health_all_ok() != true) {
-        std::cout << "Vehicle is getting ready to arm" << std::endl;
+        std::cout << "Vehicle is getting ready to arm\n";
         sleep_for(seconds(1));
     }
 
     // Arm vehicle
-    std::cout << "Arming..." << std::endl;
+    std::cout << "Arming...\n";
     const Action::Result arm_result = action.arm();
 
     if (arm_result != Action::Result::Success) {
-        std::cout << ERROR_CONSOLE_TEXT << "Arming failed:" << arm_result << NORMAL_CONSOLE_TEXT
-                  << std::endl;
+        std::cerr << "Arming failed: " << arm_result << '\n';
         return 1;
     }
 
     // Take off
-    std::cout << "Taking off..." << std::endl;
+    std::cout << "Taking off...\n";
     const Action::Result takeoff_result = action.takeoff();
     if (takeoff_result != Action::Result::Success) {
-        std::cout << ERROR_CONSOLE_TEXT << "Takeoff failed:" << takeoff_result
-                  << NORMAL_CONSOLE_TEXT << std::endl;
+        std::cerr << "Takeoff failed: " << takeoff_result << '\n';
         return 1;
     }
 
     // Let it hover for a bit before landing again.
     sleep_for(seconds(10));
 
-    std::cout << "Landing..." << std::endl;
+    std::cout << "Landing...\n";
     const Action::Result land_result = action.land();
     if (land_result != Action::Result::Success) {
-        std::cout << ERROR_CONSOLE_TEXT << "Land failed:" << land_result << NORMAL_CONSOLE_TEXT
-                  << std::endl;
+        std::cerr << "Land failed: " << land_result << '\n';
         return 1;
     }
 
     // Check if vehicle is still in air
     while (telemetry.in_air()) {
-        std::cout << "Vehicle is landing..." << std::endl;
+        std::cout << "Vehicle is landing...\n";
         sleep_for(seconds(1));
     }
-    std::cout << "Landed!" << std::endl;
+    std::cout << "Landed!\n";
 
     // We are relying on auto-disarming but let's keep watching the telemetry for a bit longer.
     sleep_for(seconds(3));
-    std::cout << "Finished..." << std::endl;
+    std::cout << "Finished...\n";
 
     return 0;
 }
