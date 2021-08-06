a0631166b36dc250944dbfc8e37e3db93f55da36
Julian Oes <julian@oes.ch>
2021-08-06 11:21:13 +0200

    core: debugging around lazy plugin

--- src/core/lazy_plugin.h
+++ src/core/lazy_plugin.h
@@ -1,8 +1,10 @@
 #pragma once
 
 #include <memory>
+#include <typeinfo>
 
 #include <mavsdk.h>
+#include "log.h"
 
 namespace mavsdk {
 namespace mavsdk_server {
@@ -13,11 +15,21 @@ public:
 
     Plugin* maybe_plugin()
     {
+        bool created = false;
         if (_action == nullptr) {
             if (_mavsdk.systems().size() == 0) {
+                LogWarn() << "No system yet (" << typeid(Plugin).name() << ")";
                 return nullptr;
             }
+            LogDebug() << "Create plugin (" << typeid(Plugin).name() << ")";
             _action = std::make_unique<Plugin>(_mavsdk.systems()[0]);
+            LogDebug() << "Created plugin (" << typeid(Plugin).name() << ")";
+            created = true;
+        }
+        if (created) {
+            LogDebug() << "Use created plugin (" << typeid(Plugin).name() << ")";
+        } else {
+            LogDebug() << "Use existing plugin (" << typeid(Plugin).name() << ")";
         }
         return _action.get();
     }
