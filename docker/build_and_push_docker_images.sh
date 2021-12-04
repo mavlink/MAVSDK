f64ee76cd6e02b7211c4e1977403937442b38106
Julian Oes <julian@oes.ch>
2021-12-04 14:33:29 +0100

    Remove Ubuntu 18.04 from CI and docker images
    This also removes testing for PX4 1.8 and 1.9 which honestly by now is
    pretty old anyway.

--- docker/build_and_push_docker_images.sh
+++ docker/build_and_push_docker_images.sh
@@ -2,20 +2,12 @@
 
 set -e
 
-docker build -f Dockerfile-Fedora-32 -t mavsdk/mavsdk-fedora-32 .
 docker build -f Dockerfile-Fedora-33 -t mavsdk/mavsdk-fedora-33 .
-docker build -f Dockerfile-Ubuntu-18.04 -t mavsdk/mavsdk-ubuntu-18.04 .
 docker build -f Dockerfile-Ubuntu-20.04 -t mavsdk/mavsdk-ubuntu-20.04 .
-docker build -f Dockerfile-Ubuntu-18.04-PX4-SITL-v1.9 -t mavsdk/mavsdk-ubuntu-18.04-px4-sitl-v1.9 .
-docker build -f Dockerfile-Ubuntu-18.04-PX4-SITL-v1.10 -t mavsdk/mavsdk-ubuntu-18.04-px4-sitl-v1.10 .
 docker build -f Dockerfile-Ubuntu-20.04-PX4-SITL-v1.11 -t mavsdk/mavsdk-ubuntu-20.04-px4-sitl-v1.11 .
 docker build -f Dockerfile-Ubuntu-20.04-PX4-SITL-v1.12 -t mavsdk/mavsdk-ubuntu-20.04-px4-sitl-v1.12 .
 
-docker push mavsdk/mavsdk-fedora-32:latest
 docker push mavsdk/mavsdk-fedora-33:latest
-docker push mavsdk/mavsdk-ubuntu-18.04:latest
 docker push mavsdk/mavsdk-ubuntu-20.04:latest
-docker push mavsdk/mavsdk-ubuntu-18.04-px4-sitl-v1.9:latest
-docker push mavsdk/mavsdk-ubuntu-18.04-px4-sitl-v1.10:latest
 docker push mavsdk/mavsdk-ubuntu-20.04-px4-sitl-v1.11:latest
 docker push mavsdk/mavsdk-ubuntu-20.04-px4-sitl-v1.12:latest
