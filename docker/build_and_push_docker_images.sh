27eed115703a0273cd8ea21c91689c90e21f83aa
Julian Oes <julian@oes.ch>
2021-12-04 15:18:00 +0100

    Remove Fedora 33 from CI, add 34 and 35

--- docker/build_and_push_docker_images.sh
+++ docker/build_and_push_docker_images.sh
@@ -2,12 +2,14 @@
 
 set -e
 
-docker build -f Dockerfile-Fedora-33 -t mavsdk/mavsdk-fedora-33 .
+docker build -f Dockerfile-Fedora-34 -t mavsdk/mavsdk-fedora-34 .
+docker build -f Dockerfile-Fedora-35 -t mavsdk/mavsdk-fedora-35 .
 docker build -f Dockerfile-Ubuntu-20.04 -t mavsdk/mavsdk-ubuntu-20.04 .
 docker build -f Dockerfile-Ubuntu-20.04-PX4-SITL-v1.11 -t mavsdk/mavsdk-ubuntu-20.04-px4-sitl-v1.11 .
 docker build -f Dockerfile-Ubuntu-20.04-PX4-SITL-v1.12 -t mavsdk/mavsdk-ubuntu-20.04-px4-sitl-v1.12 .
 
-docker push mavsdk/mavsdk-fedora-33:latest
+docker push mavsdk/mavsdk-fedora-34:latest
+docker push mavsdk/mavsdk-fedora-35:latest
 docker push mavsdk/mavsdk-ubuntu-20.04:latest
 docker push mavsdk/mavsdk-ubuntu-20.04-px4-sitl-v1.11:latest
 docker push mavsdk/mavsdk-ubuntu-20.04-px4-sitl-v1.12:latest
