d6858256d92a962327c79511536ac3ac8f5c2cc1
Julian Oes <julian@oes.ch>
2022-01-05 08:40:25 +0100

    action: attempt to fix Windows build

--- src/mavsdk/plugins/action/action_impl.cpp
+++ src/mavsdk/plugins/action/action_impl.cpp
@@ -393,7 +393,7 @@ void ActionImpl::goto_location_async(
 
             command.command = MAV_CMD_DO_REPOSITION;
             command.target_component_id = _parent->get_autopilot_id();
-            command.params.maybe_param4 = {to_rad_from_deg(yaw_deg)};
+            command.params.maybe_param4 = static_cast<float>(to_rad_from_deg(yaw_deg));
             command.params.x = int32_t(std::round(latitude_deg * 1e7));
             command.params.y = int32_t(std::round(longitude_deg * 1e7));
             command.params.maybe_z = altitude_amsl_m;
