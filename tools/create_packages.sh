3c3fba9d8932eb09586b030b19176dd336fa483c
Julian Oes <julian@oes.ch>
2022-01-20 08:41:02 +0100

    tools: fix package generation
    This should:
    - reset the cache for arm builds
    - use the correct name for library packages
    - fix the arm package architecture

--- tools/create_packages.sh
+++ tools/create_packages.sh
@@ -5,10 +5,10 @@ set -e
 echo "Packaging"
 echo "---------"
 
-if [ "$#" -ne 3 ]; then
+if [ "$#" -ne 4 ]; then
   echo "Error: missing path to the 'install' directory (i.e. what was specified as -DCMAKE_INSTALL_PREFIX)" >&2
   echo "       or output path (where the package will be generated)!" >&2
-  echo "Usage: $0 <path/to/install> <output/path> <architecture>" >&2
+  echo "Usage: $0 <path/to/install> <output/path> <architecture> <name>" >&2
   exit 1
 fi
 
@@ -16,6 +16,14 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 install_dir=$1
 output_dir=$2
 arch="$3"
+name="$4"
+
+# The package architecture needs to be armhf, at least for RPi.
+if [ "$arch" = "armv6" ] || [ "$arch" = "armv7" ]; then
+    package_arch="armhf"
+else
+    package_arch=$arch
+fi
 
 working_dir=$(mktemp -d)
 mkdir -p ${working_dir}/install
@@ -36,14 +44,15 @@ echo "/sbin/ldconfig" >> ${working_dir}/run_ldconfig
 
 common_args="--chdir ${working_dir}/install \
              --input-type dir  \
-             --name mavsdk \
+             --name ${name} \
              --version ${version} \
              --maintainer julian@oes.ch \
              --url https://mavsdk.mavlink.io \
              --license BSD-3-Clause \
              --after-install ${working_dir}/run_ldconfig \
              --after-remove ${working_dir}/run_ldconfig \
-             --force"
+             --force \
+             -a ${package_arch}"
 
 if cat /etc/os-release | grep 'Ubuntu'
 then
@@ -51,30 +60,29 @@ then
     fpm ${common_args} \
         --output-type deb \
         --deb-no-default-config-files \
-        -a ${arch} \
         ${library_files}
 
     dist_version=$(cat /etc/os-release | grep VERSION_ID | sed 's/[^0-9.]*//g')
 
-    for file in *_${arch}.deb
+    for file in *_${package_arch}.deb
     do
-        mv -v "${file}" "${output_dir}/${file%_${arch}.deb}_ubuntu${dist_version}_${arch}.deb"
+        mv -v "${file}" "${output_dir}/${file%_${package_arch}.deb}_ubuntu${dist_version}_${arch}.deb"
     done
 
 elif cat /etc/os-release | grep 'Debian'
 then
     echo "Building Debian DEB package"
+
     fpm ${common_args} \
         --output-type deb \
         --deb-no-default-config-files \
-        -a ${arch} \
         ${library_files}
 
     dist_version=$(cat /etc/os-release | grep VERSION_ID | sed 's/[^0-9.]*//g')
 
-    for file in *_${arch}.deb
+    for file in *_${package_arch}.deb
     do
-        mv -v "${file}" "${output_dir}/${file%_${arch}.deb}_debian${dist_version}_${arch}.deb"
+        mv -v "${file}" "${output_dir}/${file%_${package_arch}.deb}_debian${dist_version}_${arch}.deb"
     done
 
 elif cat /etc/os-release | grep 'Fedora'
@@ -83,13 +91,12 @@ then
     fpm ${common_args} \
         --output-type rpm \
         --rpm-rpmbuild-define "_build_id_links none" \
-        -a ${arch} \
         ${library_files}
 
     dist_version=$(cat /etc/os-release | grep VERSION_ID | sed 's/[^0-9]*//g')
 
-    for file in *.${arch}.rpm
+    for file in *.${package_arch}.rpm
     do
-        mv -v "${file}" "${output_dir}/${file%.${arch}.rpm}.fc${dist_version}-${arch}.rpm"
+        mv -v "${file}" "${output_dir}/${file%.${package_arch}.rpm}.fc${dist_version}-${arch}.rpm"
     done
 fi
