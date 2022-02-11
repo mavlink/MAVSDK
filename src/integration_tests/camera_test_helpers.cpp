f05201bc435c03baa4008d48dc45ab17e7ff3696
Julian Oes <julian@oes.ch>
2022-02-11 11:44:51 +0100

    integration_tests: camera helper cleanup
    I was able to simplify this a bit.

--- src/integration_tests/camera_test_helpers.cpp
+++ src/integration_tests/camera_test_helpers.cpp
@@ -5,53 +5,6 @@
 
 using namespace mavsdk;
 
-Camera::Mode get_mode(std::shared_ptr<Camera> camera)
-{
-    auto prom = std::make_shared<std::promise<Camera::Mode>>();
-    auto ret = prom->get_future();
-
-    camera->subscribe_mode([prom, camera](Camera::Mode mode) {
-        prom->set_value(mode);
-        camera->subscribe_mode(nullptr);
-    });
-
-    auto status = ret.wait_for(std::chrono::seconds(7));
-
-    EXPECT_EQ(status, std::future_status::ready);
-
-    if (status == std::future_status::ready) {
-        Camera::Mode mode = ret.get();
-        EXPECT_NE(mode, Camera::Mode::Unknown);
-        return mode;
-    } else {
-        return Camera::Mode::Unknown;
-    }
-}
-
-void set_mode_async(std::shared_ptr<Camera> camera, Camera::Mode mode)
-{
-    //// FIXME: this should not be required.
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-
-    auto prom = std::make_shared<std::promise<void>>();
-    auto ret = prom->get_future();
-    camera->set_mode_async(mode, [prom](Camera::Result result) {
-        EXPECT_EQ(result, Camera::Result::Success);
-        prom->set_value();
-    });
-
-    auto status = ret.wait_for(std::chrono::seconds(10));
-
-    EXPECT_EQ(status, std::future_status::ready);
-
-    if (status == std::future_status::ready) {
-        ret.get();
-    }
-
-    // FIXME: this should not be required.
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-}
-
 Camera::Result set_setting(
     std::shared_ptr<Camera> camera, const std::string& setting_id, const std::string& option_id)
 {
