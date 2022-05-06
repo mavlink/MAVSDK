17cf9b4deb50edcb1f2c2642cd6f8a7777c73d7e
Julian Oes <julian@oes.ch>
2022-05-06 18:25:25 +1200

    core: add some missing comments

--- src/mavsdk/core/include/mavsdk/server_component.h
+++ src/mavsdk/core/include/mavsdk/server_component.h
@@ -9,9 +9,21 @@ class MavsdkImpl;
 class ServerComponentImpl;
 class ServerPluginImplBase;
 
+/**
+ * @brief This class represents a component, used to initialize a server plugin.
+ */
 class ServerComponent {
 public:
+    /**
+     * @private Constructor, used internally
+     *
+     * This constructor is not (and should not be) directly called by application code.
+     */
     ServerComponent(MavsdkImpl& mavsdk_impl, uint8_t component_id);
+
+    /**
+     * @brief Destructor.
+     */
     ~ServerComponent() = default;
 
 private:
