b06ddf26d722b4f7cf2d2ea267485b956f07f5d0
Julian Oes <julian@oes.ch>
2022-05-09 10:51:40 +1200

    examples: fix camera server
    We can now take advantage of the server components.

--- examples/camera_server/camera_server.cpp
+++ examples/camera_server/camera_server.cpp
@@ -22,98 +22,76 @@ int main(int argc, char** argv)
     mavsdk.set_configuration(configuration);
 
     // 14030 is the default camera port for PX4 SITL
+    // FIXME: the camera should probably initiate the connection and not just listen
     auto result = mavsdk.add_any_connection("udp://:14030");
-    if (result == mavsdk::ConnectionResult::Success) {
-        std::cout << "Created camera server connection" << std::endl;
+    if (result != mavsdk::ConnectionResult::Success) {
+        std::cerr << "Could not establish connection: " << result << std::endl;
+        return 1;
+    }
+    std::cout << "Created camera server connection" << std::endl;
+
+    auto camera_server = mavsdk::CameraServer{
+        mavsdk.server_component_by_type(mavsdk::Mavsdk::ServerComponentType::Camera)};
+
+    // First add all subscriptions. This defines the camera capabilities.
+
+    camera_server.subscribe_take_photo([&camera_server](int32_t index) {
+        camera_server.set_in_progress(true);
+
+        std::cout << "taking a picture (" << +index << ")..." << std::endl;
+
+        // TODO : actually capture image here
+        // simulating with delay
+        sleep_for(milliseconds(500));
+
+        // TODO: populate with telemetry data
+        auto position = CameraServer::Position{};
+        auto attitude = CameraServer::Quaternion{};
+
+        auto timestamp =
+            duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+        auto success = true;
+
+        camera_server.set_in_progress(false);
+
+        camera_server.respond_take_photo(
+            mavsdk::CameraServer::TakePhotoFeedback::Ok,
+            mavsdk::CameraServer::CaptureInfo{
+                .position = position,
+                .attitude_quaternion = attitude,
+                .time_utc_us = static_cast<uint64_t>(timestamp),
+                .is_success = success,
+                .index = index,
+                .file_url = {},
+            });
+    });
+
+    // Then set the initial state of everything.
+
+    // TODO: this state is not guaranteed, e.g. a new system appears
+    // while a capture is in progress
+    camera_server.set_in_progress(false);
+
+    // Finally call set_information() to "activate" the camera plugin.
+
+    auto ret = camera_server.set_information({
+        .vendor_name = "MAVSDK",
+        .model_name = "Example Camera Server",
+        .firmware_version = "1.0.0",
+        .focal_length_mm = 3.0,
+        .horizontal_sensor_size_mm = 3.68,
+        .vertical_sensor_size_mm = 2.76,
+        .horizontal_resolution_px = 3280,
+        .vertical_resolution_px = 2464,
+    });
+
+    if (ret != CameraServer::Result::Success) {
+        std::cerr << "Failed to set camera info, exiting" << std::endl;
+        return 2;
     }
 
-    // map of system id to camera server instance
-    std::map<uint8_t, std::shared_ptr<mavsdk::CameraServer>> all_camera_servers{};
-
-    auto attach_camera_server = [&]() {
-        for (auto&& system : mavsdk.systems()) {
-            // if we have already created a camera server for this system, skip it
-            if (all_camera_servers.find(system->get_system_id()) != all_camera_servers.end()) {
-                continue;
-            }
-
-            // Create server plugin
-            auto camera_server = std::make_shared<mavsdk::CameraServer>(system);
-
-            all_camera_servers.insert({system->get_system_id(), camera_server});
-
-            // First add all subscriptions. This defines the camera capabilities.
-
-            camera_server->subscribe_take_photo(
-                [&all_camera_servers](CameraServer::Result result, int32_t index) {
-                    for (auto&& [key, value] : all_camera_servers) {
-                        value->set_in_progress(true);
-                    }
-
-                    std::cout << "taking a picture (" << +index << ")..." << std::endl;
-
-                    // TODO : actually capture image here
-                    // simulating with delay
-                    sleep_for(milliseconds(500));
-
-                    // TODO: populate with telemetry data
-                    auto position = CameraServer::Position{};
-                    auto attitude = CameraServer::Quaternion{};
-
-                    auto timestamp =
-                        duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
-                    auto success = true;
-
-                    for (auto&& [key, value] : all_camera_servers) {
-                        value->set_in_progress(false);
-                    }
-
-                    for (auto&& [key, value] : all_camera_servers) {
-                        value->respond_take_photo({
-                            .position = position,
-                            .attitude_quaternion = attitude,
-                            .time_utc_us = static_cast<uint64_t>(timestamp),
-                            .is_success = success,
-                            .index = index,
-                            .file_url = {},
-                        });
-                    }
-                });
-
-            // Then set the initial state of everything.
-
-            // TODO: this state is not guaranteed, e.g. a new system appears
-            // while a capture is in progress
-            camera_server->set_in_progress(false);
-
-            // Finally call set_information() to "activate" the camera plugin.
-
-            auto ret = camera_server->set_information({
-                .vendor_name = "MAVSDK",
-                .model_name = "Example Camera Server",
-                .firmware_version = "1.0.0",
-                .focal_length_mm = 3.0,
-                .horizontal_sensor_size_mm = 3.68,
-                .vertical_sensor_size_mm = 2.76,
-                .horizontal_resolution_px = 3280,
-                .vertical_resolution_px = 2464,
-            });
-
-            if (ret != CameraServer::Result::Success) {
-                std::cerr << "Failed to set camera info, exiting" << std::endl;
-                exit(1);
-            }
-
-            std::cout << "Connected to " << (system->is_standalone() ? "GCS" : "autopilot")
-                      << " system ID " << +system->get_system_id() << std::endl;
-        }
-    };
-
-    mavsdk.subscribe_on_new_system(attach_camera_server);
-    attach_camera_server();
-
     while (true) {
-        // TODO: better way to wait forever?
+        // Wait forever?
         sleep_for(seconds(1));
     }
 
