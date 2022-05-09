302e9abb537dd4e5a3b7f300eedf6635bca2c704
Julian Oes <julian@oes.ch>
2022-05-09 12:25:18 +1200

    core: don't warn on first discovery
    There is no point in having a warning when first changing from the
    initial MAV_TYPE_GENERIC, hence we ignore that.

--- src/mavsdk/core/system_impl.cpp
+++ src/mavsdk/core/system_impl.cpp
@@ -220,7 +220,8 @@ void SystemImpl::process_heartbeat(const mavlink_message_t& message)
         LogErr() << "type received in HEARTBEAT was not recognized";
     } else {
         const auto new_vehicle_type = static_cast<MAV_TYPE>(heartbeat.type);
-        if (heartbeat.autopilot != MAV_AUTOPILOT_INVALID && _vehicle_type != new_vehicle_type) {
+        if (heartbeat.autopilot != MAV_AUTOPILOT_INVALID && _vehicle_type != new_vehicle_type &&
+            _vehicle_type != MAV_TYPE_GENERIC) {
             LogWarn() << "Vehicle type changed (new type: " << static_cast<unsigned>(heartbeat.type)
                       << ", old type: " << static_cast<unsigned>(_vehicle_type) << ")";
             _vehicle_type = new_vehicle_type;
