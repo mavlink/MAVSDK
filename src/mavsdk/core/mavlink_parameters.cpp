1666224b307c688fa8dd0c01a1f22e490b0beae3
consti10 <geierconstantinabc@gmail.com>
2022-06-16 15:45:48 +0200

    document

--- src/mavsdk/core/mavlink_parameters.cpp
+++ src/mavsdk/core/mavlink_parameters.cpp
@@ -994,9 +994,12 @@ void MAVLinkParameters::process_param_value(const mavlink_message_t& message)
         std::lock_guard<std::mutex> lock(_all_params_mutex);
         _all_params[param_id] = received_value;
 
-        // check if we are looking for param list
-		// Consti10 - now this is broken since the n of parameters is different for ext and non-ext use cases.
+        // check if we are looking for param list (get all parameters).
         if (_all_params_callback) {
+			// If we are currently waiting for all parameters, this is a hacky way to basically say
+			// "Hey, we got the last parameter, so we got all parameters, and can forward that to the user."
+			// Note that I don't think this accounts for the case where a parameter that is not the last parameter went missing.
+			// So it is better than nothing, but also not ideal. Correct me if I'm wrong.
             if (param_value.param_index + 1 == param_value.param_count) {
                 _timeout_handler.remove(_all_params_timeout_cookie);
                 _all_params_callback(_all_params);
