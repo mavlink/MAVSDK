856f108b3bd0a1fd10a92a09c23033f7d63aa57d
consti10 <geierconstantinabc@gmail.com>
2022-06-19 19:48:10 +0200

    set the flavour directly in cmake, so we don't need to manually build all the time

--- build_install.sh
+++ build_install.sh
@@ -1,5 +1,5 @@
 rm -rf build
-cmake -DCMAKE_BUILD_TYPE=Debug -DMAVLINK_DIALECT=openhd -Bbuild/default -H.
+cmake -DCMAKE_BUILD_TYPE=Debug -Bbuild/default -H.
 cmake --build build/default -j8
 
 sudo cmake --build build/default --target install
