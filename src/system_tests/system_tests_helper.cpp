63b30547c595947f69ac0e70671d44f54a162d23
Julian Oes <julian@oes.ch>
2023-04-22 09:52:28 +1200

    core: fix system tests subscription
    It turns out that the handle went out of scope and we segfaulted.
    Therefore, we need to create a shared_ptr containing it.
    
    This also required the Handle to be default constructible, with id 0
    which basically means it's a NullHandle.
    
    Signed-off-by: Julian Oes <julian@oes.ch>

--- src/system_tests/system_tests_helper.cpp
+++ src/system_tests/system_tests_helper.cpp
@@ -1,4 +1,5 @@
 #include "system_tests_helper.h"
+#include <optional>
 
 namespace mavsdk {
 
@@ -8,17 +9,15 @@ std::future<std::shared_ptr<System>> wait_for_first_system_detected(Mavsdk& mavs
 
     auto prom = std::make_shared<std::promise<std::shared_ptr<System>>>();
 
-    // We pass a copy of the shared ptr to the promise to keep it alive after
-    // this function goes out of scope.
-    const Mavsdk::NewSystemHandle handle =
-        mavsdk.subscribe_on_new_system([&mavsdk, prom, handle]() {
-            const auto system = mavsdk.systems().at(0);
-            if (system->is_connected()) {
-                mavsdk.unsubscribe_on_new_system(handle);
-                LogInfo() << "Discovered system";
-                prom->set_value(system);
-            }
-        });
+    auto handle = std::make_shared<Mavsdk::NewSystemHandle>();
+    *handle = mavsdk.subscribe_on_new_system([prom, &mavsdk, handle]() {
+        const auto system = mavsdk.systems().at(0);
+        if (system->is_connected()) {
+            mavsdk.unsubscribe_on_new_system(*handle);
+            LogInfo() << "Discovered system";
+            prom->set_value(system);
+        }
+    });
 
     return prom->get_future();
 }
