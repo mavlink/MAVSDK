f8d73fc2d3ebf4f10bc5efa339ae3f4cdf6229a2
Julian Oes <julian@oes.ch>
2023-06-17 13:29:05 +1200

    Add cpptrace
    This is to get more information about segfaults in CI.
    
    Signed-off-by: Julian Oes <julian@oes.ch>

---
+++ src/system_tests/system_tests_runner.cpp
@@ -0,0 +1,32 @@
+#if defined(ENABLE_CPPTRACE)
+#include <signal.h>
+#include <cpptrace/cpptrace.hpp>
+#include "log.h"
+#include <cstring>
+#endif
+#include <gtest/gtest.h>
+
+#if defined(ENABLE_CPPTRACE)
+void handler(int sig)
+{
+    mavsdk::LogErr() << "Got signal: " << strsignal(sig) << " (" << sig << ")";
+    cpptrace::print_trace();
+    exit(1);
+}
+#endif
+
+int main(int argc, char** argv)
+{
+#if defined(ENABLE_CPPTRACE)
+    signal(SIGSEGV, handler);
+    signal(SIGQUIT, handler);
+    signal(SIGABRT, handler);
+    signal(SIGILL, handler);
+    signal(SIGBUS, handler);
+    signal(SIGPIPE, handler);
+    signal(SIGFPE, handler);
+#endif
+
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
