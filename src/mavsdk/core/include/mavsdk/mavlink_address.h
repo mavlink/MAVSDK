df72929f9ca25847517cc8a5aebcf0467f5ecb82
Julian Oes <julian@oes.ch>
2023-10-18 10:12:09 +1300

    core: fixup docstrings
    Signed-off-by: Julian Oes <julian@oes.ch>

--- src/mavsdk/core/include/mavsdk/mavlink_address.h
+++ src/mavsdk/core/include/mavsdk/mavlink_address.h
@@ -2,7 +2,16 @@
 
 #include <cstdint>
 
+/**
+ * @brief Struct to represent a MAVLink address.
+ */
 struct MavlinkAddress {
+    /**
+     * @brief System ID, also called sysid.
+     */
     uint8_t system_id;
+    /**
+     * @brief Component ID, also called compid.
+     */
     uint8_t component_id;
 };
