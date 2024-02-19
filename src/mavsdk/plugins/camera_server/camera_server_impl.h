9ba800b2591860acbcbdb997ed3f0b46ad722958
denys <ddatsko010101@gmail.com>
2024-02-19 02:15:29 +0100

    Merged tracking_server plugin into camera_server

--- src/mavsdk/plugins/camera_server/camera_server_impl.h
+++ src/mavsdk/plugins/camera_server/camera_server_impl.h
@@ -3,6 +3,7 @@
 #include "plugins/camera_server/camera_server.h"
 #include "server_plugin_impl_base.h"
 #include "callback_list.h"
+#include <thread>
 
 namespace mavsdk {
 
@@ -13,6 +14,12 @@ public:
 
     void init() override;
     void deinit() override;
+
+    void set_tracking_point_status(CameraServer::TrackPoint tracked_point);
+
+    void set_tracking_rectangle_status(CameraServer::TrackRectangle tracked_rectangle);
+
+    void set_tracking_off_status();
 
     CameraServer::Result set_information(CameraServer::Information information);
     CameraServer::Result set_video_streaming(CameraServer::VideoStreaming video_streaming);
@@ -76,6 +83,7 @@ public:
     CameraServer::Result
     respond_reset_settings(CameraServer::CameraFeedback reset_settings_feedback);
 
+<<<<<<< HEAD
     CameraServer::ZoomInStartHandle
     subscribe_zoom_in_start(const CameraServer::ZoomInStartCallback& callback);
     void unsubscribe_zoom_in_start(CameraServer::ZoomInStartHandle handle);
@@ -93,6 +101,26 @@ public:
     subscribe_zoom_range(const CameraServer::ZoomRangeCallback& callback);
     void unsubscribe_zoom_range(CameraServer::ZoomRangeHandle handle);
     CameraServer::Result respond_zoom_range(CameraServer::CameraFeedback zoom_range_feedback);
+=======
+    CameraServer::TrackingPointCommandHandle
+    subscribe_tracking_point_command(const CameraServer::TrackingPointCommandCallback& callback);
+    void unsubscribe_tracking_point_command(CameraServer::TrackingPointCommandHandle handle);
+    CameraServer::Result
+    respond_tracking_point_command(CameraServer::CameraFeedback tracking_point_feedback);
+
+    CameraServer::TrackingRectangleCommandHandle subscribe_tracking_rectangle_command(
+        const CameraServer::TrackingRectangleCommandCallback& callback);
+    void
+    unsubscribe_tracking_rectangle_command(CameraServer::TrackingRectangleCommandHandle handle);
+    CameraServer::Result
+    respond_tracking_rectangle_command(CameraServer::CameraFeedback tracking_rectangle_feedback);
+
+    CameraServer::TrackingOffCommandHandle
+    subscribe_tracking_off_command(const CameraServer::TrackingOffCommandCallback& callback);
+    void unsubscribe_tracking_off_command(CameraServer::TrackingOffCommandHandle handle);
+    CameraServer::Result
+    respond_tracking_off_command(CameraServer::CameraFeedback tracking_off_feedback);
+>>>>>>> 50d1d132 (Merged tracking_server plugin into camera_server)
 
 private:
     enum StatusFlags {
@@ -106,11 +134,77 @@ private:
         ENABLE = 1,
     };
 
+<<<<<<< HEAD
+=======
+    enum class TrackingMode {
+        NONE = 0,
+        POINT = 1,
+        RECTANGLE = 2,
+    };
+
+    bool _is_information_set{};
+
+    // CAMERA_TRACKING_STATUS messages sending fields
+    std::mutex _tracking_status_mutex{};
+    bool _sending_tracking_status{};
+    TrackingMode _tracking_mode{};
+    CameraServer::TrackPoint _tracked_point{};
+    CameraServer::TrackRectangle _tracked_rectangle{};
+    std::thread _tracking_status_sending_thread{};
+
+    CameraServer::Information _information{};
+    bool _is_video_streaming_set{};
+    CameraServer::VideoStreaming _video_streaming{};
+
+    CameraServer::CaptureStatus _capture_status{};
+
+    // CAMERA_CAPTURE_STATUS fields
+    // TODO: how do we keep this info in sync between plugin instances?
+    bool _is_image_capture_in_progress{};
+    bool _is_image_capture_interval_set{};
+    float _image_capture_timer_interval_s{};
+    void* _image_capture_timer_cookie{};
+    int32_t _image_capture_count{};
+
+    CallbackList<int32_t> _take_photo_callbacks{};
+    CallbackList<int32_t> _start_video_callbacks{};
+    CallbackList<int32_t> _stop_video_callbacks{};
+    CallbackList<int32_t> _start_video_streaming_callbacks{};
+    CallbackList<int32_t> _stop_video_streaming_callbacks{};
+    CallbackList<CameraServer::Mode> _set_mode_callbacks{};
+    CallbackList<int32_t> _storage_information_callbacks{};
+    CallbackList<int32_t> _capture_status_callbacks{};
+    CallbackList<int32_t> _format_storage_callbacks{};
+    CallbackList<int32_t> _reset_settings_callbacks{};
+    CallbackList<CameraServer::TrackPoint> _tracking_point_callbacks{};
+    CallbackList<CameraServer::TrackRectangle> _tracking_rectangle_callbacks{};
+    CallbackList<int32_t> _tracking_off_callbacks{};
+
+    MavlinkCommandReceiver::CommandLong _last_take_photo_command;
+    MavlinkCommandReceiver::CommandLong _last_start_video_command;
+    MavlinkCommandReceiver::CommandLong _last_stop_video_command;
+    MavlinkCommandReceiver::CommandLong _last_start_video_streaming_command;
+    MavlinkCommandReceiver::CommandLong _last_stop_video_streaming_command;
+    MavlinkCommandReceiver::CommandLong _last_set_mode_command;
+    MavlinkCommandReceiver::CommandLong _last_storage_information_command;
+    MavlinkCommandReceiver::CommandLong _last_capture_status_command;
+    MavlinkCommandReceiver::CommandLong _last_format_storage_command;
+    MavlinkCommandReceiver::CommandLong _last_reset_settings_command;
+    MavlinkCommandReceiver::CommandLong _last_track_point_command;
+    MavlinkCommandReceiver::CommandLong _last_track_rectangle_command;
+    MavlinkCommandReceiver::CommandLong _last_tracking_off_command;
+
+    uint8_t _last_storage_id;
+
+>>>>>>> 50d1d132 (Merged tracking_server plugin into camera_server)
     bool parse_version_string(const std::string& version_str);
     bool parse_version_string(const std::string& version_str, uint32_t& version);
+    bool is_command_sender_ok(const MavlinkCommandReceiver::CommandLong& command);
     void start_image_capture_interval(float interval, int32_t count, int32_t index);
     void stop_image_capture_interval();
-    uint32_t camera_cap_flags_to_bitmask(const CameraServer::CameraCapFlags& flags) const;
+    void start_sending_tracking_status(uint32_t interval_us);
+    void stop_sending_tracking_status();
+    void send_tracking_status_with_interval(uint32_t interval_us);
 
     std::optional<mavlink_command_ack_t>
     process_camera_information_request(const MavlinkCommandReceiver::CommandLong& command);
@@ -150,6 +244,14 @@ private:
     process_video_stream_information_request(const MavlinkCommandReceiver::CommandLong& command);
     std::optional<mavlink_command_ack_t>
     process_video_stream_status_request(const MavlinkCommandReceiver::CommandLong& command);
+    std::optional<mavlink_command_ack_t>
+    process_track_point_command(const MavlinkCommandReceiver::CommandLong& command);
+    std::optional<mavlink_command_ack_t>
+    process_track_rectangle_command(const MavlinkCommandReceiver::CommandLong& command);
+    std::optional<mavlink_command_ack_t>
+    process_track_off_command(const MavlinkCommandReceiver::CommandLong& command);
+    std::optional<mavlink_command_ack_t>
+    process_set_message_interval(const MavlinkCommandReceiver::CommandLong& command);
 
     void send_capture_status();
 
