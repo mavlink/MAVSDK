0420e4e4fb01a14cee11a108fa147c47f9a8f7d0
Julian Oes <julian@oes.ch>
2024-03-04 16:54:59 +1300

    docker: remove unused SITL testing images
    We no longer maintain and use the integration tests and SITL tests.
    
    To do this right, we would need a lot more testing infrastructure, and
    would have to keep up to date with all PX4 and ArduPilot releases.
    
    I don't have bandwidth to do that, and I don't think it should be up to
    MAVSDK to keep a docker image to run PX4 and ArduPilot.
    
    Hence, I'm removing it all.
    
    Signed-off-by: Julian Oes <julian@oes.ch>

--- src/integration_tests/integration_test_helper.h
+++ src/integration_tests/integration_test_helper.h
@@ -2,69 +2,12 @@
 
 #include <chrono>
 #include <functional>
-#include <future>
-#include <string>
 #include <thread>
 #include <gtest/gtest.h>
 #include "log.h"
 #include "unused.h"
 
 // Autostarting SITL is not supported on Windows
-
-class SitlTest : public testing::Test {
-protected:
-    void StartSitl(const std::string& model)
-    {
-#ifndef WINDOWS
-        const int ret = system((std::string("./tools/start_sitl.sh ") + model).c_str());
-        if (ret != 0) {
-            mavsdk::LogErr() << "./tools/start_sitl.sh failed, giving up.";
-            fflush(stdout);
-            fflush(stderr);
-            abort();
-        }
-#else
-        UNUSED(model);
-        mavsdk::LogErr() << "Auto-starting SITL not supported on Windows.";
-#endif
-    }
-
-    void StopSitl()
-    {
-#ifndef WINDOWS
-        const int ret = system("./tools/stop_sitl.sh");
-        if (ret != 0) {
-            mavsdk::LogErr() << "./tools/stop_sitl.sh failed, giving up.";
-            fflush(stdout);
-            fflush(stderr);
-            abort();
-        }
-#else
-        mavsdk::LogErr() << "Auto-starting SITL not supported on Windows.";
-#endif
-    }
-
-    virtual void SetUp() override { StartSitl(determineModel()); }
-
-    virtual void TearDown() override { StopSitl(); }
-
-private:
-    static std::string determineModel()
-    {
-        // If no model name is appended to the test name, we use the default which is iris.
-        std::string model_name = "iris";
-
-        const std::string& test_name =
-            ::testing::UnitTest::GetInstance()->current_test_info()->name();
-        const size_t pos = test_name.find_first_of('_', 0);
-        if (pos != std::string::npos) {
-            model_name = test_name.substr(pos + 1, test_name.length() - pos - 1);
-        }
-
-        mavsdk::LogDebug() << "Model chosen: '" << model_name << "'";
-        return model_name;
-    }
-};
 
 template<typename Rep, typename Period>
 bool poll_condition_with_timeout(
