94a934851927db07f306439e44e39b6522aaa980
alexklimaj <alex@arkelectron.com>
2024-05-16 21:55:38 -0600

    add arm64-lts docker workflow

--- docker/build_and_push_docker_images.sh
+++ docker/build_and_push_docker_images.sh
@@ -14,8 +14,10 @@ $DOCKER_CMD build -f Dockerfile-dev -t mavsdk/mavsdk-dev .
 $DOCKER_CMD build -f Dockerfile.dockcross-linux-armv6-custom -t mavsdk/mavsdk-dockcross-linux-armv6-custom .
 $DOCKER_CMD build -f Dockerfile.dockcross-linux-armv7-custom -t mavsdk/mavsdk-dockcross-linux-armv7-custom .
 $DOCKER_CMD build -f Dockerfile.dockcross-linux-arm64-custom -t mavsdk/mavsdk-dockcross-linux-arm64-custom .
+$DOCKER_CMD build -f Dockerfile.dockcross-linux-arm64-lts-custom -t mavsdk/mavsdk-dockcross-linux-arm64-lts-custom .
 
 $DOCKER_CMD push mavsdk/mavsdk-dev:latest
 $DOCKER_CMD push mavsdk/mavsdk-dockcross-linux-armv6-custom:latest
 $DOCKER_CMD push mavsdk/mavsdk-dockcross-linux-armv7-custom:latest
 $DOCKER_CMD push mavsdk/mavsdk-dockcross-linux-arm64-custom:latest
+$DOCKER_CMD push mavsdk/mavsdk-dockcross-linux-arm64-lts-custom:latest
