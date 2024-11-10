dd1ba790d716f6135ab17e4cd836ad1671834060
Julian Oes <julian@oes.ch>
2024-11-11 06:47:55 +1300

    examples: fixup start_stop_server

--- examples/start_stop_server/start_stop_server.cpp
+++ examples/start_stop_server/start_stop_server.cpp
@@ -9,23 +9,26 @@
 
 using namespace mavsdk;
 
-
 static struct MavsdkServer* mavsdk_server;
 
-
-void signal_handler(int sig) {
+void signal_handler(int sig)
+{
     std::cout << "Received SIGUSR1 signal" << std::endl;
 
     mavsdk_server_stop(mavsdk_server);
 }
 
-
-int main(int, char**)
+int main(int argc, char* argv[])
 {
+    if (argc != 2) {
+        std::cout << "Connection string required as argument" << std::endl;
+        return 1;
+    }
+
     signal(SIGUSR1, signal_handler);
 
     mavsdk_server_init(&mavsdk_server);
-    mavsdk_server_run(mavsdk_server, "serial:///dev/serial/dev/by-id/usb-Auterion_PX4_FMU_v6C.x_0-if00", 50051);
+    mavsdk_server_run(mavsdk_server, argv[1], 50051);
 
     mavsdk_server_destroy(mavsdk_server);
 
