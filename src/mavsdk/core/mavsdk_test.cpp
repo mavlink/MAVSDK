d0e9089d43085535667e58d577a0933bc4e66e35
Carsten Ramke <carsten.ramke@gmail.com>
2025-01-01 15:29:39 +0100

    test: add unit test for configuration from component and mav_type

--- src/mavsdk/core/mavsdk_test.cpp
+++ src/mavsdk/core/mavsdk_test.cpp
@@ -8,3 +8,12 @@ TEST(Mavsdk, version)
     Mavsdk mavsdk{Mavsdk::Configuration{ComponentType::GroundStation}};
     ASSERT_GT(mavsdk.version().size(), 5);
 }
+
+TEST(Mavsdk, Configuration)
+{
+    Mavsdk::Configuration configuration{ComponentType::Autopilot};
+
+    ASSERT_EQ(configuration.get_mav_type(), MAV_TYPE::MAV_TYPE_GENERIC); // Default
+    configuration.set_mav_type(MAV_TYPE::MAV_TYPE_FIXED_WING);
+    ASSERT_EQ(configuration.get_mav_type(), MAV_TYPE::MAV_TYPE_FIXED_WING);
+}
