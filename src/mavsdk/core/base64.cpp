edf7fddfdc10809ce9793a6ece72e52080b9a7ac
Julian Oes <julian@oes.ch>
2025-06-17 18:11:57 +1200

    core: fix base64 style

--- src/mavsdk/core/base64.cpp
+++ src/mavsdk/core/base64.cpp
@@ -30,26 +30,30 @@ std::string base64_encode(std::vector<uint8_t>& raw)
             char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
             char_array_4[3] = char_array_3[2] & 0x3f;
 
-            for (i = 0; (i < 4); i++)
+            for (i = 0; (i < 4); i++) {
                 ret += base64_chars[char_array_4[i]];
+            }
             i = 0;
         }
     }
 
     if (i) {
-        for (j = i; j < 3; j++)
+        for (j = i; j < 3; j++) {
             char_array_3[j] = '\0';
+        }
 
         char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
         char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
         char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
         char_array_4[3] = char_array_3[2] & 0x3f;
 
-        for (j = 0; (j < i + 1); j++)
+        for (j = 0; (j < i + 1); j++) {
             ret += base64_chars[char_array_4[j]];
+        }
 
-        while ((i++ < 3))
+        while ((i++ < 3)) {
             ret += '=';
+        }
     }
 
     return ret;
@@ -68,32 +72,37 @@ std::vector<uint8_t> base64_decode(std::string const& encoded_string)
         char_array_4[i++] = encoded_string[in_];
         in_++;
         if (i == 4) {
-            for (i = 0; i < 4; i++)
+            for (i = 0; i < 4; i++) {
                 char_array_4[i] = base64_chars.find(char_array_4[i]);
+            }
 
             char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
             char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
             char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
 
-            for (i = 0; (i < 3); i++)
+            for (i = 0; (i < 3); i++) {
                 ret.push_back(char_array_3[i]);
+            }
             i = 0;
         }
     }
 
     if (i) {
-        for (j = i; j < 4; j++)
+        for (j = i; j < 4; j++) {
             char_array_4[j] = 0;
+        }
 
-        for (j = 0; j < 4; j++)
+        for (j = 0; j < 4; j++) {
             char_array_4[j] = base64_chars.find(char_array_4[j]);
+        }
 
         char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
         char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
         char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
 
-        for (j = 0; (j < i - 1); j++)
+        for (j = 0; (j < i - 1); j++) {
             ret.push_back(char_array_3[j]);
+        }
     }
 
     return ret;
