8c8edb974db78748e00a0dc55c9481160ee6e3e2
Julian Oes <julian@oes.ch>
2025-06-23 10:55:30 +1200

    core: cleanup http loader
    This fixes up HttpLoader and enables the unit tests again.

--- src/mavsdk/core/http_loader.cpp
+++ src/mavsdk/core/http_loader.cpp
@@ -1,17 +1,10 @@
 #include "http_loader.h"
 #include "curl_wrapper.h"
+#include "overloaded.h"
 
 namespace mavsdk {
 
-#ifdef TESTING
-HttpLoader::HttpLoader(const std::shared_ptr<ICurlWrapper>& curl_wrapper) :
-    _curl_wrapper(curl_wrapper)
-{
-    start();
-}
-#endif
-
-HttpLoader::HttpLoader() : _curl_wrapper(std::make_shared<CurlWrapper>())
+HttpLoader::HttpLoader() : _curl_wrapper(std::make_unique<CurlWrapper>(CurlWrapper()))
 {
     start();
 }
@@ -24,61 +17,80 @@ HttpLoader::~HttpLoader()
 void HttpLoader::start()
 {
     _should_exit = false;
-    _work_thread = new std::thread(work_thread, this);
+    _work_thread = std::thread{&HttpLoader::work_thread, this};
 }
 
 void HttpLoader::stop()
 {
     _should_exit = true;
-    _work_queue.stop();
-    if (_work_thread != nullptr) {
-        _work_thread->join();
-        delete _work_thread;
-        _work_thread = nullptr;
+    if (_work_thread.joinable()) {
+        _work_thread.join();
     }
 }
 
 bool HttpLoader::download_sync(const std::string& url, const std::string& local_path)
 {
-    auto work_item = std::make_shared<DownloadItem>(url, local_path, nullptr);
-    bool success = do_download(work_item, _curl_wrapper);
+    WorkItem item = DownloadItem{url, local_path, nullptr};
+    bool success = do_item(item);
     return success;
 }
 
 void HttpLoader::download_async(
     const std::string& url, const std::string& local_path, ProgressCallback progress_callback)
 {
-    auto work_item = std::make_shared<DownloadItem>(url, local_path, progress_callback);
-    _work_queue.enqueue(work_item);
+    _work_queue.push_back(
+        std::make_shared<WorkItem>(DownloadItem{url, local_path, progress_callback}));
 }
 
-void HttpLoader::work_thread(HttpLoader* self)
+void HttpLoader::download_text_async(const std::string& url, TextDownloadCallback text_callback)
 {
-    while (!self->_should_exit) {
-        auto item = self->_work_queue.dequeue();
-        auto curl_wrapper = self->_curl_wrapper;
-        if (!item || curl_wrapper == nullptr) {
+    _work_queue.push_back(std::make_shared<WorkItem>(DownloadTextItem{url, text_callback}));
+}
+
+void HttpLoader::work_thread()
+{
+    while (!_should_exit) {
+        LockedQueue<WorkItem>::Guard work_queue_guard(_work_queue);
+
+        auto work = work_queue_guard.get_front();
+
+        if (!work) {
             continue;
         }
-        do_item(item.value(), curl_wrapper);
+
+        do_item(*work);
+        work_queue_guard.pop_front();
     }
 }
 
-void HttpLoader::do_item(
-    const std::shared_ptr<WorkItem>& item, const std::shared_ptr<ICurlWrapper>& curl_wrapper)
+bool HttpLoader::do_item(WorkItem& item)
 {
-    auto download_item = std::dynamic_pointer_cast<DownloadItem>(item);
-    if (nullptr != download_item) {
-        do_download(download_item, curl_wrapper);
-        return;
-    }
+    return std::visit(
+        overloaded{
+            [&](DownloadItem& download_item) { return do_download(download_item); },
+            [&](DownloadTextItem& download_text_item) {
+                return do_download_text(download_text_item);
+            },
+        },
+        item);
 }
 
-bool HttpLoader::do_download(
-    const std::shared_ptr<DownloadItem>& item, const std::shared_ptr<ICurlWrapper>& curl_wrapper)
+bool HttpLoader::do_download(const DownloadItem& item)
 {
-    bool success = curl_wrapper->download_file_to_path(
-        item->get_url(), item->get_local_path(), item->get_progress_callback());
+    bool success = _curl_wrapper->download_file_to_path(
+        item.get_url(), item.get_local_path(), item.get_progress_callback());
+    return success;
+}
+
+bool HttpLoader::do_download_text(const DownloadTextItem& item)
+{
+    std::string content;
+    bool success = _curl_wrapper->download_text(item.get_url(), content);
+
+    if (item.get_text_callback()) {
+        item.get_text_callback()(success, content);
+    }
+
     return success;
 }
 
