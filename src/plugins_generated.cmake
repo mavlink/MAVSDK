9899a0940abba6da42faf5472bbbf1a6f7d4e1a8
Jonas Vautherin <dev@jonas.vautherin.ch>
2025-10-11 19:32:33 +0200

    Add ftp plugin and better handling of repeated strings

--- src/plugins_generated.cmake
+++ src/plugins_generated.cmake
@@ -11,6 +11,7 @@ set(PLUGIN_SOURCES
     cmavsdk/plugins/component_metadata_server/component_metadata_server.cpp
     cmavsdk/plugins/events/events.cpp
     cmavsdk/plugins/failure/failure.cpp
+    cmavsdk/plugins/ftp/ftp.cpp
     cmavsdk/plugins/ftp_server/ftp_server.cpp
     cmavsdk/plugins/geofence/geofence.cpp
     cmavsdk/plugins/gimbal/gimbal.cpp
@@ -31,6 +32,7 @@ set(PLUGIN_HEADERS
     cmavsdk/plugins/component_metadata_server/component_metadata_server.h
     cmavsdk/plugins/events/events.h
     cmavsdk/plugins/failure/failure.h
+    cmavsdk/plugins/ftp/ftp.h
     cmavsdk/plugins/ftp_server/ftp_server.h
     cmavsdk/plugins/geofence/geofence.h
     cmavsdk/plugins/gimbal/gimbal.h
