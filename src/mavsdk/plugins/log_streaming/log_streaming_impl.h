4e194b16844b26f1267242b6c9bd567fc9cb85e7
Julian Oes <julian@oes.ch>
2025-12-03 15:42:21 +1300

    log_streaming: add ArduPilot support
    This adds support for ArduPilot's REMOTE_LOG_DATA_BLOCK protocol.

--- src/mavsdk/plugins/log_streaming/log_streaming_impl.h
+++ src/mavsdk/plugins/log_streaming/log_streaming_impl.h
@@ -2,9 +2,12 @@
 
 #include "plugins/log_streaming/log_streaming.h"
 
+#include "call_every_handler.h"
 #include "plugin_impl_base.h"
 #include "callback_list.h"
+#include "log_streaming_backend.h"
 
+#include <memory>
 #include <mutex>
 
 namespace mavsdk {
@@ -36,32 +39,19 @@ public:
     void unsubscribe_log_streaming_raw(LogStreaming::LogStreamingRawHandle handle);
 
 private:
-    enum class DropState {
-        Unknown,
-        Dropped,
-        RecoveringFromDropped,
-        Ok,
-    };
-
-    void reset();
-    void process_logging_data(const mavlink_message_t& message);
-    void process_logging_data_acked(const mavlink_message_t& message);
-    bool is_duplicate(uint16_t sequence) const;
-    void check_drop_state(uint16_t sequence, uint8_t first_message);
-    void process_message();
-
-    static LogStreaming::Result
-    log_streaming_result_from_command_result(MavlinkCommandSender::Result result);
+    bool maybe_create_backend();
+    void process_data(const std::vector<uint8_t>& data);
 
     std::mutex _mutex{};
+    std::unique_ptr<LogStreamingBackend> _backend{};
     CallbackList<LogStreaming::LogStreamingRaw> _subscription_callbacks{};
-    std::vector<uint8_t> _ulog_data{};
-    DropState _drop_state{DropState::Unknown};
-    unsigned _drops{0};
-    uint16_t _current_sequence{0};
-    bool _active = false;
 
     bool _debugging{false};
+
+    // For waiting on autopilot type during start
+    LogStreaming::ResultCallback _start_callback{};
+    CallEveryHandler::Cookie _check_autopilot_cookie{};
+    unsigned _autopilot_poll_count{0};
 };
 
 } // namespace mavsdk
