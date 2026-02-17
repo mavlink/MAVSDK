c9202f16ee2f4d7c9d72d658a2a0092f97fe2349
Jose Canales <jose.canales@utec.edu.pe>
2026-02-17 16:22:25 -0500

    Apply shell best practices
    - Double quoting to avoid globbing/word splitting.
    - Use of bash array for common args.
    - Use grep to avoid Useless Use of Cat (UUOC)
    - Removed unused variable script_dir

--- cpp/tools/create_packages.sh
+++ cpp/tools/create_packages.sh
@@ -12,7 +12,6 @@ if [ "$#" -ne 4 ]; then
   exit 1
 fi
 
-script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 install_dir=$1
 output_dir=$2
 arch="$3"
@@ -26,77 +25,77 @@ else
 fi
 
 working_dir=$(mktemp -d)
-mkdir -p ${working_dir}/install
-cp -r ${install_dir} ${working_dir}/install/usr
+mkdir -p "${working_dir}/install"
+cp -r "${install_dir}" "${working_dir}/install/usr"
 
 # We need the relative path of all files.
-library_files=`find ${working_dir}/install -type f | cut -sd / -f 5-`
+library_files=$(find "${working_dir}/install" -type f | cut -sd / -f 5-)
 echo "Files to be packaged:"
-echo ${library_files}
+echo "${library_files}"
 
 # This creates a version such as "v1.2.3-5-g123abc".
-version=`git describe --always --tags`
+version=$(git describe --always --tags)
 # We want to extract 1.2.3 from it.
-version=`echo ${version} | sed 's/v\([0-9]*\.[0-9]*\.[0-9]*\).*$/\1/'`
+version=$(echo "${version}" | sed 's/v\([0-9]*\.[0-9]*\.[0-9]*\).*$/\1/')
 
-echo "#!/bin/sh" > ${working_dir}/run_ldconfig
-echo "/sbin/ldconfig" >> ${working_dir}/run_ldconfig
+echo "#!/bin/sh" > "${working_dir}/run_ldconfig"
+echo "/sbin/ldconfig" >> "${working_dir}/run_ldconfig"
 
-common_args="--chdir ${working_dir}/install \
-             --input-type dir  \
-             --name ${name} \
-             --version ${version} \
-             --maintainer julian@oes.ch \
-             --url https://mavsdk.mavlink.io \
-             --license BSD-3-Clause \
-             --after-install ${working_dir}/run_ldconfig \
-             --after-remove ${working_dir}/run_ldconfig \
-             --force \
-             -a ${package_arch}"
+common_args=(--chdir "${working_dir}/install"
+             --input-type dir
+             --name "${name}"
+             --version "${version}"
+             --maintainer julian@oes.ch
+             --url https://mavsdk.mavlink.io
+             --license BSD-3-Clause
+             --after-install "${working_dir}/run_ldconfig"
+             --after-remove "${working_dir}/run_ldconfig"
+             --force
+             -a "${package_arch}")
 
-if cat /etc/os-release | grep 'Ubuntu'
+if grep -q "Ubuntu" /etc/os-release;
 then
     echo "Building Ubuntu DEB package"
-    fpm ${common_args} \
+    fpm "${common_args[@]}" \
         --output-type deb \
         --deb-no-default-config-files \
-        ${library_files}
+        "${library_files}"
 
-    dist_version=$(cat /etc/os-release | grep VERSION_ID | sed 's/[^0-9.]*//g')
+    dist_version=$(grep VERSION_ID /etc/os-release | sed 's/[^0-9.]*//g')
 
-    for file in *_${package_arch}.deb
+    for file in *_"${package_arch}.deb"
     do
-        mv -v "${file}" "${output_dir}/${file%_${package_arch}.deb}_ubuntu${dist_version}_${arch}.deb"
+        mv -v "${file}" "${output_dir}/${file%_"${package_arch}".deb}_ubuntu${dist_version}_${arch}.deb"
     done
 
-elif cat /etc/os-release | grep 'Debian'
+elif grep -q "Debian" /etc/os-release;
 then
     echo "Building Debian DEB package"
 
-    fpm ${common_args} \
+    fpm "${common_args[@]}" \
         --output-type deb \
         --deb-no-default-config-files \
-        ${library_files}
+        "${library_files}"
 
-    dist_version=$(cat /etc/os-release | grep VERSION_ID | sed 's/[^0-9.]*//g')
+    dist_version=$(grep VERSION_ID /etc/os-release | sed 's/[^0-9.]*//g')
 
-    for file in *_${package_arch}.deb
+    for file in *_"${package_arch}".deb
     do
-        mv -v "${file}" "${output_dir}/${file%_${package_arch}.deb}_debian${dist_version}_${arch}.deb"
+        mv -v "${file}" "${output_dir}/${file%_"${package_arch}".deb}_debian${dist_version}_${arch}.deb"
     done
 
-elif cat /etc/os-release | grep 'Fedora'
+elif grep -q "Fedora" /etc/os-release;
 then
     echo "Building RPM package"
-    fpm ${common_args} \
+    fpm "${common_args[@]}" \
         --output-type rpm \
         --rpm-rpmbuild-define "_build_id_links none" \
-        ${library_files}
+        "${library_files}"
 
-    dist_version=$(cat /etc/os-release | grep VERSION_ID | sed 's/[^0-9]*//g')
+    dist_version=$(grep VERSION_ID /etc/os-release | sed 's/[^0-9]*//g')
 
-    for file in *.${package_arch}.rpm
+    for file in *."${package_arch}".rpm
     do
-        mv -v "${file}" "${output_dir}/${file%.${package_arch}.rpm}.fc${dist_version}-${arch}.rpm"
+        mv -v "${file}" "${output_dir}/${file%."${package_arch}".rpm}.fc${dist_version}-${arch}.rpm"
     done
 fi
