e31b6d023f33530ef3e02921ef188e5b816cd32b
Pavel Guzenfeld <pavelguzenfeld@gmail.com>
2026-03-30 23:43:58 +0300

    system_tests: Fix race in mission tests by registering server before connections
    MissionRawServer must be created before establishing connections so that
    MAV_PROTOCOL_CAPABILITY_MISSION_INT is registered before the first
    AUTOPILOT_VERSION exchange. Otherwise, the GCS may receive capabilities
    without MISSION_INT and reject uploads with "Int messages not supported".
    
    Fixes #2702

--- cpp/src/system_tests/mission_raw_upload_empty.cpp
+++ cpp/src/system_tests/mission_raw_upload_empty.cpp
@@ -15,13 +15,13 @@ TEST(SystemTest, MissionRawUploadEmpty)
     Mavsdk mavsdk_groundstation{Mavsdk::Configuration{ComponentType::GroundStation}};
     Mavsdk mavsdk_autopilot{Mavsdk::Configuration{ComponentType::Autopilot}};
 
+    auto mission_raw_server = MissionRawServer{mavsdk_autopilot.server_component()};
+
     ASSERT_EQ(
         mavsdk_groundstation.add_any_connection("udpin://0.0.0.0:17000"),
         ConnectionResult::Success);
     ASSERT_EQ(
         mavsdk_autopilot.add_any_connection("udpout://127.0.0.1:17000"), ConnectionResult::Success);
-
-    auto mission_raw_server = MissionRawServer{mavsdk_autopilot.server_component()};
 
     auto maybe_system = mavsdk_groundstation.first_autopilot(10.0);
     ASSERT_TRUE(maybe_system);
