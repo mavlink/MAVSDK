297b68dcd53c5151eb3a774ed74c4e38cf63a32e
Bartok9 <danielrpike9@gmail.com>
2026-07-21 04:08:24 -0400

    test(core): hostname_to_ip private dotted and whitespace edges

--- cpp/src/mavsdk/core/hostname_to_ip_test.cpp
+++ cpp/src/mavsdk/core/hostname_to_ip_test.cpp
@@ -32,3 +32,23 @@ TEST(HostnameToIp, Something)
     auto ip = resolve_hostname_to_ip(host);
     EXPECT_FALSE(ip);
 }
+
+TEST(HostnameToIp, DottedPrivate)
+{
+    auto ip = resolve_hostname_to_ip("192.168.1.10");
+    ASSERT_TRUE(ip);
+    EXPECT_EQ(ip.value(), "192.168.1.10");
+}
+
+TEST(HostnameToIp, ZeroZeroZeroZero)
+{
+    auto ip = resolve_hostname_to_ip("0.0.0.0");
+    ASSERT_TRUE(ip);
+    EXPECT_EQ(ip.value(), "0.0.0.0");
+}
+
+TEST(HostnameToIp, WhitespaceRejected)
+{
+    EXPECT_FALSE(resolve_hostname_to_ip(" "));
+    EXPECT_FALSE(resolve_hostname_to_ip("127.0.0.1 "));
+}
