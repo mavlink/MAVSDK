ea6d70eaf4e957b24a0d1b93922298531528129d
Jonas Vautherin <dev@jonas.vautherin.ch>
2026-07-30 00:37:49 +0200

    kt: coroutines should be part of the api

--- kt/mavsdk-kotlin/build.gradle.kts
+++ kt/mavsdk-kotlin/build.gradle.kts
@@ -65,7 +65,7 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+                api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
             }
         }
         
